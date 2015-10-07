94a07f70396ca9451d2320883dd0c1ee23bb9af3
LeDYoM <ledyom@hotmail.com>
2015-10-07 13:38:05 +0200

    Using created types Communication with BoardModelController

--- Zoper/lib/board/boardmodel.cpp
+++ Zoper/lib/board/boardmodel.cpp
@@ -1,15 +1,65 @@
 #include "boardmodel.hpp"
+#include "../log.hpp"
 
 namespace lib
 {
 	namespace board
 	{
-		BoardModel::BoardModel()
+		BoardModel::BoardModel(u32 w, u32 h, ITilesController *tController)
 		{
+			p_tController = tController;
+			LOG_CONSTRUCT(" w: " << w << " h: " << h);
+			for (u32 x = 0; x < w; ++x)
+			{
+				std::vector<s32> column;
+				for (u32 y = 0; y < h; ++y)
+				{
+					column.push_back(0);
+				}
+				_tiles.push_back(column);
+			}
 		}
 
 		BoardModel::~BoardModel()
 		{
+			LOG_DESTRUCT_NOPARAMS;
+			_tiles.clear();
+			p_tController = nullptr;
+		}
+
+		int BoardModel::getTile(u32 x, u32 y) const
+		{
+			if (validCoords(x, y))
+			{
+				return _tiles[x][y];
+			}
+			__ASSERT(false, "Error getting tile in coords " << x << "," << y);
+			return -1;
+		}
+
+		void BoardModel::setTile(u32 x, u32 y, s32 newTile)
+		{
+			if (validCoords(x, y))
+			{
+				_tiles[x][y] = newTile;
+				if (p_tController) p_tController->tileSet(x, y, newTile);
+			}
+			else
+			{
+				__ASSERT(false, "Error getting tile in coords " << x << "," << y);
+			}
+		}
+
+		void BoardModel::moveTile(u32 xSource, u32 ySource, u32 xDest, u32 yDest)
+		{
+			s32 sValue = getTile(xSource, ySource);
+			s32 dValue = getTile(xDest, yDest);
+
+			LOG_DEBUG("Moving tile from " << xSource << "," << ySource << " to " << xDest << "," << yDest << 
+				". Source Value: " << sValue << ". Erased value: " << dValue);
+
+			setTile(xDest, yDest, sValue);
+			if (p_tController) p_tController->tileMoved(xSource, ySource, xDest, yDest, sValue,dValue);
 		}
 	}
 }
--- Zoper/lib/board/boardmodel.hpp
+++ Zoper/lib/board/boardmodel.hpp
@@ -1,22 +1,28 @@
-#ifndef __BOARDMODEL_HPP__
-#define __BOARDMODEL_HPP__
+#ifndef __LIB_BOARDMODEL_HPP__
+#define __LIB_BOARDMODEL_HPP__
+
+#include <vector>
+#include "../types.hpp"
+#include "../compileconfig.hpp"
+#include "itilescontroller.hpp"
 
 namespace lib
 {
 	namespace board
 	{
-		struct BoardModelOptions
-		{
-			unsigned int w;
-			unsigned int h;
-		};
 		class BoardModel
 		{
 		public:
-			BoardModel();
+			BoardModel(u32 w, u32 h, ITilesController *tController = nullptr);
 			virtual ~BoardModel();
 
-			void create(const BoardModelOptions &options);
+			int getTile(u32 x, u32 y) const;
+			void setTile(u32 x, u32 y, s32 newTile);
+			void moveTile(u32 xSource, u32 ySource, u32 xDest, u32 yDest);
+		private:
+			inline bool validCoords(u32 x, u32 y) const { return _tiles.size() > x && _tiles[0].size() > y; }
+			std::vector<std::vector<s32>> _tiles;
+			ITilesController *p_tController{ nullptr };
 		};
 	}
 }
