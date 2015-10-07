7cbb9a8131ad031e205f39399c28a435ae976600
LeDYoM <ledyom@hotmail.com>
2015-10-07 13:33:55 +0200

    Created StateManager boilerplate code

---
+++ Zoper/lib/core/statemanager.cpp
@@ -0,0 +1,16 @@
+#include "statemanager.hpp"
+
+namespace lib
+{
+	namespace core
+	{
+		StateManager::StateManager()
+		{
+		}
+
+
+		StateManager::~StateManager()
+		{
+		}
+	}
+}
---
+++ Zoper/lib/core/statemanager.hpp
@@ -0,0 +1,16 @@
+#ifndef __LIB_STATEMANAGER_HPP__
+#define __LIB_STATEMANAGER_HPP__
+
+namespace lib
+{
+	namespace core
+	{
+		class StateManager
+		{
+		public:
+			StateManager();
+			virtual ~StateManager();
+		};
+	}
+}
+#endif
