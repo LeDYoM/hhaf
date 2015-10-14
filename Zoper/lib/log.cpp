837ac2c4b9ffd84234a6df6fbd41bac0dbf9970a
LeDYoM <ledyom@hotmail.com>
2015-10-14 19:11:12 +0200

    Multithread log

--- Zoper/lib/log.cpp
+++ Zoper/lib/log.cpp
@@ -4,9 +4,63 @@
 	#define WIN32_LEAN_AND_MEAN
 	#include <Windows.h>
 #endif
+
 #include <iostream>
 
+#ifdef __MULTITHREAD_LOG__
+	#include <queue>
+	#include <string>
+	#include <thread>
+	#include <mutex>
+	#include <condition_variable>
 
+
+	std::queue<std::string> logQueue;
+	std::mutex _mutex;
+	std::condition_variable _condVar;
+
+	void doLogOutput();
+	bool doLoop = true;
+
+	void logOutput(const char *str)
+	{
+		std::unique_lock<std::mutex> _lock(_mutex);
+		logQueue.push(str);
+		_condVar.notify_all();
+	}
+
+	void doLogOutput()
+	{
+		while (doLoop)
+		{
+			std::unique_lock<std::mutex> _lock(_mutex);
+			if (logQueue.empty())
+			{
+				_condVar.wait(_lock);
+			}
+			else
+			{
+				while (!logQueue.empty())
+				{
+					const std::string &str = logQueue.front();
+					std::cout << str;
+#if defined(_MSC_VER) || defined(__BORLANDC__)
+					OutputDebugString(str.c_str());
+#endif
+					logQueue.pop();
+				}
+			}
+		}
+	}
+
+	std::thread t1(doLogOutput);
+
+	void finishLog()
+	{
+		doLoop = false;
+		t1.join();
+	}
+#else
 void logOutput(const char *str)
 {
 	std::cout << str;
@@ -14,3 +68,9 @@ void logOutput(const char *str)
 	OutputDebugString(str);
 #endif
 }
+void finishLog()
+{
+
+}
+
+#endif
--- Zoper/lib/log.hpp
+++ Zoper/lib/log.hpp
@@ -3,9 +3,10 @@
 
 #include "compileconfig.hpp"
 
-#ifdef DEBUG
+#ifdef __USE_LOGS__
 	#include <sstream>
 
+	void finishLog();
 	void logOutput(const char *);
 	#define PREPARE_LOG(params) { std::ostringstream os_; os_ << params << std::endl; logOutput(os_.str().c_str()); }
 	#define LOG_DEBUG(x)			PREPARE_LOG(x)
@@ -15,7 +16,7 @@
 	#define LOG_CONSTRUCT_NOPARAMS	LOG_CONSTRUCT("")
 	#define LOG_DESTRUCT_NOPARAMS	LOG_DESTRUCT("")
 
-	#define __ASSERT(cond,x)		if (!cond) LOG_ERROR(x)
+	#define __ASSERT(cond,x)		if (!(cond)) LOG_ERROR(x<< "\n\tIn file "<<__FILE__<< " and line: "<<__LINE__<<"\n\tFunction: "<<__FUNCDNAME__ );
 
 #endif
 
