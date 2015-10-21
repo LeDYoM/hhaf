b4502d16c6711012e18bec59c301ac0f8cc87799
LeDYoM <ledyom@hotmail.com>
2015-10-21 22:37:06 +0200

    Deleted empty line

--- Zoper/lib/board/itilescontroller.hpp
+++ Zoper/lib/board/itilescontroller.hpp
@@ -17,7 +17,6 @@ namespace lib
 
 			virtual void tileSet(u32 x, u32 y, s32 oTile, s32 nTile) = 0;
 			virtual void tileMoved(u32 xSource, u32 ySource, u32 xDest, u32 yDest, s32 tile) = 0;
-
 		};
 	}
 }
