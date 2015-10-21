fd33a36b773e96617a7bd558be7d110e0cb763c7
Gonzalez <igonzalez@techconnect.local>
2015-10-21 16:52:23 +0200

    Empty value is class defined

--- Zoper/lib/board/boardmodel.hpp
+++ Zoper/lib/board/boardmodel.hpp
@@ -10,6 +10,7 @@ namespace lib
 {
 	namespace board
 	{
+		using BoardTile = s32;
 		class BoardModel
 		{
 		public:
@@ -17,13 +18,17 @@ namespace lib
 			virtual ~BoardModel();
 
 			int getTile(u32 x, u32 y) const;
+			inline bool tileEmpty(u32 x, u32 y) const { return getTile(x, y) == _emptyValue; }
 			void setTile(u32 x, u32 y, s32 newTile);
 			void moveTile(u32 xSource, u32 ySource, u32 xDest, u32 yDest);
 
 		private:
+			s32 _setTile(u32 x, u32 y, u32 newTile);
 			inline bool validCoords(u32 x, u32 y) const { return _tiles.size() > x && _tiles[0].size() > y; }
 			std::vector<std::vector<s32>> _tiles;
 			ITilesController *p_tController{ nullptr };
+			BoardTile _emptyValue{ 0 };
+
 		};
 	}
 }
