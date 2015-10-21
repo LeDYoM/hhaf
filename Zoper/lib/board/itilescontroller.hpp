0268f270a32f17ea9becb2d964a5578af8e5f04c
Gonzalez <igonzalez@techconnect.local>
2015-10-21 16:49:50 +0200

    Destination tile is not needed

--- Zoper/lib/board/itilescontroller.hpp
+++ Zoper/lib/board/itilescontroller.hpp
@@ -16,7 +16,7 @@ namespace lib
 			virtual ~ITilesController() {}
 
 			virtual void tileSet(u32 x, u32 y, s32 oTile, s32 nTile) = 0;
-			virtual void tileMoved(u32 xSource, u32 ySource, u32 xDest, u32 yDest, s32 sTile, s32 dTile) = 0;
+			virtual void tileMoved(u32 xSource, u32 ySource, u32 xDest, u32 yDest, s32 tile) = 0;
 
 		};
 	}
