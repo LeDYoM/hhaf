5fbb0113b8986a61ae48f8c3a0e9394b0086e169
Gonzalez <igonzalez@techconnect.local>
2015-10-30 17:07:42 +0100

    Tile is also a SFML Shape

--- Zoper/tile.cpp
+++ Zoper/tile.cpp
@@ -1,9 +1,9 @@
 #include "tile.hpp"
-
+#include <SFML/Graphics.hpp>
 
 namespace zoper
 {
-	Tile::Tile(lib::board::BoardTileData data) : lib::board::ITile{ data }
+	Tile::Tile(lib::board::BoardTileData data) : lib::board::ITile{ data }, lib::draw::Renderizable("",new sf::CircleShape)
 	{
 	}
 	Tile::~Tile()
--- Zoper/tile.hpp
+++ Zoper/tile.hpp
@@ -2,13 +2,14 @@
 #define __TILE_HPP__
 
 #include "lib/board/itilescontroller.hpp"
+#include "lib/draw/renderizable.hpp"
 
 namespace zoper
 {
-	class Tile : public lib::board::ITile
+	class Tile : public lib::board::ITile, public lib::draw::Renderizable
 	{
 	public:
-		Tile(lib::board::BoardTileData data);
+		explicit Tile(lib::board::BoardTileData data);
 		Tile(const Tile &other) = delete;
 		virtual ~Tile();
 	};
