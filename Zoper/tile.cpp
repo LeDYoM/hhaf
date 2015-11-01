0164a1691c1b06982986d8f017e2eb22a7ded488
LeDYoM <ledyom@hotmail.com>
2015-11-01 21:26:33 +0100

    Start drawing of the tile

--- Zoper/tile.cpp
+++ Zoper/tile.cpp
@@ -5,7 +5,37 @@ namespace zoper
 {
 	Tile::Tile(lib::board::BoardTileData data) : lib::board::ITile{ data }, lib::draw::Renderizable("",new sf::CircleShape)
 	{
+		sf::Color c;
+		switch (getData())
+		{
+		case 0:
+			c = sf::Color::Red;
+			break;
+		case 1:
+			c = sf::Color::Green;
+			break;
+		case 2:
+			c = sf::Color::Blue;
+			break;
+		case 3:
+			c = sf::Color::Yellow;
+			break;
+		case 4:
+			c = sf::Color::Magenta;
+			break;
+		default:
+			c = sf::Color::White;
+			LOG_ERROR("Error value for token: " << getData() << " is not supported");
+			break;
+		}
+		sf::CircleShape *this_ = getAsCircleShape();
+		this_->setFillColor(c);
+		this_->setPosition(100, 100);
+		this_->setRadius(500);
+		this_->setPointCount(5);
+
 	}
+
 	Tile::~Tile()
 	{
 	}
