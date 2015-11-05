546d6a921f513c096d9d17310aa22af1a8899af9
Gonzalez <igonzalez@techconnect.local>
2015-11-05 16:36:03 +0100

    Using the new utility class and move code that was in the gamescene.

--- Zoper/player.cpp
+++ Zoper/player.cpp
@@ -5,12 +5,16 @@
 
 namespace zoper
 {
-	Player::Player(lib::u32 x, lib::u32 y)
+	Player::Player(lib::u32 x, lib::u32 y, const sf::Vector2f &size)
 		:lib::board::ITile{ PLAYER_TOKEN }, lib::scn::draw::Renderizable{ "player", new lib::scn::draw::EllipseShape }
 	{
-		this->getAsEllipseShape()->setPointCount(3);
-		this->getAsEllipseShape()->setFillColor(sf::Color::White);
+		auto this_ = getAsEllipseShape();
+		this_->setPointCount(3);
+		this_->setFillColor(sf::Color::White);
+		this_->setSize(size);
+
 		setBoardPosition(x, y);
+		playerDirection = Direction::Up;
 	}
 
 	Player::~Player()
@@ -20,5 +24,26 @@ namespace zoper
 	{
 		_x = x;
 		_y = y;
+	}
+	bool Player::onKeyPressed(sf::Event::KeyEvent kEvent)
+	{
+		auto dir = getDirectionFromKey(kEvent.code);
+		if (dir != Direction::Total)
+		{
+			if (dir != playerDirection)
+			{
+				playerDirection = dir;
+			}
+		}
+		return false;
+	}
+	bool Player::onKeyReleased(sf::Event::KeyEvent kEvent)
+	{
+		return false;
+	}
+
+	void Player::updateGraphicalDirection()
+	{
+//		this->getAsEllipseShape()->setRotation();
 	}
 }
--- Zoper/player.hpp
+++ Zoper/player.hpp
@@ -4,19 +4,29 @@
 #include "lib/board/itilescontroller.hpp"
 #include "lib/scn/draw/renderizable.hpp"
 
+#include "utils.hpp"
+
 namespace zoper
 {
-	class Player : public lib::board::ITile, public lib::scn::draw::Renderizable
+	class Player : public lib::board::ITile, public lib::scn::draw::Renderizable, public KeyMapping
 	{
 	public:
-		Player(lib::u32 x,lib::u32 y);
+		Player(lib::u32 x,lib::u32 y, const sf::Vector2f &size);
 		virtual ~Player();
 
 		void setBoardPosition(lib::u32 x, lib::u32 y);
 		inline lib::u32 boardX() const { return _x; }
 		inline lib::u32 boardY() const { return _y; }
+
+		virtual bool onKeyPressed(sf::Event::KeyEvent kEvent);
+		virtual bool onKeyReleased(sf::Event::KeyEvent kEvent);
+
 	private:
-		lib::u32 _x, _y;
+		void updateGraphicalDirection();
+
+		lib::u32 _x;
+		lib::u32 _y;
+		Direction playerDirection;
 	};
 }
 
