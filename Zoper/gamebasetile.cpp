6bfc749eec200cb6dc2ea1257ad82a5c86f4effc
LeDYoM <ledyom@hotmail.com>
2015-11-29 11:53:40 +0100

    New base class for all game tiles created.

---
+++ Zoper/gamebasetile.cpp
@@ -0,0 +1,40 @@
+#include "gamebasetile.hpp"
+
+namespace zoper
+{
+	GameBaseTile::GameBaseTile(lib::board::BoardTileData data, const sf::Vector2f &size, const std::string &baseName) 
+		: lib::board::ITile{ data }, lib::scn::draw::Renderizable(baseName, new lib::scn::draw::EllipseShape)
+	{
+		sf::Color c;
+
+		switch (getData())
+		{
+		case 0:
+			c = sf::Color::Red;
+			break;
+		case 1:
+			c = sf::Color::Green;
+			break;
+		case 2:
+			c = sf::Color::Blue;
+			break;
+		case 3:
+			c = sf::Color::Yellow;
+			break;
+		case 4:
+			c = sf::Color::Magenta;
+			break;
+		default:
+			c = sf::Color::White;
+			LOG_ERROR("Error value for token: " << getData() << " is not supported");
+			break;
+		}
+		auto this_ = getAsEllipseShape();
+		this_->setFillColor(c);
+		this_->setSize(size);
+	}
+
+	GameBaseTile::~GameBaseTile()
+	{
+	}
+}
---
+++ Zoper/gamebasetile.hpp
@@ -0,0 +1,18 @@
+#ifndef __GAMEBASETILE_HPP__
+#define __GAMEBASETILE_HPP__
+
+#include "lib/board/itilescontroller.hpp"
+#include "lib/scn/draw/renderizable.hpp"
+
+namespace zoper
+{
+	class GameBaseTile : public lib::board::ITile, public lib::scn::draw::Renderizable
+	{
+	public:
+		explicit GameBaseTile(lib::board::BoardTileData data, const sf::Vector2f &size, const std::string &baseName);
+		GameBaseTile(const GameBaseTile &other) = delete;
+		virtual ~GameBaseTile();
+	};
+}
+
+#endif
