0330b1c2b15f1c1d9135de9134222619cd00fd16
LeDYoM <ledyom@hotmail.com>
2015-12-07 22:59:20 +0100

    Configuration wip

--- Zoper/lib/configuration.cpp
+++ Zoper/lib/configuration.cpp
@@ -6,13 +6,48 @@
 
 namespace lib
 {
-	Configuration::CMap Configuration::_data;
+	Configuration::CDataMap Configuration::_data;
 
 	Configuration::CMapLine split(const std::string& input, const std::string& regex) {
-		// passing -1 as the submatch index parameter performs splitting
-		std::sregex_token_iterator first(input.begin(), input.end(), std::regex(regex), -1 ),
+		// passing -1 as the sub match index parameter performs splitting
+		std::sregex_token_iterator first(input.begin(), input.end(), std::regex(regex), -1),
 			last;
 		return Configuration::CMapLine(*first, *last);
+	}
+
+	Configuration::Configuration(const std::string file, bool overwrite /*= false*/)
+		: _overwrite{overwrite}
+	{
+		CDataMap::iterator fIterator{ _data.find(file) };
+
+		if (fIterator != _data.end())
+		{
+			// Configuration file already in use.
+			currentMap = &(fIterator->second);
+		}
+		else
+		{
+			CMap cMap;
+
+			std::ifstream f(file);
+
+			if (f.is_open())
+			{
+				while (f)
+				{
+					std::string line;
+					f >> line;
+					CMapLine lineData(split(line, "="));
+					cMap[lineData.first] = lineData.second;
+				}
+			}
+
+			if (cMap.size() > 0)
+			{
+				_data[file] = cMap;
+				currentMap = &(_data[file]);
+			}
+		}
 	}
 
 	s32 Configuration::getAsInt(const std::string &name) const
@@ -22,8 +57,8 @@ namespace lib
 
 	std::string Configuration::getAsString(const std::string & name) const
 	{
-		CMap::iterator dataIterator = _data.find(name);
-		if (dataIterator != _data.end())
+		CMap::iterator dataIterator = currentMap->find(name);
+		if (dataIterator != currentMap->end())
 		{
 			return dataIterator->second;
 		}
@@ -35,20 +70,19 @@ namespace lib
 	{
 		if (_overwrite)
 		{
-			_data[name] = value;
+			(*currentMap)[name] = value;
 		}
 		else
 		{
-			auto iterator = _data.find(name);
-			if (iterator != _data.end())
+			auto iterator = currentMap->find(name);
+			if (iterator != currentMap->end())
 			{
 				return iterator->second;
 			}
 			else
 			{
-				_data.emplace(std::pair<std::string, std::string>(name, value));
+				currentMap->emplace(std::pair<std::string, std::string>(name, value));
 			}
-
 		}
 		return value;
 	}
@@ -57,22 +91,4 @@ namespace lib
 	{
 		return std::stoi(addConfigProperty(name, std::to_string(value)));
 	}
-
-	bool Configuration::addConfigFile(const std::string & fileName)
-	{
-		std::ifstream file(fileName);
-
-		if (file.is_open())
-		{
-			while (file)
-			{
-				std::string line;
-				file >> line;
-				CMapLine lineData(split(line, "="));
-			}
-			return true;
-		}
-		return false;
-	}
-
 }
--- Zoper/lib/configuration.hpp
+++ Zoper/lib/configuration.hpp
@@ -11,27 +11,27 @@ namespace lib
 	class Configuration
 	{
 	public:
-		Configuration(bool overwrite = false) : _overwrite{ overwrite } {}
+		Configuration(const std::string file, bool overwrite = false);
 		virtual ~Configuration() {}
 
 		s32 getAsInt(const std::string &name) const;
 		std::string getAsString(const std::string &name) const;
 
-		typedef std::map < std::string, std::string > CMap;
+		typedef std::map<std::string, std::string> CMap;
 		typedef std::pair<std::string, std::string> CMapLine;
+		typedef std::map<std::string, CMap> CDataMap;
 
 		std::string addConfigProperty(const std::string &name, const std::string &value);
 		s32 addConfigInt(const std::string &name, int value);
-		bool addConfigFile(const std::string &fileName);
 
 		inline bool overwrite() const { return _overwrite; }
 		inline void setOverwrite(bool nv) { _overwrite = nv; }
 
 	private:
 		bool _overwrite;
-		static CMap _data;
-
-
+		const std::string currentFile;
+		CMap *currentMap;
+		static CDataMap _data;
 	};
 
 }
