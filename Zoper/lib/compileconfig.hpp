64a05f8c39552fae38d59780f777b774cbf743dd
LeDYoM <ledyom@hotmail.com>
2015-12-30 20:47:09 +0100

    Use standard C++11 defines

--- Zoper/lib/compileconfig.hpp
+++ Zoper/lib/compileconfig.hpp
@@ -1,23 +1,19 @@
 #ifndef __LIB_COMPILECONFIG_HPP__
 #define __LIB_COMPILECONFIG_HPP__
 
-#ifdef _DEBUG
-	#define DEBUG
-#endif
-
-#ifdef DEBUG
-	#define USE_MEM_MANAGER
-
-	#define __USE_LOGS__
-	#define __EXIT_ON_ERROR__
-	#define __MULTITHREAD_LOG__
-	#define __LOGFILE__		"log.txt"
-#else
+#ifdef NDEBUG
 //	#define USE_MEM_MANAGER
 
 //	#define __USE_LOGS__
 //	#define __MULTITHREAD_LOG__
 //	#define __LOGFILE__		"log.txt"
+#else
+#define USE_MEM_MANAGER
+
+#define __USE_LOGS__
+#define __EXIT_ON_ERROR__
+#define __MULTITHREAD_LOG__
+#define __LOGFILE__		"log.txt"
 #endif
 
 
