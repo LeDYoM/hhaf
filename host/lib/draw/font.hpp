8ed082d68d8eaa63666ffe541e9b7690c214c8d8
LeDYoM <ledyom@hotmail.com>
2016-11-29 08:52:32 +0100

    Resource wrappers created

---
+++ host/lib/draw/font.hpp
@@ -0,0 +1,14 @@
+#ifndef LIB_DRAW_FONT_INCLUDE_HPP__
+#define LIB_DRAW_FONT_INCLUDE_HPP__
+
+namespace lib
+{
+	namespace draw
+	{
+		class Font
+		{
+		};
+	}
+}
+
+#endif
---
+++ host/lib/draw/texture.hpp
@@ -0,0 +1,14 @@
+#ifndef LIB_DRAW_TEXTURE_INCLUDE_HPP__
+#define LIB_DRAW_TEXTURE_INCLUDE_HPP__
+
+namespace lib
+{
+	namespace draw
+	{
+		class Texture
+		{
+		};
+	}
+}
+
+#endif
