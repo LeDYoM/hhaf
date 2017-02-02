96f0c8a2883b2be790cd8af1da5b29830eb0dd94
LeDYoM <ledyom@hotmail.com>
2017-02-02 13:05:49 +0100

    Deleted unused files

--- lib/backend/conversions.cpp
+++
@@ -1,9 +0,0 @@
-#include "conversions.hpp"
-
-#include <lib/draw/texture.hpp>
-#include <SFML/Graphics/Texture.hpp>
-
-const sf::Texture * lib::backend::convert(const draw::Texture *)
-{
-	return nullptr;
-}
