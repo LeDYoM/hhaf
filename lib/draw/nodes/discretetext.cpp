f231a9bd86e66b961345e2a53fd03e8a652a707a
LeDYoM <ledyom@hotmail.com>
2017-02-22 16:14:40 +0100

    Preparing node discrete text

---
+++ lib/draw/nodes/discretetext.cpp
@@ -0,0 +1,15 @@
+#include "discretetext.hpp"
+
+namespace lib
+{
+	namespace draw
+	{
+		namespace nodes
+		{
+			void DiscreteText::configure()
+			{
+				NodeText::configure();
+			}
+		}
+	}
+}
---
+++ lib/draw/nodes/discretetext.hpp
@@ -0,0 +1,26 @@
+#ifndef LIB_GUI_DISCRETETEXT_INCLUDE_HPP__
+#define LIB_GUI_DISCRETETEXT_INCLUDE_HPP__
+
+#include <lib/draw/renderizable.hpp>
+#include <lib/include/types.hpp>
+#include <lib/draw/nodes/nodetext.hpp>
+
+namespace lib
+{
+	namespace draw
+	{
+		namespace nodes
+		{
+			class DiscreteText : public NodeText
+			{
+			public:
+				using NodeText::NodeText;
+				virtual void configure() override;
+
+				Property<u32> index;
+			};
+		}
+	}
+}
+
+#endif
