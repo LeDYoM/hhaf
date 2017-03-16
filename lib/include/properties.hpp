ee57115e65a380dcee38bb7ba633b7c851b57a4b
LeDYoM <ledyom@hotmail.com>
2017-03-16 16:08:17 +0100

    First step in properties refractoring

--- lib/include/properties.hpp
+++ lib/include/properties.hpp
@@ -7,29 +7,50 @@
 namespace lib
 {
 	template <typename T>
-	class VirtualPropertyRead
+	class IPropertyRead
+	{
+	public:
+		virtual const T&get() const noexcept = 0;
+		virtual const T&operator()() const noexcept = 0;
+	};
+
+	template <typename T>
+	class IPropertyWrite
+	{
+	public:
+		virtual void set(const T&v) = 0;
+		virtual void set(T&&v) = 0;
+		virtual void operator=(const T&v) = 0;
+		virtual void operator=(T&&v) = 0;
+	};
+
+	template <typename T>
+	class IProperty : public IPropertyRead<T>, public IPropertyWrite<T> {};
+
+	template <typename T>
+	class VirtualPropertyRead : public IPropertyRead<T>
 	{
 	public:
 		using getter_t = std::function<const T&()>;
 		constexpr VirtualPropertyRead(const getter_t &getterp = {}) : getter{ getterp } {}
 		inline void setGetter(const getter_t &getterp) { getter = getterp; }
-		constexpr const T&get() const noexcept { return getter(); }
-		constexpr const T&operator()() const noexcept { return get(); }
+		virtual const T&get() const noexcept override { return getter(); }
+		virtual const T&operator()() const noexcept override { return get(); }
 	protected:
 		getter_t getter;
 	};
 
 	template <typename T>
-	class VirtualPropertyWrite
+	class VirtualPropertyWrite : public IPropertyWrite<T>
 	{
 	public:
 		using setter_t = std::function<void(const T&)>;
 		constexpr VirtualPropertyWrite(const setter_t &setterp = {}) : setter{ setterp } {}
 		inline void setSetter(const setter_t &setterp) { setter = setterp; }
-		inline void set(const T&v) { setter(v); }
-		inline void set(T&&v) { setter(std::move(v)); }
-		inline void operator=(const T&v) { set(v); }
-		inline void operator=(T&&v) { set(std::move(v)); }
+		virtual void set(const T&v) override { setter(v); }
+		virtual void set(T&&v) override { setter(std::move(v)); }
+		virtual void operator=(const T&v) override { set(v); }
+		virtual void operator=(T&&v) override { set(std::move(v)); }
 	protected:
 		setter_t setter;
 	};
@@ -42,46 +63,44 @@ namespace lib
 	};
 
 	template <typename T>
-	class Property
+	class Property : public IProperty<T>
 	{
 	public:
 		using callback_t = std::function<void()>;
 		constexpr Property(T iv = {}, callback_t c = {}) noexcept : m_value{ std::move(iv) }, m_callback{ std::move(c) } {}
-		Property(const Property&) = delete;
-		Property& operator=(const Property&) = delete;
 
-		constexpr const T &operator()() const { return m_value; }
 		inline void setCallback(callback_t c) { m_callback = std::move(c); }
-		constexpr const T &get() const { return m_value; }
-		inline void set(const T&v) { m_value = v; update(); }
-		inline void set(T&&v) { m_value = std::move(v); update(); }
-		inline Property &operator=(const T&v) { set(v); return *this; }
-		inline Property &operator=(T&&v) { set(std::move(v)); return *this; }
 
-		inline void update() { if (m_callback) m_callback(); }
+		virtual const T &operator()() const noexcept override { return m_value; }
+		virtual const T &get() const noexcept override { return m_value; }
+		virtual void set(const T&v) override { m_value = v; update(); }
+		virtual void set(T&&v) override { m_value = std::move(v); update(); }
+		virtual void operator=(const T&v) override { set(v); }
+		virtual void operator=(T&&v) override { set(std::move(v)); }
+
+		void update() { if (m_callback) m_callback(); }
 	private:
 		T m_value;
 		callback_t m_callback;
 	};
 
 	template <typename T>
-	class ForwardProperty
+	class ForwardProperty : public IProperty<T>
 	{
 	public:
 		using callback_t = std::function<void()>;
 		constexpr ForwardProperty() {}
 		constexpr ForwardProperty(Property<T> *const iv) noexcept : m_value{ iv } {}
-		ForwardProperty(const ForwardProperty&) = delete;
-		ForwardProperty& operator=(const ForwardProperty&) = delete;
 
-		constexpr const T &operator()() const noexcept { return (*m_value)(); }
 		inline void setForwardProperty(Property<T> *const p) noexcept { m_value = p; }
 		inline void setCallback(callback_t nc) noexcept { m_callback = nc; }
-		constexpr const T &get() const { return (*m_value).get(); }
-		inline void set(const T&v) { (*m_value) = v; update(); }
-		inline void set(T&&v) { (*m_value) = std::move(v); update(); }
-		inline ForwardProperty &operator=(const T&v) { set(v); return *this; }
-		inline ForwardProperty &operator=(T&&v) { set(std::move(v)); return *this; }
+
+		virtual const T &operator()() const noexcept override { return (*m_value)(); }
+		virtual const T &get() const noexcept override { return (*m_value).get(); }
+		virtual void set(const T&v) override { (*m_value) = v; update(); }
+		virtual void set(T&&v) override { (*m_value) = std::move(v); update(); }
+		virtual void operator=(const T&v) override { set(v); }
+		virtual void operator=(T&&v) override { set(std::move(v)); }
 		inline Property<T> *const innerProperty() const { return m_value; }
 		inline void update() { if (m_callback) m_callback(); }
 
