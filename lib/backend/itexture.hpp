81758a5d7f92576f38083b2cf45a3f5fd9576ac0
LeDYoM <ledyom@hotmail.com>
2017-04-16 19:44:55 +0200

    Added backend classes

---
+++ lib/backend/itexture.hpp
@@ -0,0 +1,22 @@
+#ifndef LIB_BACKEND_ITEXTURE_HPP
+#define LIB_BACKEND_ITEXTURE_HPP
+
+#include <lib/include/types.hpp>
+#include <lib/include/vector2d.hpp>
+#include <lib/include/rect.hpp>
+
+namespace lib
+{
+	namespace backend
+	{
+		class ITexture
+		{
+		public:
+
+			virtual ~ITexture() {}
+
+		};
+	}
+}
+#endif
+
---
+++ lib/backend/itexturefactory.hpp
@@ -0,0 +1,22 @@
+#ifndef LIB_BACKEND_ITEXTUREFACTORY_HPP
+#define LIB_BACKEND_ITEXTUREFACTORY_HPP
+
+#include <lib/include/types.hpp>
+#include <lib/include/vector2d.hpp>
+#include <lib/include/rect.hpp>
+
+namespace lib
+{
+	namespace backend
+	{
+		class ITextureFactory
+		{
+		public:
+
+			virtual ~ITextureFactory() {}
+
+		};
+	}
+}
+#endif
+
