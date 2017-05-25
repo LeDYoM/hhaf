2676a23dc04ae972b194ad3fa9cc16bd67dd4009
LeDYoM <ledyom@hotmail.com>
2017-05-25 23:00:19 +0200

    Small updates

--- lib/include/str.hpp
+++ lib/include/str.hpp
@@ -22,21 +22,50 @@ namespace lib
 		const size_t m_size;
 	};
 
-	class str_ {
-	public:
-		using size_t = unsigned int;
+#define INITIAL_SIZE 15
 
-		explicit str_(std::basic_string<char> rh) : m_data{ std::move(rh) } {}
+	class string
+	{
+	public:
+		string(size_t size) : m_data{ new char[size] }, m_size{ size } {}
+		string() : string(INITIAL_SIZE) {}
 
 		template<size_t N>
-		constexpr str_(const char(&a)[N]) noexcept : m_data{ a } {}
+		constexpr string(const char(&a)[N]) noexcept : m_data{ new char[N] }
 
-		char operator[](const size_t n) const noexcept { return m_data[n]; }
-		size_t size() const noexcept { return m_data.size(); }
-		explicit operator std::string() const { return std::string(m_data); }
-		const std::basic_string<char> &data() const noexcept { return m_data; }
+		string(const char*rhs) {
+			if (rhs) {
+				int iSize = strlen(rhs) + 1;
+				m_data = new char[iSize];
+				memset(m_data, 0, iSize);
+				m_size = iSize;
+				memcpy(m_data, rhs, strlen(rhs));
+			}
+		}
+		string(const string&strInstance) {
+			if (strInstance.empty() == false) {
+				this->m_data = new char[strInstance.size() + 1];
+				this->m_size = strInstance.size() + 1;
+				memset(this->m_data, 0, this->m_size);
+				memcpy(this->m_data, strInstance.data(), this->m_size);
+			}
+		}
+		string& string::operator=(const string &) {
+
+		}
+		string& string::operator+(const string &strInstance);
+		int find(const char*);
+		bool empty() const {
+
+		}
+		int size() const;
+		char* data() const;
+		~string(void);
 	private:
-		std::basic_string<char> m_data;
+		char *m_data;
+		size_t m_size;
+		const static int npos = -1;
+
 	};
 }
 
