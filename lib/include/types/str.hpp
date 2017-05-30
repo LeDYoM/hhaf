c8edff008f4a61b2084838aa01b49e0127c6fd54
LeDYoM <ledyom@hotmail.com>
2017-05-30 12:47:16 +0200

    clean up

--- lib/include/types/str.hpp
+++ lib/include/types/str.hpp
@@ -14,28 +14,14 @@ namespace lib
 	public:
 		using std::string::string;
 
-		str() : std::string{} {}
+		str() = default;
 		str(const std::string &source) : std::string{ source } {}
-		str(std::string &&source) : std::string{ std::move(source) } {}
 		str(const unsigned int n);
 		str(const signed int n);
 		str(const str &n);
 		str(const char *n);
 		str(const float n);
 		str(const double n);
-
-		template <typename T, typename... T2>
-		str(T&& head, T2&&... tail) : str() {
-			appendn(std::forward<T>(head));
-			appendn(std::forward<T2>(tail)...);
-		}
-
-		template <typename T>
-		str(T&& head) = delete;
-
-		//		str(str&&rhs) = default;
-		//		str &operator=(const std::string &source) { static_cast<std::string>(*this) = source; return *this; }
-		//		str &operator=(const str &source) = default;
 
 		std::vector<str> split(const char separator) const;
 		str &append() { return *this; }
@@ -45,25 +31,6 @@ namespace lib
 		str &append(const char *n);
 		str &append(const float n);
 		str &append(const double n);
-
-		template <typename T, class... T2>
-		str& appendn(T&& head, T2&&... tail)
-		{
-			append(std::forward<T>(head));
-			return appendn(std::forward<T2>(tail)...);
-		}
-
-		template <typename T>
-		str& appendn(T&& head)
-		{
-			return append(std::forward<T>(head));
-		}
-
-
-		str& appendn()
-		{
-			return *this;
-		}
 
 		template <typename T>
 		str &operator<<(const T&n)
@@ -79,7 +46,7 @@ namespace lib
 
 	};
 
-//	static_assert(std::is_move_constructible_v<str>, "str must be movable");
+	static_assert(std::is_move_constructible_v<str>, "str must be movable");
 	static_assert(std::is_move_assignable_v<str>, "str must be movable");
 	using string_vector = std::vector<str>;
 }
