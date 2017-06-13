6c03179a1596f7358acd608d4c079b146d4732d7
LeDYoM <ledyom@hotmail.com>
2017-06-13 13:56:09 +0200

    Removing includes and optimizations

--- backend_sfml/conversions.hpp
+++ backend_sfml/conversions.hpp
@@ -1,14 +1,9 @@
 #include "renderwindow.hpp"
-#include <lib/core/host.hpp>
-#include <lib/core/inputsystem.hpp>
 #include <lib/include/key.hpp>
-#include <lib/scene/transformation.hpp>
-#include <lib/scene/texture.hpp>
 #include <lib/backend/itexture.hpp>
 #include "texture.hpp"
 #include "texturettfont.hpp"
 #include <SFML/Window.hpp>
-#include <SFML/Graphics/Texture.hpp>
 
 namespace lib
 {
@@ -17,31 +12,29 @@ namespace lib
 		namespace sfmlb
 		{
 			template <typename T>
-			constexpr const sf::Rect<T> fromRect(const Rect<T> &rect) { return sf::Rect<T>{rect.left, rect.top, rect.width, rect.height}; }
+			constexpr const sf::Rect<T> fromRect(const Rect<T> &rect) noexcept { return sf::Rect<T>{rect.left, rect.top, rect.width, rect.height}; }
 
 			template <typename T>
-			constexpr const Rect<T> toRect(const sf::Rect<T> &rect) { return Rect<T>{rect.left, rect.top, rect.width, rect.height}; }
+			constexpr const Rect<T> toRect(const sf::Rect<T> &rect) noexcept { return Rect<T>{rect.left, rect.top, rect.width, rect.height}; }
 
 			template <typename T>
-			constexpr const sf::Vector2<T> fromVector2d(const vector2d<T> &v) { return sf::Vector2<T>{v.x, v.y}; }
+			constexpr const sf::Vector2<T> fromVector2d(const vector2d<T> &v) noexcept { return sf::Vector2<T>{v.x, v.y}; }
 
 			template <typename T>
-			constexpr const vector2d<T> toVector2d(const sf::Vector2<T> &v) { return vector2d<T>{v.x, v.y}; }
+			constexpr const vector2d<T> toVector2d(const sf::Vector2<T> &v) noexcept { return vector2d<T>{v.x, v.y}; }
 
-			inline input::Key doCast(const sf::Keyboard::Key &k)
+			constexpr input::Key doCast(const sf::Keyboard::Key &k) noexcept
 			{
-				int temp = k;
-				return static_cast<input::Key>(temp);
+				return static_cast<input::Key>((int)k);
 			}
 
-			inline sf::String getAsString(const inline_str &other)
+			inline sf::String getAsString(const inline_str &other) noexcept
 			{
 				std::wstring wsTmp(other.str, other.str + other.size);
-				sf::String temp(wsTmp);
-				return temp;
+				return sf::String(wsTmp);
 			}
 
-			inline sf::Transform asTransform(const f32* matrix)
+			inline sf::Transform asTransform(const f32* matrix) noexcept
 			{
 				return sf::Transform{ matrix[0], matrix[4], matrix[12],
 					matrix[1], matrix[5], matrix[13],
@@ -49,7 +42,7 @@ namespace lib
 
 			}
 
-			inline const sf::Texture *const asTexture(const ITexture*t)
+			constexpr const sf::Texture *const asTexture(const ITexture*t)
 			{
 				if (t) {
 					if (auto tmp = dynamic_cast<const Texture*>(t)) {
@@ -62,7 +55,7 @@ namespace lib
 				return nullptr;
 			}
 
-			inline const sf::RenderStates asRenderStates(const f32 *matrix, const ITexture *texture)
+			inline const sf::RenderStates asRenderStates(const f32 *matrix, const ITexture *texture) noexcept
 			{
 				return sf::RenderStates(sf::RenderStates::Default.blendMode,
 					asTransform(matrix),
