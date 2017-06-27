9ad3db3d6f6b00079bf4658dd638fc661b6a0ef4
LeDYoM <ledyom@hotmail.com>
2017-06-27 12:34:54 +0200

    Small optimization

--- lib/scene/nodes/nodeshape.cpp
+++ lib/scene/nodes/nodeshape.cpp
@@ -32,32 +32,33 @@ namespace lib
 
 			void NodeShape::updateGeometrySimpleNode()
 			{
-				const u32 nPoints(pointCount());
-				const u32 nVertex(nPoints + 2);
+				if (pointCount()) {
+					const u32 nPoints(pointCount());
+					const u32 nVertex(nPoints + 2);
 
-				const Rectf32 &cBox{ box() };
-				const vector2df size{ cBox.size() };
+					const Rectf32 &cBox{ box() };
+					const vector2df size{ cBox.size() };
 
-				BasicVertexArray &vertices(m_vertices.verticesArray());
-				const vector2df radius{ size / 2.0f };
+					BasicVertexArray &vertices(m_vertices.verticesArray());
+					const vector2df radius{ size / 2.0f };
 
-				vertices.resize(nVertex); // + 2 for center and repeated first point
-				const f64 baseAngle((2 * PIConstant<f64>) / nPoints);
-				for (std::size_t i = 0; i < nPoints; ++i)
-				{
-					const f64 angle = (i*baseAngle) - (PID2Constant<f64>);
-					const vector2dd r{ std::cos(angle) * radius.x, std::sin(angle) * radius.y };
-					vertices[i + 1].position = { static_cast<f32>(radius.x + r.x), static_cast<f32>(radius.y + r.y) };
-					vertices[i + 1].position += cBox.leftTop();
+					vertices.resize(nVertex); // + 2 for center and repeated first point
+					const f64 baseAngle((2 * PIConstant<f64>) / static_cast<f64>(nPoints));
+					for (u32 i = 0; i < nPoints; ++i) {
+						const f64 angle = (i*baseAngle) - (PID2Constant<f64>);
+						const vector2dd r{ std::cos(angle) * radius.x, std::sin(angle) * radius.y };
+						vertices[i + 1].position = { static_cast<f32>(radius.x + r.x), static_cast<f32>(radius.y + r.y) };
+						vertices[i + 1].position += cBox.leftTop();
+					}
+
+					vertices[nPoints + 1].position = vertices[1].position;
+
+					vertices[0].position = { size.x / 2, size.y / 2 };
+					vertices[0].position += cBox.leftTop();
+
+					// Update the bounding rectangle
+					m_vertices.setBounds(cBox);
 				}
-
-				vertices[nPoints + 1].position = vertices[1].position;
-
-				vertices[0].position = { size.x / 2, size.y / 2 };
-				vertices[0].position += cBox.leftTop();
-
-				// Update the bounding rectangle
-				m_vertices.setBounds(cBox);
 			}
 		}
 	}
