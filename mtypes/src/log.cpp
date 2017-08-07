9342b8cf94258556acb188c502a2b81fef6cf5ac
LeDYoM <ledyom@hotmail.com>
2017-08-07 23:40:58 +0200

    Added log to vs

--- mtypes/src/log.cpp
+++ mtypes/src/log.cpp
@@ -2,6 +2,11 @@
 #include <mtypes/include/str.hpp>
 #include <iostream>
 
+#ifdef _MSC_VER
+	#define WIN32_LEAN_AND_MEAN
+	#include <Windows.h>
+#endif
+
 namespace lib
 {
 	namespace log
@@ -18,6 +23,9 @@ namespace lib
 			void commitlog(str& log_stream) {
 				log_stream << "\n";
 				std::cout << log_stream.c_str();
+#ifdef _MSC_VER
+				OutputDebugString(log_stream.c_str());
+#endif
 			}
 		}
 	}
