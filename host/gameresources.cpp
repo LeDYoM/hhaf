d14d41b70c76b4972f6ac27fb9b0b1732fff869e
LeDYoM <ledyom@hotmail.com>
2017-08-10 12:23:12 +0200

    empty line

--- host/gameresources.cpp
+++ host/gameresources.cpp
@@ -5,6 +5,7 @@ namespace zoper
 {
 	using namespace lib;
 	using namespace lib::scene;
+
 	void GameResourcesLoader::loadResources(core::ResourceManager&resourceManager)
 	{
 		scoreFont = resourceManager.getResource<TTFont>("game_scene.scoreFont");
