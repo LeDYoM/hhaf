4680337e410a32e80af8fb57a679ccc471b9655b
Ismael González <ismael.gonzalez@zenuity.com>
2017-12-21 16:35:33 +0100

    Fixed loader in linux

--- loader/loadedinstance.cpp
+++ loader/loadedinstance.cpp
@@ -2,6 +2,26 @@
 #include <map>
 #include <string>
 
+namespace
+{
+    inline bool endsWith (const std::string &fullString, const std::string &ending) {
+        return fullString.length() >= ending.length() ?
+            fullString.compare (fullString.length() - ending.length(), ending.length(), ending) == 0 :
+            false;
+    }
+
+    inline void updateFileExtension(std::string &fileName,const std::string &extension) {
+        if (!endsWith(fileName, extension)) {
+            fileName += extension;
+        }
+    }
+
+    inline std::string formatFileName(std::string fileName, const char *const extension, const char *const prefix) {
+        updateFileExtension(fileName, extension);
+        return prefix + fileName;
+    }
+}
+
 #if defined(_MSC_VER) || defined(__BORLANDC__)
 	#define WIN32_LEAN_AND_MEAN
 	#include <windows.h>
@@ -21,6 +41,8 @@
 	}
     
     constexpr const char *const extension = ".dll";
+    constexpr const char *const prefix = "";
+    const
 #else
     // For now, windows or linux
     #include <dlfcn.h>
@@ -31,7 +53,7 @@
 
 	inline void *loadSharedObject(const char *fileName)
 	{
-		return static_cast<void*>(dlopen(fileName, RTLD_NOW));
+		return static_cast<void*>(dlopen(fileName, RTLD_LAZY));
 	}
 
 	inline bool freeSharedObject(void *handle)
@@ -39,29 +61,15 @@
 		return (dlclose(handle) != 0);
 	}   
     constexpr const char *const extension = ".so";
+    constexpr const char *const prefix = "lib";
+
 #endif
 
 namespace loader
 {
-    namespace
-    {
-        inline bool endsWith (const std::string &fullString, const std::string &ending) {
-            return fullString.length() >= ending.length() ? 
-                fullString.compare (fullString.length() - ending.length(), ending.length(), ending) == 0 :
-                false;
-        }
-    }
-
 	class LoadedInstancePrivate
 	{
 	public:
-        std::string updateFileExtension(const char *filename) {
-            std::string fileName{filename};
-            if (!endsWith(fileName, extension)) {
-                fileName += extension;
-            }
-            return fileName;
-        }
 
 		void *m_sharedFileHandle{ nullptr };
 		std::map<std::string, void*> m_methods;
@@ -77,7 +85,7 @@ namespace loader
 
 	bool LoadedInstance::load(const char * fileName)
 	{
-		m_private->m_sharedFileHandle = loadSharedObject(m_private->updateFileExtension(fileName).c_str());
+		m_private->m_sharedFileHandle = loadSharedObject(formatFileName(fileName, extension, prefix).c_str());
 		return loaded();
 	}
 
