0176bbf9036e1ae0836d08ab46ac76a7297866c9
Ismael González <ismael.gonzalez@zenuity.com>
2018-01-12 13:00:08 +0100

    Small code refractor

--- lib/scene/components/icomponent.hpp
+++ lib/scene/components/icomponent.hpp
@@ -7,58 +7,55 @@
 #include <mtypes/include/properties.hpp>
 #include <mtypes/include/log.hpp>
 
-namespace lib
+namespace lib::scene
 {
-	namespace scene
-	{
-		class SceneNode;
+    class SceneNode;
 
-		/**
-		* Base class for all components attached to a scene node.
-		*/
-		class IComponent
-		{
-		public:
-			/**
-			* Interface to be implemented to update the component
-			*/
-			virtual void update() {}
+    /**
+    * Base class for all components attached to a scene node.
+    */
+    class IComponent
+    {
+    public:
+        /**
+        * Interface to be implemented to update the component
+        */
+        virtual void update() {}
 
-			/**
-			* Destructor
-			*/
-			virtual ~IComponent() = default;
+        /**
+        * Destructor
+        */
+        virtual ~IComponent() {}
 
-			/**
-			* Method called after the component is attached to a node.
-			* Override it to perform initialization
-			*/
-			virtual void onAttached() {}
+        /**
+        * Method called after the component is attached to a node.
+        * Override it to perform initialization
+        */
+        virtual void onAttached() {}
 
-			/**
-			* Shortcut method to cast to another scenenode type
-			*/
-			template <typename T>
-			inline T*const attachedNodeAs() const { return dynamic_cast<T*>(m_sceneNode); }
+        /**
+        * Shortcut method to cast to another scenenode type
+        */
+        template <typename T>
+        inline T*const attachedNodeAs() const { return dynamic_cast<T*>(m_sceneNode); }
 
-			inline const SceneNode *const attachedNode() const noexcept { return m_sceneNode; }
-			inline SceneNode *const attachedNode() noexcept { return m_sceneNode; }
+        inline const SceneNode *const attachedNode() const noexcept { return m_sceneNode; }
+        inline SceneNode *const attachedNode() noexcept { return m_sceneNode; }
 
-		private:
-			SceneNode *m_sceneNode;
-			friend class ComponentContainer;
-		};
+    private:
+        SceneNode *m_sceneNode;
+        friend class ComponentContainer;
+    };
 
-		template <typename T1, typename T2>
-		class IComponentMixin : public IComponent
-		{
-			virtual void update() override
-			{
-				T1::update();
-				T2::update();
-			}
-		};
-	}
+    template <typename T1, typename T2>
+    class IComponentMixin : public IComponent
+    {
+        virtual void update() override
+        {
+            T1::update();
+            T2::update();
+        }
+    };
 }
 
 #endif
