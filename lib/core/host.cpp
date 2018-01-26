026de027b4f026dabd5c336f75764a49b92ed1e0
Ismael González <ismael.gonzalez@zenuity.com>
2018-01-26 19:57:17 +0100

    Using parpar

--- lib/core/host.cpp
+++ lib/core/host.cpp
@@ -11,188 +11,190 @@
 #include <lib/core/debugsystem.hpp>
 
 #include <mtypes/include/log.hpp>
-
 #include <parpar/include/parpar.hpp>
+
 #include <algorithm>
 
 #define LIB_VERSION_MAJOR 1
 #define LIB_VERSION_MINOR 4
 #define LIB_VERSION_PATCH 2
 
-namespace lib
+namespace lib::core
 {
-    namespace core
+    class HostPrivate final
     {
-        auto transformParams(int argc, char *argv[])
-        {
-            vector<str> temp;
+    public:
+        HostPrivate(const int argc, char *argv[]) : m_params{parpar::create(argc,argv)} {}
+        parpar::ParametersParser m_params;
+    };
 
-            for (int i = 1; i<argc; ++i) {
-                temp.push_back(argv[i]);
-            }
-            return temp;
+    auto transformParams(int argc, char *argv[])
+    {
+        vector<str> temp;
+
+        for (int i = 1; i<argc; ++i) {
+            temp.push_back(argv[i]);
         }
+        return temp;
+    }
 
-        enum class Host::AppState : u8
-        {
-            NotInitialized,
-            ReadyToStart,
-            Executing,
-            ReadyToTerminate,
-            Terminated
-        };
+    enum class Host::AppState : u8
+    {
+        NotInitialized,
+        ReadyToStart,
+        Executing,
+        ReadyToTerminate,
+        Terminated
+    };
 
-        Host *Host::m_instance = nullptr;
+    Host *Host::m_instance = nullptr;
 
-        bool Host::createHost(int argc, char * argv[])
-        {
-            if (!m_instance) {
-                m_instance = new Host(argc, argv);
-                backend::BackendFactory::initilialize("bsfml");
-                return true;
-            }
-            return false;
+    bool Host::createHost(int argc, char * argv[])
+    {
+        if (!m_instance) {
+            m_instance = new Host(argc, argv);
+            backend::BackendFactory::initilialize("bsfml");
+            return true;
         }
+        return false;
+    }
 
-        bool Host::destroyHost() noexcept
-        {
-            if (m_instance) {
-                backend::BackendFactory::destroy();
-                delete m_instance;
-                m_instance = nullptr;
-                return true;
-            }
-            return false;
+    bool Host::destroyHost() noexcept
+    {
+        if (m_instance) {
+            backend::BackendFactory::destroy();
+            delete m_instance;
+            m_instance = nullptr;
+            return true;
         }
+        return false;
+    }
 
-        Host::Host(int argc, char *argv[])
-            : m_state{ AppState::NotInitialized }
-        {
-            logConstruct_NOPARAMS;
-            log_release_info("Starting HostController...");
-            log_release_info("LIB version: ", LIB_VERSION_MAJOR,".", LIB_VERSION_MINOR,".", LIB_VERSION_PATCH);
-            log_release_info("Parsing parameters...");
-            m_params = transformParams(argc, argv);
+    Host::Host(int argc, char *argv[])
+        : m_private{muptr<HostPrivate>(argc,argv)}, m_state{ AppState::NotInitialized }
+    {
+        logConstruct_NOPARAMS;
+        log_release_info("Starting HostController...");
+        log_release_info("LIB version: ", LIB_VERSION_MAJOR,".", LIB_VERSION_MINOR,".", LIB_VERSION_PATCH);
+        log_release_info("Parsing parameters...");
+    }
+
+    Host::~Host()
+    {
+        logDestruct_NOPARAMS;
+    }
+
+    bool Host::setApplication(uptr<IApp> iapp)
+    {
+        if (!m_iapp && iapp) {
+            std::swap(m_iapp, iapp);
+            log_debug_info("Starting app ", appId(), "...");
+            m_state = AppState::ReadyToStart;
+            return true;
         }
+        return false;
+    }
 
-        Host::~Host()
+    bool Host::update()
+    {
+        switch (m_state)
         {
-            logDestruct_NOPARAMS;
-        }
-
-        bool Host::setApplication(uptr<IApp> iapp)
-        {
-            if (!m_iapp && iapp) {
-                std::swap(m_iapp, iapp);
-                log_debug_info("Starting app ", appId(), "...");
-                m_state = AppState::ReadyToStart;
-                return true;
-            }
-            return false;
-        }
-
-        bool Host::update()
-        {
-            switch (m_state)
-            {
-            case AppState::NotInitialized:
-                break;
-            case AppState::ReadyToStart:
-            {
-                log_debug_info(appId(), ": Starting initialization...");
-                m_state = AppState::Executing;
-
-                m_inputSystem = muptr<input::InputSystem>();
-                m_randomizer = muptr<Randomizer>();
-                m_eventManager = muptr<EventManager>();
-                m_window = muptr<Window>(m_iapp->getAppDescriptor().wcp);
-                m_sceneManager = muptr<scene::SceneManager>(*m_window);
-                m_resourceManager = muptr<core::ResourceManager>();
-                m_debugSystem = muptr<DebugSystem>();
-
-                m_iapp->onInit();
-                log_debug_info(appId(), ": Starting execution...");
-            }
+        case AppState::NotInitialized:
             break;
-            case AppState::Executing:
-            {
-                if (loopStep()) {
-                    m_state = AppState::ReadyToTerminate;
-                    log_debug_info(appId(), ": ", " is now ready to terminate");
-                }
-                else if (m_state == AppState::ReadyToTerminate) {
-                    log_debug_info(appId(), ": ", " requested to terminate");
-                }
+        case AppState::ReadyToStart:
+        {
+            log_debug_info(appId(), ": Starting initialization...");
+            m_state = AppState::Executing;
+
+            m_inputSystem = muptr<input::InputSystem>();
+            m_randomizer = muptr<Randomizer>();
+            m_eventManager = muptr<EventManager>();
+            m_window = muptr<Window>(m_iapp->getAppDescriptor().wcp);
+            m_sceneManager = muptr<scene::SceneManager>(*m_window);
+            m_resourceManager = muptr<core::ResourceManager>();
+            m_debugSystem = muptr<DebugSystem>();
+
+            m_iapp->onInit();
+            log_debug_info(appId(), ": Starting execution...");
+        }
+        break;
+        case AppState::Executing:
+        {
+            if (loopStep()) {
+                m_state = AppState::ReadyToTerminate;
+                log_debug_info(appId(), ": ", " is now ready to terminate");
             }
-            break;
-            case AppState::ReadyToTerminate:
-                log_debug_info(appId(), ": started termination");
-                m_sceneManager->finish();
-                m_state = AppState::Terminated;
+            else if (m_state == AppState::ReadyToTerminate) {
+                log_debug_info(appId(), ": ", " requested to terminate");
+            }
+        }
+        break;
+        case AppState::ReadyToTerminate:
+            log_debug_info(appId(), ": started termination");
+            m_sceneManager->finish();
+            m_state = AppState::Terminated;
 //				m_iapp->onFinish();
-                m_debugSystem = nullptr;
-                m_sceneManager = nullptr;
-                m_window = nullptr;
-                m_resourceManager = nullptr;
-                m_eventManager = nullptr;
-                m_randomizer = nullptr;
-                m_inputSystem = nullptr;
-                m_params.clear();
-                log_debug_info(appId(), ": terminated");
-                return true;
-                break;
-            case AppState::Terminated:
-                return true;
-                break;
-            default:
-                break;
+            m_debugSystem = nullptr;
+            m_sceneManager = nullptr;
+            m_window = nullptr;
+            m_resourceManager = nullptr;
+            m_eventManager = nullptr;
+            m_randomizer = nullptr;
+            m_inputSystem = nullptr;
+            m_private = nullptr;
+            log_debug_info(appId(), ": terminated");
+            return true;
+            break;
+        case AppState::Terminated:
+            return true;
+            break;
+        default:
+            break;
+        }
+        return false;
+    }
+
+    int Host::run()
+    {
+        while (!exit) {
+            if (update()) {
+                m_iapp.reset();
+                exit = true;
             }
-            return false;
         }
 
-        int Host::run()
-        {
-            while (!exit) {
-                const bool terminated{ update() };
-                if (terminated) {
-                    m_iapp.reset();
-                    exit = true;
-                }
-            }
-
-            if (!m_iapp) {
-                log_release_info("App destroyed. Exiting normally");
-            }
-            return 0;
+        if (!m_iapp) {
+            log_release_info("App destroyed. Exiting normally");
         }
+        return 0;
+    }
 
-        bool Host::loopStep()
-        {
-            const bool windowWants2Close{ m_window->preLoop() };
-            m_eventManager->update();
-            m_inputSystem->preUpdate();
-            m_sceneManager->update();
+    bool Host::loopStep()
+    {
+        const bool windowWants2Close{ m_window->preLoop() };
+        m_eventManager->update();
+        m_inputSystem->preUpdate();
+        m_sceneManager->update();
 
-            __ASSERT(m_currentScene || m_nextScene, "Current scene and nextscene cannot be nullptr at same time");
+        __ASSERT(m_currentScene || m_nextScene, "Current scene and nextscene cannot be nullptr at same time");
 
-            m_window->postLoop();
-            m_inputSystem->postUpdate();
-            return windowWants2Close;
+        m_window->postLoop();
+        m_inputSystem->postUpdate();
+        return windowWants2Close;
+    }
+
+    void Host::exitProgram()
+    {
+        __ASSERT(m_state == AppState::Executing, "Cannot terminate a program that is not in the executing state");
+        m_state = AppState::ReadyToTerminate;
+    }
+
+    const str Host::appId() const
+    {
+        if (m_iapp) {
+            const auto &cAppDescriptor(m_iapp->getAppDescriptor());
+            return make_str(cAppDescriptor.Name, ":", cAppDescriptor.Version, ".", cAppDescriptor.SubVersion, ".", cAppDescriptor.Patch);
         }
-
-        void Host::exitProgram()
-        {
-            __ASSERT(m_state == AppState::Executing, "Cannot terminate a program that is not in the executing state");
-            m_state = AppState::ReadyToTerminate;
-        }
-
-        const str Host::appId() const
-        {
-            if (m_iapp) {
-                const auto &cAppDescriptor(m_iapp->getAppDescriptor());
-                return make_str(cAppDescriptor.Name, ":", cAppDescriptor.Version, ".", cAppDescriptor.SubVersion, ".", cAppDescriptor.Patch);
-            }
-            return "NoApp:0.0.0";
-        }
+        return "NoApp:0.0.0";
     }
 }
--- lib/core/host.hpp
+++ lib/core/host.hpp
@@ -8,88 +8,92 @@
 
 namespace lib
 {
-	namespace input
-	{
-		class InputSystem;
-	}
-	namespace scene
-	{
-		class SceneManager;
-	}
-	namespace core
-	{
-		class Window;
-		class ResourceManager;
-		class EventManager;
-		class Randomizer;
-		class DebugSystem;
+    namespace input
+    {
+        class InputSystem;
+    }
+    namespace scene
+    {
+        class SceneManager;
+    }
+    namespace core
+    {
+        class Window;
+        class ResourceManager;
+        class EventManager;
+        class Randomizer;
+        class DebugSystem;
+        class HostPrivate;
 
-		class Host final
-		{
-		public:
-			static bool createHost(int argc, char *argv[]);
-			static bool destroyHost() noexcept;
+        class Host final
+        {
+        public:
+            static bool createHost(int argc, char *argv[]);
+            static bool destroyHost() noexcept;
 
-			inline static Host &host() noexcept { return *m_instance; }
-			enum class AppState : u8;
-			Host(int argc, char *argv[]);
-			~Host();
+            inline static Host &host() noexcept { return *m_instance; }
+            enum class AppState : u8;
+            Host(int argc, char *argv[]);
+            ~Host();
 
-			bool setApplication(uptr<IApp> iapp);
-			int run();
-			bool update();
+            bool setApplication(uptr<IApp> iapp);
+            int run();
+            bool update();
 
-			void exitProgram();
+            void exitProgram();
 
-			inline Window const &parentWindow() const noexcept { return *m_window; }
-			inline Window &parentWindow() noexcept { return *m_window; }
-			inline ResourceManager const &resourceManager() const  noexcept { return *m_resourceManager; }
-			inline ResourceManager &resourceManager()  noexcept { return *m_resourceManager; }
-			inline EventManager const &eventManager() const  noexcept { return *m_eventManager; }
-			inline EventManager &eventManager()  noexcept { return *m_eventManager; }
-			inline input::InputSystem const &inputSystem() const noexcept { return *m_inputSystem; }
-			inline input::InputSystem &inputSystem() noexcept { return *m_inputSystem; }
-			inline DebugSystem const &debugSystem() const noexcept { return *m_debugSystem; }
-			inline DebugSystem &debugSystem() noexcept { return *m_debugSystem; }
-			inline scene::SceneManager &sceneManager() noexcept { return *m_sceneManager; }
-			inline const scene::SceneManager &sceneManager() const noexcept { return *m_sceneManager; }
+            inline Window const &parentWindow() const noexcept { return *m_window; }
+            inline Window &parentWindow() noexcept { return *m_window; }
+            inline ResourceManager const &resourceManager() const  noexcept { return *m_resourceManager; }
+            inline ResourceManager &resourceManager()  noexcept { return *m_resourceManager; }
+            inline EventManager const &eventManager() const  noexcept { return *m_eventManager; }
+            inline EventManager &eventManager()  noexcept { return *m_eventManager; }
+            inline input::InputSystem const &inputSystem() const noexcept { return *m_inputSystem; }
+            inline input::InputSystem &inputSystem() noexcept { return *m_inputSystem; }
+            inline DebugSystem const &debugSystem() const noexcept { return *m_debugSystem; }
+            inline DebugSystem &debugSystem() noexcept { return *m_debugSystem; }
+            inline scene::SceneManager &sceneManager() noexcept { return *m_sceneManager; }
+            inline const scene::SceneManager &sceneManager() const noexcept { return *m_sceneManager; }
 
-			template <typename T>
-			T &app() { return *(dynamic_cast<T*>(m_iapp.get())); }
+            template <typename T>
+            T &app() { return *(dynamic_cast<T*>(m_iapp.get())); }
 
-			const str appId() const;
+            const str appId() const;
 
-			inline Randomizer &randomizer() const noexcept { return *m_randomizer; }
+            inline Randomizer &randomizer() const noexcept { return *m_randomizer; }
 
-		private:
-			bool loopStep();
+        private:
+            friend class lib::core::HostPrivate;
+            uptr<HostPrivate> m_private;
 
-			static Host *m_instance;
-			AppState m_state;
-			uptr<Window> m_window;
-			uptr<IApp> m_iapp;
-			uptr<ResourceManager> m_resourceManager;
-			uptr<EventManager> m_eventManager;
-			uptr<Randomizer> m_randomizer;
-			uptr<DebugSystem> m_debugSystem;
-			uptr<input::InputSystem> m_inputSystem;
-			uptr<scene::SceneManager> m_sceneManager;
-			bool exit{ false };
-			vector<str> m_params;
-		};
-	}
+            bool loopStep();
+
+            static Host *m_instance;
+
+            AppState m_state;
+            uptr<Window> m_window;
+            uptr<IApp> m_iapp;
+            uptr<ResourceManager> m_resourceManager;
+            uptr<EventManager> m_eventManager;
+            uptr<Randomizer> m_randomizer;
+            uptr<DebugSystem> m_debugSystem;
+            uptr<input::InputSystem> m_inputSystem;
+            uptr<scene::SceneManager> m_sceneManager;
+            bool exit{ false };
+        };
+    }
 
     template <typename AppType>
     inline AppType &app() {
         return core::Host::host().app<AppType>();
     }
 
-	inline core::Host &host() noexcept { return core::Host::host(); }
-	inline core::Window &window() noexcept { return core::Host::host().parentWindow(); }
-	inline core::ResourceManager &resourceManager() noexcept { return core::Host::host().resourceManager(); }
-	inline core::Randomizer &randomizer() noexcept { return core::Host::host().randomizer(); }
-	inline core::DebugSystem &debugSystem() noexcept { return core::Host::host().debugSystem(); }
-	inline scene::SceneManager &sceneManager() noexcept { return core::Host::host().sceneManager(); }
+    inline core::Host &host() noexcept { return core::Host::host(); }
+    inline core::Window &window() noexcept { return core::Host::host().parentWindow(); }
+    inline core::ResourceManager &resourceManager() noexcept { return core::Host::host().resourceManager(); }
+    inline core::Randomizer &randomizer() noexcept { return core::Host::host().randomizer(); }
+    inline core::DebugSystem &debugSystem() noexcept { return core::Host::host().debugSystem(); }
+    inline scene::SceneManager &sceneManager() noexcept { return core::Host::host().sceneManager(); }
 }
 
 #endif
