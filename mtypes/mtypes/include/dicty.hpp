8bf01da73ed38578f48c64bf1bcbda7706e5c0cc
LeDYoM <ledyom@hotmail.com>
2018-01-28 23:09:55 +0100

    dicty wip

--- mtypes/mtypes/include/dicty.hpp
+++ mtypes/mtypes/include/dicty.hpp
@@ -1,64 +1,56 @@
-<<<<<<< HEAD
 #pragma once
 
 #ifndef MTYPES_DICTY_INCLUDE_HPP
 #define MTYPES_DICTY_INCLUDE_HPP
 
 #include "types.hpp"
+#include "str.hpp"
 
 namespace lib
 {
     class Dictionary
     {
     public:
-        bool add(str name, str value, const bool overwrite = true)
+		using element = std::pair<str, str>;
+		using content = vector<element>;
+		using iterator = content::iterator;
+		using const_iterator = content::const_iterator;
+
+        bool add(str key, str value, const bool overwrite = true)
         {
-            return std::get<1>(addInternal(name, value));
+			auto it(find(key));
+
+			if (it == m_data.cend()) {
+				m_data.emplace_back(std::move(key), std::move(value));
+				return true;
+			}
+			else if (overwrite) {
+				(*it) = element(key, value);
+				return true;
+			}
+			return false;
         }
     private:
-        template <typename T1, typename T2>
-        constexpr decltype(auto) addInternal(T1&& t1, T2&& t2, const bool overwrite = true)
-        {
-            if (overwrite) {
-                return m_data.insert_or_assign(std::forward<T1>(t1),std::forward<T2>(t2));
-            } else {
-                return m_data.try_emplace(std::forward<T1>(t1),std::forward<T2>(t2));
-            }
-        }
+		const_iterator find(const str &key) const noexcept {
+			for (const auto& element : m_data) {
+				if (element.first == key) {
+					return &element;
+				}
+			}
+			return m_data.cend();
+		}
 
-        map<str,str> m_data;
+		iterator find(const str &key) noexcept {
+			for (auto& element : m_data) {
+				if (element.first == key) {
+					return &element;
+				}
+			}
+			return m_data.end();
+		}
+
+        content m_data;
     };
-=======
-#ifndef LIB_DICTY_INCLUDE_HPP
-#define LIB_DICTY_INCLUDE_HPP
-
-#include "types.hpp"
-#include "str.hpp"
-#include <utility>
-#include <variant>
-
-namespace lib::dicty
-{
-	class Element;
-
-	class BasicElement
-	{
-	public:
-		inline const str& key() const noexcept { return m_data.first; }
-		inline const str& value() const noexcept { return m_data.second; }
-
-	private:
-		std::pair<str, str> m_data;
-	};
-
-	class Array
-	{
-	public:
-
-	private:
-		vector<Element> data;
-	};
->>>>>>> development
 }
 
 #endif
