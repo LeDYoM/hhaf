ff994a2aef590d9ad7666006e49c7dc4f5347069
Ismael González <ismael.gonzalez@zenuity.com>
2018-01-29 16:30:28 +0100

    wip in dicty

--- mtypes/mtypes/include/dicty.hpp
+++ mtypes/mtypes/include/dicty.hpp
@@ -3,51 +3,64 @@
 #ifndef MTYPES_DICTY_INCLUDE_HPP
 #define MTYPES_DICTY_INCLUDE_HPP
 
-#include "types.hpp"
-#include "str.hpp"
-
 namespace lib
 {
-    class Dictionary
+    template <typename str_type>
+    class BasicDictionary
     {
     public:
-		using element = std::pair<str, str>;
-		using content = vector<element>;
-		using iterator = content::iterator;
-		using const_iterator = content::const_iterator;
+        using element = std::pair<str_type, str_type>;
+        using content = vector<element>;
+        using iterator = content::iterator;
+        using const_iterator = content::const_iterator;
 
-        bool add(str key, str value, const bool overwrite = true)
+        bool add(std::initializer_list<element> eList, const bool overwrite=true)
         {
-			auto it(find(key));
-
-			if (it == m_data.cend()) {
-				m_data.emplace_back(std::move(key), std::move(value));
-				return true;
-			}
-			else if (overwrite) {
-				(*it) = element(key, value);
-				return true;
-			}
-			return false;
+            bool result{true};
+            for (element&& elems : eList) {
+                result &= add(elems.first, elems.second, overwrite);
+            }
+            return result;
         }
-    private:
-		const_iterator find(const str &key) const noexcept {
-			for (const auto& element : m_data) {
-				if (element.first == key) {
-					return &element;
-				}
-			}
-			return m_data.cend();
-		}
 
-		iterator find(const str &key) noexcept {
-			for (auto& element : m_data) {
-				if (element.first == key) {
-					return &element;
-				}
-			}
-			return m_data.end();
-		}
+        bool add(str_type key, str_type value, const bool overwrite = true)
+        {
+            auto it(find(key));
+
+            if (it == m_data.cend()) {
+                m_data.emplace_back(std::move(key), std::move(value));
+                return true;
+            }
+            else if (overwrite) {
+                (*it) = element(key, value);
+                return true;
+            }
+            return false;
+        }
+
+        void clear() {
+            m_data.clear();
+        }
+
+
+    private:
+        const_iterator find(const str_type &key) const noexcept {
+            for (const auto& element : m_data) {
+                if (element.first == key) {
+                    return &element;
+                }
+            }
+            return m_data.cend();
+        }
+
+        iterator find(const str_type &key) noexcept {
+            for (auto& element : m_data) {
+                if (element.first == key) {
+                    return &element;
+                }
+            }
+            return m_data.end();
+        }
 
         content m_data;
     };
