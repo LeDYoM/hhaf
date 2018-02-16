c6c7c7bdb048e408e411557d894523e044d7320e
Ismael González <ismael.gonzalez@zenuity.com>
2018-02-16 16:06:07 +0100

    Improved dicty features

--- mtypes/mtypes/include/dicty.hpp
+++ mtypes/mtypes/include/dicty.hpp
@@ -21,15 +21,15 @@ namespace lib::dicty
         using iterator = typename content::iterator;
         using const_iterator = typename content::const_iterator;
 
-		constexpr BasicDictionary() = default;
+        constexpr BasicDictionary() = default;
 
-		constexpr BasicDictionary(std::initializer_list<element> eList)
+        constexpr BasicDictionary(std::initializer_list<element> eList)
             : m_data{ eList } {}
 
-		constexpr BasicDictionary(const content &eList)
+        constexpr BasicDictionary(const content &eList)
             : m_data{ eList } {}
 
-		constexpr bool add(const content &eList, const bool overwrite = true)
+        constexpr bool add(const content &eList, const bool overwrite = true)
         {
             bool result{ true };
             for (const element& elems : eList) {
@@ -38,7 +38,7 @@ namespace lib::dicty
             return result;
         }
 
-		constexpr bool add(std::initializer_list<element> eList, const bool overwrite = true)
+        constexpr bool add(std::initializer_list<element> eList, const bool overwrite = true)
         {
             bool result{true};
             for (const element& elems : eList) {
@@ -47,7 +47,7 @@ namespace lib::dicty
             return result;
         }
 
-		constexpr bool add(str key, T value, const bool overwrite = true)
+        constexpr bool add(str key, T value, const bool overwrite = true)
         {
             auto it(find(key));
 
@@ -97,33 +97,44 @@ namespace lib::dicty
             return find(key) != m_data.cend();
         }
 
-		constexpr std::pair<bool,iterator> findChecked(const str&key) noexcept
+        constexpr std::pair<bool,iterator> findChecked(const str&key) noexcept
         {
             auto iterator(find(key));
             return {iterator != m_data.end(), iterator};
         }
 
-		constexpr std::pair<bool,const_iterator> findChecked(const str&key) const noexcept
+        constexpr std::pair<bool,const_iterator> findChecked(const str&key) const noexcept
         {
             auto iterator(find(key));
             return {iterator != m_data.cend(), iterator};
         }
 
+        constexpr size_type size() const noexcept {
+            return m_data.size();
+        }
+
+        constexpr bool operator==(const BasicDictionary &other) const noexcept {
+            if (m_data.size() == other.m_data.size()) {
+
+                for (size_type i = 0; i < m_data.size(); ++i) {
+                    if (m_data[i].first != other.m_data[i].first) {
+                        return false;
+                    }
+                    if (m_data[i].second != other.m_data[i].second) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        constexpr bool operator!=(const BasicDictionary &other) const noexcept {
+            return !(*this == other);
+        }
+
         content m_data;
         friend class Object;
-    };
-
-    class Value
-    {
-    public:
-        constexpr Value() = default;
-        constexpr Value(Object *obj) : m_object{obj} {}
-        constexpr Value(str *data) : m_data{data} {}
-
-        constexpr bool isValid() const noexcept { return m_object || m_data; }
-    private:
-        Object *m_object{nullptr};
-        str *m_data{nullptr};
     };
 
     class Object
@@ -134,25 +145,87 @@ namespace lib::dicty
 
         constexpr Object() {}
 
-		constexpr Object(const std::initializer_list<std::pair<str,str>> iListValues) {
+        constexpr Object(const std::initializer_list<std::pair<str,str>> iListValues) {
             set(iListValues);
         }
 
-		constexpr Object(const std::initializer_list<std::pair<str,Object>> iListObjects) {
-			set(iListObjects);
+        constexpr Object(const std::initializer_list<std::pair<str,Object>> iListObjects) {
+            set(iListObjects);
         }
 
-		constexpr Object(std::initializer_list<std::pair<str, Object>> iListObjects,
-	            std::initializer_list<std::pair<str, str>> iListValues) {
-			set(iListObjects);
-			set(iListValues);
-		}
+        constexpr Object(std::initializer_list<std::pair<str, Object>> iListObjects,
+                std::initializer_list<std::pair<str, str>> iListValues) {
+            set(iListObjects);
+            set(iListValues);
+        }
 
-		constexpr Object(std::initializer_list<std::pair<str, str>> iListValues,
+        constexpr Object(std::initializer_list<std::pair<str, str>> iListValues,
                std::initializer_list<std::pair<str, Object>> iListObjects) {
-			set(iListValues);
-			set(iListObjects);
-		}
+            set(iListValues);
+            set(iListObjects);
+        }
+
+        constexpr bool operator==(const Object &obj) const noexcept {
+            return m_values == obj.m_values && m_objects == obj.m_objects;
+        }
+
+        constexpr bool operator!=(const Object &obj) const noexcept {
+            return !((*this) == obj);
+        }
+
+        class Value
+        {
+        public:
+            constexpr Value() = default;
+            constexpr Value(Object *obj) : m_object{obj} {}
+            constexpr Value(str *val) : m_value{val} {}
+
+            constexpr bool isValid() const noexcept { return m_object || m_value; }
+            constexpr bool isValue() const noexcept { return m_value != nullptr; }
+            constexpr bool isObject() const noexcept { return m_object != nullptr; }
+
+            constexpr bool operator==(const str&key) const noexcept {
+                if (isValue()) {
+                    return (*m_value) == key;
+                }
+                else {
+                    return false;
+                }
+            }
+
+            constexpr bool operator!=(const str&key) const noexcept {
+                return !(*this==key);
+            }
+
+            constexpr bool operator==(const Object &obj) const noexcept {
+                if (isObject()) {
+                    return (*m_object) == obj;
+                }
+                else {
+                    return false;
+                }
+            }
+
+            constexpr bool operator!=(const Object &obj) const noexcept {
+                return !(*this==obj);
+            }
+
+            Value operator[](const str&key)
+            {
+                // Note: Priority to str
+                if (isValue()) {
+                    return {};
+                }
+                else {
+                    return (*m_object)[key];
+                }
+                return *this;
+            }
+
+        private:
+            Object *m_object{nullptr};
+            str *m_value{nullptr};
+        };
 
         Value getObject(const str&key) {
             auto token(m_objects.findChecked(key));
@@ -168,29 +241,40 @@ namespace lib::dicty
                 Value());
         }
 
-		template <typename... Args>
-		Value getObject(const str&key, Args&&... args) {
-			Value v{ std::move(getObject(key)) };
-			if constexpr (sizeof...(args) > 0) {
-				return (v.isValid()) ?
-					return getObject(std::forward<Args>(args)...)
-					: v;
-			}
-			return v;
-		}
+        Value operator[](const str&key)
+        {
+            // Note: Priority to str
+            Value val{getValue(key)};
+            if (val.isValid()) {
+                return val;
+            }
+            else {
+                return getObject(key);
+            }
+        }
 
+        template <typename... Args>
+        Value getObject(const str&key, Args&&... args) {
+            Value v{ std::move(getObject(key)) };
+            if constexpr (sizeof...(args) > 0) {
+                return (v.isValid()) ?
+                    getObject(std::forward<Args>(args)...) :
+                    v;
+            }
+            return v;
+        }
 
     private:
-        constexpr bool set(const std::initializer_list<std::pair<str, str>> iListValues, bool overwrite = true)
+        bool set(const std::initializer_list<std::pair<str, str>> iListValues, bool overwrite = true)
         {
             bool ok{true};
             for (auto&& element : iListValues) {
-                ok &= m_values.add(element.first,element.second, overwrite);
+                ok &= m_values.add(std::move(element.first), std::move(element.second), overwrite);
             }
             return ok;
         }
 
-        constexpr bool set(const std::initializer_list<std::pair<str,Object>> iListObject, bool overwrite = true)
+        bool set(const std::initializer_list<std::pair<str,Object>> iListObject, bool overwrite = true)
         {
             bool ok{true};
             for (auto&& element : iListObject) {
--- mtypes/tests/dicty.test.cpp
+++ mtypes/tests/dicty.test.cpp
@@ -5,63 +5,92 @@
 using namespace lib;
 using namespace lib::dicty;
 
-TEST_CASE("basic values", "[dicty][vd]")
+TEST_CASE("basic values", "[dicty]")
 {
     Object::ValueDictionary bdictionary{ {"key1", "value1"} };
     CHECK(bdictionary.data() == Object::ValueDictionary::content{ Object::ValueDictionary::element("key1", "value1")});
     CHECK_FALSE(bdictionary.add("key1", "asd",false));
 }
 
-TEST_CASE("dicty create", "[dicty][vd]")
+TEST_CASE("dicty create 1", "[dicty]")
 {
     Object tester{ {"subkey","subValue"} };
     Object obj{
         {"key1", "value1"},
-        {"key2", "value1"}
+        {"key2", "value2"}
     };
 
+    CHECK(tester["subkey"] == "subValue");
+    CHECK(obj["key1"] == "value1");
+    CHECK(obj["key2"] == "value2");
+    CHECK_FALSE(obj["key3"].isValid());
+    CHECK_FALSE(obj["key1"]["value1"].isValid());
+}
 
-
+TEST_CASE("dicty create 2", "[dicty]")
+{
     // Initialize with initializer list of objects
-    Object obj2{
-        { "key1", Object{{
-                "subkey1", "subvalue1"
-            }}
+    Object obj
+    {
+        { "key1",
+            {
+                { "subkey1", "subvalue1" }
+            }
         },
-        { "key2", Object{ {
-                "subkey2", "subvalue2"
-            } }
+        { "key2",
+            {
+                { "subkey2", "subvalue2" }
+            }
         },
-        { "key3", Object{ {
-                Object{ {
-                    "subsubkey1", "subsubvalue"
-                } }
+        { "key3",
+            Object{ {"subkey1",
+                Object {
+                    { "subsubkey1", "subsubvalue" }
+                }
             } }
         }
     };
 
+    CHECK(obj["key1"]["subkey1"] == "subvalue1");
+    CHECK(obj["key2"]["subkey2"] == "subvalue2");
+    CHECK(obj["key3"]["subkey1"]["subsubkey1"] == "subsubvalue");
+}
+
+TEST_CASE("dicty create 3", "[dicty]")
+{
     // Initialize with initializer list of objects and initializer list of
     // properties.
-    Object obj3{
+    Object obj3
+    {
         {
-            { "key1", Object{{
-                    "subkey1", "subvalue1"
-                }}
+            { "key1",
+                {
+                    { "subkey1", "subvalue1" }
+                }
             },
-            { "key2", Object{ {
-                    "subkey2", "subvalue2"
+            { "key2",
+                {
+                    { "subkey2", "subvalue2" }
+                }
+            },
+            { "key3",
+                Object{ {"subkey1",
+                    Object {
+                        { "subsubkey1", "subsubvalue" }
+                    }
                 } }
-            },
-            { "key3", Object{ {
-                    Object{ {
-                        "subsubkey1", "subsubvalue"
-                    } }
-                } }
-            },
+            }
         },
         {
-            {"key4", "value4"}
+            {"key4","value4"}
         }
     };
 
+    CHECK(obj3["key1"]["subkey1"] == "subvalue1");
+    CHECK(obj3["key2"]["subkey2"] == "subvalue2");
+    CHECK(obj3["key3"]["subkey1"]["subsubkey1"] == "subsubvalue");
+    CHECK_FALSE(obj3["key4"]["subkey2"] == "subvalue2");
+    CHECK(obj3["key4"] == "value4");
+    CHECK_FALSE(obj3["adf"].isValid());
+    CHECK_FALSE(obj3["adf"] == "");
 }
