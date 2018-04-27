1b6329a61c250028dfe04bafc2c7f50c1ea24bc0
Ismael González <ismael.gonzalez@zenuity.com>
2018-04-27 12:10:25 +0200

    Added more tests for vector

--- mtypes/tests/vector.test.cpp
+++ mtypes/tests/vector.test.cpp
@@ -1,6 +1,7 @@
 #include "catch.hpp"
 
 #include <mtypes/include/vector.hpp>
+#include <mtypes/include/types.hpp>
 
 using namespace lib;
 
@@ -27,5 +28,41 @@ TEST_CASE("vector constructors", "[vector]")
 	const u32 arr[] = { 4, 3, 2, 1 };
 	vector<u32> v(arr, sizeof(arr) / sizeof(u32));
 	CHECK(v.size() == 4);
-//	CHECK(v == arr);
+}
+
+class A {
+public:
+    s32 b;
+};
+
+inline auto init_vector_shared_pointers_A() {
+    vector_shared_pointers<A> test_vector1;
+    test_vector1.push_back(msptr<A>(A{ 1 }));
+    test_vector1.push_back(msptr<A>(A{ -2 }));
+    test_vector1.push_back(msptr<A>(A{ 3 }));
+    test_vector1.push_back(msptr<A>(A{ -2 }));
+    test_vector1.push_back(msptr<A>(A{ 5 }));
+    test_vector1.push_back(msptr<A>(A{ 100 }));
+    test_vector1.push_back(msptr<A>(A{ -2 }));
+    test_vector1.push_back(msptr<A>(A{ -2 }));
+    test_vector1.push_back(msptr<A>(A{ -1 }));
+    test_vector1.push_back(msptr<A>(A{ 0 }));
+    return test_vector1;
+}
+
+TEST_CASE("vector removes", "[vector]")
+{
+    vector_shared_pointers<A> test_vector1(init_vector_shared_pointers_A());
+    CHECK(test_vector1.size() == 10);
+
+    test_vector1[1] = nullptr;
+    test_vector1[5] = nullptr;
+
+    test_vector1.remove_values(nullptr);
+    CHECK(test_vector1.size() == 8);
+
+    test_vector1.push_back(nullptr);
+    CHECK(test_vector1.size() == 9);
+    test_vector1.remove_values(nullptr);
+    CHECK(test_vector1.size() == 8);
 }
