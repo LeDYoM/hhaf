7f5594bd701a2403db33c8eff94ca8ef4841246e
Ismael González <ismael.gonzalez@zenuity.com>
2018-05-16 00:04:33 +0200

    Added documentation for lockable vector

--- mtypes/mtypes/include/lockablevector.hpp
+++ mtypes/mtypes/include/lockablevector.hpp
@@ -5,23 +5,52 @@
 
 namespace lib
 {
+    /// Vector that must not change during the iteration.
+    /**
+    * This class encapsulates two vectors of T, providing a wrapper
+    * to one of them to modify and doing the update in the other one.
+    * The logic to hide the vectors internally is part of the provided
+    * functionality.
+    */
     template <class T>
     class LockableVector final
     {
     public:
+        /**
+        * Add a new element. Overload for const references.
+        * @param element The new element.
+        */
         constexpr void push_back(const T &element) {
             m_supportContainer.push_back(element);
         }
 
+        /**
+        * Add a new element. Overload for rvalues.
+        * @param element The new element.
+        */
         constexpr void push_back(T &&element) {
             m_supportContainer.push_back(std::move(element));
         }
 
+        /**
+        * Create the new element in place.
+        * @param args Arguments forwarded to the constructor of T.
+        */
         template<typename ...Args>
         constexpr void emplace_back(Args&&... args) {
             m_supportContainer.emplace_back(std::forward<Args>(args)...);
         }
 
+        /**
+        * Update function.
+        * This function must be called by the user with a callable object returning
+        * a bool. It first adds the container avaiable to the user to the internal one,
+        * then process the vector with f. All elements whose f(T) returns false,
+        * will be set to T() and deleted afterwards.
+        * Finally adds the container available to the user in the internal again.
+        * New values will be processed the next call to update.
+        * @param f A callable object receiving a T& as parameter and returning a bool
+        */
         constexpr void update(function<bool(T &)> f) {
             addSupportContainerToMainContainer();
             if (!m_mainContainer.empty()) {
@@ -40,6 +69,7 @@ namespace lib
             }
         }
 
+    private:
         constexpr void addSupportContainerToMainContainer() {
             if (!m_supportContainer.empty()) {
                 m_mainContainer.insert(m_supportContainer);
@@ -47,7 +77,6 @@ namespace lib
             }
         }
 
-    private:
         vector<T> m_mainContainer;
         vector<T> m_supportContainer;
     };
