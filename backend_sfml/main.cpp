b6927665da1ee387a9a6a51203972833ab3bdf15
Ismael Gonzalez <ledyom@hotmail.com>
2019-02-08 17:12:03 +0100

    Remove unnecessary types

--- backend_sfml/main.cpp
+++ backend_sfml/main.cpp
@@ -1,4 +1,3 @@
-#include <lib/include/backend/iwindow.hpp>
 #include <lib/include/backend/interfaces.hpp>
 
 #include "renderwindow.hpp"
@@ -11,68 +10,6 @@
 #else
     #define EXPORT   extern "C"
 #endif
-
-EXPORT lib::backend::IWindowProviderInfo * createWindowProviderInfo() { 
-	return new lib::backend::sfmlb::WindowBackendInfo;
-}
-
-EXPORT bool destroyWindowProviderInfo(lib::backend::IWindowProviderInfo *iwpi) {
-	if (iwpi) {
-		delete iwpi;
-		return true;
-	}
-	return false;
-}
-
-EXPORT lib::backend::IWindow * createWindow() {
-	return new lib::backend::sfmlb::RenderWindow;
-}
-
-EXPORT bool destroyWindow(lib::backend::IWindow *iw) {
-	if (iw) {
-		delete iw;
-		return true;
-	}
-	return false;
-}
-
-
-EXPORT lib::backend::ITTFontFactory * createTTFontFactory() {
-	return new lib::backend::sfmlb::TTFontFactory;
-}
-
-EXPORT bool destroyTTFontFactory(lib::backend::ITTFontFactory *ittff) {
-	if (ittff) {
-		delete ittff;
-		return true;
-	}
-	return false;
-}
-
-
-EXPORT lib::backend::ITextureFactory * createTextureFactory() {
-	return new lib::backend::sfmlb::TextureFactory;
-}
-
-EXPORT bool destroyTextureFactory(lib::backend::ITextureFactory *itf) {
-	if (itf) {
-		delete itf;
-		return true;
-	}
-	return false;
-}
-
-EXPORT lib::backend::IShaderFactory * createShaderFactory() {
-    return new lib::backend::sfmlb::ShaderFactory;
-}
-
-EXPORT bool destroyShaderFactory(lib::backend::IShaderFactory *isf) {
-    if (isf) {
-        delete isf;
-        return true;
-    }
-    return false;
-}
 
 using WindowBackendInfo = lib::backend::DefaultFactoryOf
 	<lib::backend::IWindowProviderInfo,
--- lib/include/backend/interfaces.hpp
+++ lib/include/backend/interfaces.hpp
@@ -6,25 +6,8 @@
 #include "iwindow.hpp"
 #include "iresourcefactories.hpp"
 
-using p_createWindowProviderInfo = lib::backend::IWindowProviderInfo * (*)();
-using p_destroyWindowProviderInfo = bool(*)(lib::backend::IWindowProviderInfo *);
-
-using p_createWindow = lib::backend::IWindow * (*)();
-using p_destroyWindow = bool (*)(lib::backend::IWindow *);
-
-using p_createTTFontFactory = lib::backend::ITTFontFactory * (*)();
-using p_destroyTTFontFactory = bool (*)(lib::backend::ITTFontFactory *);
-
-using p_createTextureFactory = lib::backend::ITextureFactory * (*)();
-using p_destroyTextureFactory = bool (*)(lib::backend::ITextureFactory *);
-
-using p_createShaderFactory = lib::backend::IShaderFactory * (*)();
-using p_destroyShaderFactory = bool(*)(lib::backend::IShaderFactory *);
-
-
 namespace lib::backend
 {
-
 	template <typename T>
 	class IFactoryOf
 	{
