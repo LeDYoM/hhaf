b9cda7085d361dd0af2a969468d350895ba71bca
Ismael Gonzalez <ledyom@hotmail.com>
2019-05-08 20:13:18 +0200

    Code alignment

--- lib/system/inputsystem.hpp
+++ lib/system/inputsystem.hpp
@@ -8,46 +8,43 @@
 
 #include <lib/system/appservice.hpp>
 
-namespace lib
+namespace lib::backend
 {
-    namespace backend
+    class IInputDriver;
+}
+
+ namespace lib::input
+{
+    class InputSystem final : public AppService
     {
-        class IInputDriver;
-    }
+    public:
+        InputSystem(backend::IInputDriver* const input_driver);
+        ~InputSystem() override;
 
-    namespace input
-	{
-        class InputSystem final : public AppService
-        {
-        public:
-            InputSystem(backend::IInputDriver* const input_driver);
-            ~InputSystem() override;
+        void update();
 
-            void update();
+        const vector<Key> &pressedKeys() const noexcept;
+        const vector<Key> &releasedKeys() const noexcept;
+        const KeyStates &keyStates() const noexcept { return m_keyStates; }
 
-            const vector<Key> &pressedKeys() const noexcept;
-            const vector<Key> &releasedKeys() const noexcept;
-            const KeyStates &keyStates() const noexcept { return m_keyStates; }
+        /// Force or simulate a key press.
+        /// @param key @b Key to be added.
+        void pressKey(const Key key);
 
-            /// Force or simulate a key press.
-            /// @param key @b Key to be added.
-            void pressKey(const Key key);
+        /// Force or simulate a key release.
+        /// @param key @b Key to be added.
+        void releaseKey(const Key key);
 
-            /// Force or simulate a key release.
-            /// @param key @b Key to be added.
-            void releaseKey(const Key key);
+    private:
+        void keyPressed(const Key key);
+        void keyReleased(const Key key);
 
-        private:
-			void keyPressed(const Key key);
-			void keyReleased(const Key key);
+        backend::IInputDriver* input_driver_;
+        KeyStates m_keyStates;
+        vector<Key> m_pressedKeys;
+        vector<Key> m_releasedKeys;
 
-			backend::IInputDriver* input_driver_;
-            KeyStates m_keyStates;
-			vector<Key> m_pressedKeys;
-			vector<Key> m_releasedKeys;
-
-		};
-	}
+    };
 }
 
 #endif
