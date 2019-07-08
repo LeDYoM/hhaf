d5832fb3f258fc07881e0837762baab01550ee27
Ismael Gonzalez <ismael.gonzalez@zenuity.com>
2019-07-08 07:22:22 +0200

    Small optimization and refactoring

--- zoper_game/gameplay/direction.hpp
+++ zoper_game/gameplay/direction.hpp
@@ -23,39 +23,36 @@ namespace zoper
             Down = 3,
             Invalid = 4
         };
-        constexpr Direction(DirectionData d) : data{ d } {}
-        constexpr Direction() : Direction{ DirectionData::Up } {}
-        constexpr Direction(u8 d) : data{ static_cast<DirectionData>(d) } {}
-        constexpr Direction(const Direction &other) = default;
-        constexpr Direction &operator=(const Direction &other) = default;
-        constexpr Direction(Direction &&other) = default;
-        constexpr Direction &operator=(Direction &&other) = default;
+        static constexpr u8 Total = static_cast<u8>(DirectionData::Invalid);
+
+        constexpr Direction(DirectionData d) noexcept : data{ d } {}
+        constexpr Direction() noexcept : Direction{ DirectionData::Up } {}
+        constexpr Direction(u8 d) noexcept : Direction{ static_cast<DirectionData>(d) } {}
+        constexpr Direction(const Direction &other) noexcept = default;
+        constexpr Direction &operator=(const Direction &other) noexcept = default;
+        constexpr Direction(Direction &&other) noexcept = default;
+        constexpr Direction &operator=(Direction &&other) noexcept = default;
 
         constexpr operator u8() const noexcept { return static_cast<u8>(data); }
 
         constexpr DirectionData value() const noexcept { return data; }
         constexpr bool isValid() const noexcept { return data < DirectionData::Invalid; }
 
-        static constexpr u8 Total = static_cast<u8>(DirectionData::Invalid);
-
         constexpr bool isHorizontal() const noexcept { return data == DirectionData::Left || data == DirectionData::Right; }
-		constexpr bool isVertical() const noexcept { return data == DirectionData::Up || data == DirectionData::Down; }
+        constexpr bool isVertical() const noexcept { return data == DirectionData::Up || data == DirectionData::Down; }
 
-        constexpr  Direction negate() const noexcept {
+        constexpr  Direction negate() const noexcept
+        {
             switch (data)
             {
             case DirectionData::Left:
                 return DirectionData::Right;
-                break;
             case DirectionData::Right:
                 return DirectionData::Left;
-                break;
             case DirectionData::Up:
                 return DirectionData::Down;
-                break;
             case DirectionData::Down:
                 return DirectionData::Up;
-                break;
             case DirectionData::Invalid:
             default:
                 lib::log_debug_error("Invalid direction. Cannot convert");
@@ -63,7 +60,7 @@ namespace zoper
             return DirectionData::Invalid;
         }
 
-        constexpr vector2dst applyToVector(const vector2dst &v, const u32 scale = 1) const noexcept
+        constexpr vector2dst applyToVector(const vector2dst &v, const u32 scale = 1U) const noexcept
         {
             const vector2ds32 dv{ directionVector(scale) };
             return { v.x + dv.x, v.y + dv.y };
@@ -75,24 +72,20 @@ namespace zoper
             {
             case DirectionData::Left:
                 return { -scale, 0 };
-                break;
             case DirectionData::Right:
                 return { scale, 0 };
-                break;
             case DirectionData::Up:
                 return { 0, -scale };
-                break;
             case DirectionData::Down:
                 return { 0, scale };
-                break;
             case DirectionData::Invalid:
             default:
                 log_debug_error("Invalid direction. Cannot convert");
-                return {};
             }
+            return {};
         }
 
-        constexpr lib::vector2ds32 negatedDirectionVector(const lib::u32 scale = 1) const noexcept
+        constexpr lib::vector2ds32 negatedDirectionVector(const lib::u32 scale = 1U) const noexcept
         {
             return directionVector(scale) * - 1;
         }
@@ -103,22 +96,17 @@ namespace zoper
             {
             case DirectionData::Left:
                 return 180.F;
-                break;
             case DirectionData::Right:
                 return 0.F;
-                break;
             case DirectionData::Up:
                 return 270.F;
-                break;
             case DirectionData::Down:
                 return 90.F;
-                break;
             case DirectionData::Invalid:
             default:
                 lib::log_debug_error("Invalid direction. Cannot convert");
-                return 0.f;
             }
-
+            return 0.f;
         }
 
     private:
