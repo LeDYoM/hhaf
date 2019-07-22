f0933537ff9e0d7e39082dc076a50e42174687b0
Ismael Gonzalez <ismael.gonzalez@zenuity.com>
2019-07-22 07:25:50 +0200

    Tabs per spaces

--- lib/include/scene/iscenenode.hpp
+++ lib/include/scene/iscenenode.hpp
@@ -6,17 +6,17 @@
 #define LIB_ISCENENODE_INTERFACE_INCLUDE_HPP
 
 #define DECLARE_SCENE_NODE_FROM(SceneNodeTypeName, BaseSceneNodeTypeName) SceneNodeTypeName() : BaseSceneNodeTypeName{ lib::str(#SceneNodeTypeName) } {}	\
-	\
-	~SceneNodeTypeName() override {}	\
-	\
-	SIMPLE_DECLARE_SCENE_NODE_FROM(SceneNodeTypeName, BaseSceneNodeTypeName)
+\
+    ~SceneNodeTypeName() override {}    \
+\
+    SIMPLE_DECLARE_SCENE_NODE_FROM(SceneNodeTypeName, BaseSceneNodeTypeName)
 
 #define SIMPLE_DECLARE_SCENE_NODE_FROM(SceneNodeTypeName, BaseSceneNodeTypeName)	static constexpr char StaticTypeName[] = #SceneNodeTypeName;	\
-	\
-		static lib::uptr<BaseSceneNodeTypeName> create()	\
-		{	\
-			return lib::muptr<SceneNodeTypeName>();	\
-		}
+\
+    static lib::uptr<BaseSceneNodeTypeName> create()    \
+    {	\
+        return lib::muptr<SceneNodeTypeName>(); \
+    }
 
 #define DECLARE_SCENE_NODE(SceneNodeTypeName) DECLARE_SCENE_NODE_FROM(SceneNodeTypeName, lib::scene::SceneNode)
 
