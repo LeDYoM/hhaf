aeea3f485f8e383640463b07087a3495222135a5
Ismael Gonzalez <ismael.gonzalez@zenuity.com>
2019-07-29 10:31:38 +0200

    Adding IBMPFont. No visible changes.

--- backend_dev/backend_dev/include/iresourcefactories.hpp
+++ backend_dev/backend_dev/include/iresourcefactories.hpp
@@ -9,12 +9,14 @@
 #include "itexture.hpp"
 #include "ittfont.hpp"
 #include "ishader.hpp"
+#include "ibmpfont.hpp"
 
 namespace lib::backend
 {
     using ITextureFactory = IResourceFactory<ITexture>;
     using ITTFontFactory = IResourceFactory<ITTFont>;
     using IShaderFactory = IResourceFactory<IShader>;
+    using IBMPFontFactory = IResourceFactory<IBMPFont>;
 }
 
 #endif
