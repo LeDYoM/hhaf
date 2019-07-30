b28e7432d3227f6cc48fe4d2e56e2f27b209e19a
Ismael Gonzalez <ismael.gonzalez@zenuity.com>
2019-07-30 06:50:20 +0200

    Tabs per spaces

--- zoper_game/menu/menupage.hpp
+++ zoper_game/menu/menupage.hpp
@@ -14,54 +14,55 @@
 
 namespace zoper
 {
-	using namespace lib;
-	using namespace lib::scene;
+    using namespace lib;
+    using namespace lib::scene;
 
-	enum class MenuPageMode {
-		Selector,
-		Optioner
-	};
+    enum class MenuPageMode
+    {
+        Selector,
+        Optioner
+    };
 
     class MenuPage : public nodes::TableNode<nodes::SceneNodeText>
-	{
-	private:
+    {
+    private:
         using BaseClass = nodes::TableNode<nodes::SceneNodeText>;
         using ContainedElement = BaseClass::ContainedElement;
-	public:
-		MenuPage(SceneNode *parent, str name);
-		virtual ~MenuPage();
+    public:
+        MenuPage(SceneNode *parent, str name);
+        virtual ~MenuPage();
 
-		void configure(MenuPageMode pageMode, const string_vector &titles, const vector<string_vector> options = {});
-		void onCreated() override;
+        void configure(MenuPageMode pageMode, const string_vector &titles, const vector<string_vector> options = {});
+        void onCreated() override;
 
-		size_type SelectedOptionAtRow(const size_type row) const;
-		emitter<const MenuPageType> Forward;
-		emitter<> Back;
+        size_type SelectedOptionAtRow(const size_type row) const;
+        emitter<const MenuPageType> Forward;
+        emitter<> Back;
 
-	protected:
-		emitter<const size_type> Selection;
-	private:
+    protected:
+        emitter<const size_type> Selection;
+    private:
         void standarizeText(const sptr<ContainedElement> &ntext);
-		void goDown();
-		void goUp();
-		void goLeft();
-		void goRight();
-		void goSelected();
+        void goDown();
+        void goUp();
+        void goLeft();
+        void goRight();
+        void goSelected();
 
-		bool nodeHasOptions(const size_type y) const noexcept;
-		sptr<DiscreteTextComponent> optionsLabelAt(const size_type y);
-		void setSelectedItem(const size_type index);
-		void updateSelection();
-		void setColorToLine(const size_type, const scene::Color&);
-		size_type m_previouslySelectedItem{ 0 };
-		size_type m_selectedItem{ 0 };
+        bool nodeHasOptions(const size_type y) const noexcept;
+        sptr<DiscreteTextComponent> optionsLabelAt(const size_type y);
+        void setSelectedItem(const size_type index);
+        void updateSelection();
+        void setColorToLine(const size_type, const scene::Color&);
+        size_type m_previouslySelectedItem{ 0 };
+        size_type m_selectedItem{ 0 };
 
         sptr<IFont> m_normalFont;
-		Color m_normalColor;
-		Color m_selectedColor;
-		ireceiver m_receiver;
-		MenuPageMode m_pageMode;
-	};
+        Color m_normalColor;
+        Color m_selectedColor;
+        ireceiver m_receiver;
+        MenuPageMode m_pageMode;
+    };
 }
 
 #endif
