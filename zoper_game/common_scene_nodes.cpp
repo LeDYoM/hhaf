06933fda3c1bf5421c02dcc170f27e86c7aed708
Ismael González <ledyom@hotmail.com>
2019-12-17 07:21:08 +0100

    Extracted background creation to common_scene_nodes

---
+++ zoper_game/common_scene_nodes.cpp
@@ -0,0 +1,31 @@
+#include <mtypes/include/types.hpp>
+#include <mtypes/include/properties.hpp>
+
+#include <lib/include/liblog.hpp>
+#include <lib/scene/renderizables/renderizable.hpp>
+#include <lib/scene/components/renderizables.hpp>
+#include <lib/scene/scenenode.hpp>
+
+namespace zoper
+{
+using namespace lib;
+using namespace lib::scene;
+
+void createStandardBackground(
+    const sptr<lib::scene::Renderizables> &renderizables)
+{
+    auto background = renderizables->createNode("background");
+    background->figType.set(FigType_t::Quad);
+    background->pointCount.set(6U);
+    background->box = rectFromSize(2000.0f, 2000.0f);
+    background->color = colors::White;
+    background->color_modifier = [](const RenderizableModifierContext &context) {
+        const auto n = context.normalizedVertexInBox();
+        static constexpr auto decrease_ratio = 0.5F;
+        return Color::fromFloats(
+            n.y * decrease_ratio, n.y * decrease_ratio, n.y * decrease_ratio);
+    };
+
+    //        background->shader = resources_viewer->getShader(MainMenuResources::Shader);
+}
+} // namespace zoper
---
+++ zoper_game/common_scene_nodes.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <lib/scene/components/renderizables.hpp>
+
+namespace zoper
+{
+    void createStandardBackground(
+        const sptr<lib::scene::Renderizables>& renderizables);
+}
