41f8d5e3ef92612fc3858b176869a1294b7cce1c
Ismael González <ledyom@hotmail.com>
2020-01-03 10:10:32 +0100

    Tabs per spaces

--- lib/scene/scenenode.hpp
+++ lib/scene/scenenode.hpp
@@ -15,116 +15,115 @@
 
 namespace lib::scene
 {
-    class Renderizable;
-    class IComponent;
-    class Scene;
-    class SceneManager;
+class Renderizable;
+class IComponent;
+class Scene;
+class SceneManager;
 
-    /** \brief Main class representing all SceneNodes from a Scene.
+/** \brief Main class representing all SceneNodes from a Scene.
     * This class serves as main entry point in the hierarchy of the scene.
     * To create new SceneNode types, please, inherit from this class.
     */
-    class SceneNode : public core::HasName, public Transformable, 
-        public DataWrapperCreator, public ComponentContainer, public SceneNodeBlob
-    {
-    public:
-        SceneNode(const SceneNode&) = delete;
-        SceneNode &operator=(const SceneNode&) = delete;
+class SceneNode : public core::HasName, public Transformable, public DataWrapperCreator, public ComponentContainer, public SceneNodeBlob
+{
+public:
+    SceneNode(const SceneNode &) = delete;
+    SceneNode &operator=(const SceneNode &) = delete;
 
-        SceneNode(SceneNode *const parent, str name);
-        virtual ~SceneNode();
+    SceneNode(SceneNode *const parent, str name);
+    virtual ~SceneNode();
 
-        /**
+    /**
         * Method called when adding a new node just after creation.
         * Override it to add code on creation
         * @see configure
         */
-        virtual void onCreated() {}
+    virtual void onCreated() {}
 
-        /**
+    /**
         * This method is called just before completing the initialization
         */
-        virtual void configure() {}
+    virtual void configure() {}
 
-        template <typename T = SceneNode, typename... Args>
-        sptr<T> createSceneNode(Args&&... args)
-        {
-            auto result(msptr<T>(this, std::forward<Args>(args)...));
-            addSceneNode(result);
-            return result;
-        }
+    template <typename T = SceneNode, typename... Args>
+    sptr<T> createSceneNode(Args &&... args)
+    {
+        auto result(msptr<T>(this, std::forward<Args>(args)...));
+        addSceneNode(result);
+        return result;
+    }
 
-        sptr<SceneNode> createSceneNode(str name)
-        {
-            return createSceneNode<SceneNode>(std::move(name));
-        }
+    sptr<SceneNode> createSceneNode(str name)
+    {
+        return createSceneNode<SceneNode>(std::move(name));
+    }
 
-        bool moveLastBeforeNode(const sptr<SceneNode> &beforeNode);
-        void removeSceneNode(sptr<SceneNode> element);
-        void clearAll();
-        void clearSceneNodes();
+    bool moveLastBeforeNode(const sptr<SceneNode> &beforeNode);
+    void removeSceneNode(sptr<SceneNode> element);
+    void clearAll();
+    void clearSceneNodes();
 
-        void render(bool parentTransformationChanged);
-        virtual void update() {}
+    void render(bool parentTransformationChanged);
+    virtual void update() {}
 
-        virtual Scene *const parentScene() noexcept
-        { 
-            return m_parent->parentScene();
-        }
-        
-        virtual const Scene *const parentScene() const noexcept
-        { 
-            return m_parent->parentScene();
-        }
+    virtual Scene *const parentScene() noexcept
+    {
+        return m_parent->parentScene();
+    }
 
-        template <typename SceneType>
-        SceneType *const parentSceneAs()
-        {
-            return dynamic_cast<SceneType*>(parentScene());
-        }
+    virtual const Scene *const parentScene() const noexcept
+    {
+        return m_parent->parentScene();
+    }
 
-        template <typename SceneType>
-        const SceneType *const parentSceneAs() const
-        { 
-            return dynamic_cast<const SceneType*>(parentScene());
-        }
+    template <typename SceneType>
+    SceneType *const parentSceneAs()
+    {
+        return dynamic_cast<SceneType *>(parentScene());
+    }
 
-        SceneNode *parent() noexcept { return m_parent; }
-        const SceneNode *parent() const noexcept { return m_parent; }
+    template <typename SceneType>
+    const SceneType *const parentSceneAs() const
+    {
+        return dynamic_cast<const SceneType *>(parentScene());
+    }
 
-        template <typename SceneNodeType>
-        SceneNodeType *parentAs()
-        { 
-            return dynamic_cast<SceneNodeType*>(parent());
-        }
+    SceneNode *parent() noexcept { return m_parent; }
+    const SceneNode *parent() const noexcept { return m_parent; }
 
-        template <typename SceneNodeType>
-        const SceneNodeType *parentAs() const
-        { 
-            return dynamic_cast<const SceneNodeType*>(parent());
-        }
+    template <typename SceneNodeType>
+    SceneNodeType *parentAs()
+    {
+        return dynamic_cast<SceneNodeType *>(parent());
+    }
 
-        template <typename T>
-        constexpr T *const snCast() { return dynamic_cast<T *const>(this); }
+    template <typename SceneNodeType>
+    const SceneNodeType *parentAs() const
+    {
+        return dynamic_cast<const SceneNodeType *>(parent());
+    }
 
-        template <typename T>
-        constexpr const T *const snCast() const { return dynamic_cast<const T *const>(this); }
+    template <typename T>
+    constexpr T *const snCast() { return dynamic_cast<T *const>(this); }
 
-        BasicProperty<bool> visible;
+    template <typename T>
+    constexpr const T *const snCast() const { return dynamic_cast<const T *const>(this); }
 
-        constexpr const auto &sceneNodes() const noexcept { return m_groups; }
-        constexpr auto &sceneNodes() noexcept { return m_groups; }
-        constexpr auto sceneNodesSize() const noexcept { return sceneNodes().size(); }
+    BasicProperty<bool> visible;
 
-    protected:
-        void addSceneNode(sptr<SceneNode> node);
+    constexpr const auto &sceneNodes() const noexcept { return m_groups; }
+    constexpr auto &sceneNodes() noexcept { return m_groups; }
+    constexpr auto sceneNodesSize() const noexcept { return sceneNodes().size(); }
 
-    private:
-        SceneNode *m_parent;
-        vector<sptr<SceneNode>> m_groups;
-    };
+protected:
+    void addSceneNode(sptr<SceneNode> node);
 
-    using SceneNodeSPtr = sptr<SceneNode>;
-}
+private:
+    SceneNode *m_parent;
+    vector<sptr<SceneNode>> m_groups;
+};
+
+using SceneNodeSPtr = sptr<SceneNode>;
+} // namespace lib::scene
 
 #endif
