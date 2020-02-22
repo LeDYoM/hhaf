2ae74e15aec1168439cb9cd63356c5ed6cf06f99
Ismael Gonzalez Burgos <ismael.gonzalez@zenuity.com>
2020-02-22 19:29:21 +0100

    Improved comment

--- lib/time/include/timeview.hpp
+++ lib/time/include/timeview.hpp
@@ -3,16 +3,22 @@
 #ifndef LIB_TIME_VIEW_DATA_WRAPPER_INCLUDE_HPP
 #define LIB_TIME_VIEW_DATA_WRAPPER_INCLUDE_HPP
 
-#include <mtypes/include/types.hpp>
 #include <lib/scene/include/idatawrapper.hpp>
 #include <lib/time/include/timepoint.hpp>
 
 namespace lib::time
 {
-/// Component to provide access to resources.
+/**
+ * @brief @b IDataWrapper to provide access to the current time
+ */
 class TimeView : public scene::IDataWrapper
 {
 public:
+    /**
+     * @brief Get the current time.
+     * 
+     * @return TimePoint containing the current time
+     */
     TimePoint now() const;
 };
 } // namespace lib::time
