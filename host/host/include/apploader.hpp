85cdf399d6e737e9c040e334d31f5385ed7c9667
Ismael Gonzalez Burgos <ismael.gonzalez@zenuity.com>
2020-03-23 08:58:41 +0100

    Apply format to apploader

--- host/host/include/apploader.hpp
+++ host/host/include/apploader.hpp
@@ -6,29 +6,30 @@
 #include "hosted_app/include/iapp.hpp"
 #include <mtypes/include/types.hpp>
 #include <mtypes/include/function.hpp>
+#include <mtypes/include/str.hpp>
 
 namespace lib::sys
 {
-    struct ManagedApp
-    {
-        IApp *app{nullptr};
-        p_initApp init_app{nullptr};
-        p_finishApp finish_app{nullptr};
-    };
+struct ManagedApp
+{
+    rptr<IApp> app{nullptr};
+    p_initApp init_app{nullptr};
+    p_finishApp finish_app{nullptr};
+};
 
-    /// Class to perform a load of an app in memory.
-    /// This class does not manage the memory of the
-    /// loaded app or initializes it.
-    class AppLoader final
-    {
-    public:
-        /// Method to load an app.
-        /// @return Instance of the loaded app or nullptr
-        ///     in case of failure.
-        ManagedApp loadApp(const str& file) const;
-        ManagedApp loadApp(p_initApp init_app, p_finishApp finish_app) const;
-        bool unloadApp(ManagedApp& managed_app) const;
-    };
-}
+/// Class to perform a load of an app in memory.
+/// This class does not manage the memory of the
+/// loaded app or initializes it.
+class AppLoader final
+{
+public:
+    /// Method to load an app.
+    /// @return Instance of the loaded app or nullptr
+    ///     in case of failure.
+    ManagedApp loadApp(const str &file) const;
+    ManagedApp loadApp(p_initApp init_app, p_finishApp finish_app) const;
+    bool unloadApp(ManagedApp &managed_app) const;
+};
+} // namespace lib::sys
 
 #endif
