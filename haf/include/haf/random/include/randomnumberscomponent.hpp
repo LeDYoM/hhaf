2866e67f76beb3ad78a02afe05edc88aeed01b4f
Ismael Gonzalez Burgos <ismael.gonzalez@zenuity.com>
2020-05-12 23:28:39 +0200

    Fix include guard

--- haf/include/haf/random/include/randomnumberscomponent.hpp
+++ haf/include/haf/random/include/randomnumberscomponent.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#ifndef HAF_DATA_PROVIDER_COMPONENT_INCLUDE_HPP
-#define HAF_DATA_PROVIDER_COMPONENT_INCLUDE_HPP
+#ifndef HAF_DATA_PROVIDER_RANDOM_NUMBERS_COMPONENT_INCLUDE_HPP
+#define HAF_DATA_PROVIDER_RANDOM_NUMBERS_COMPONENT_INCLUDE_HPP
 
 #include <mtypes/include/types.hpp>
 #include <mtypes/include/str.hpp>
@@ -11,17 +11,18 @@
 namespace haf::rnd
 {
 /**
-* @brief This component class is intended to be used to generate
-* a series of random numbers.
-*/
+ * @brief This component class is intended to be used to generate
+ * a series of random numbers.
+ */
 class RandomNumbersComponent final : public scene::IComponent
 {
 public:
     RandomNumbersComponent() noexcept;
     ~RandomNumbersComponent() override;
-    mtps::u32 getUInt(const mtps::size_type max, const mtps::size_type min = 0U) const;
+    mtps::u32 getUInt(const mtps::size_type max,
+                      const mtps::size_type min = 0U) const;
     void setName(mtps::str new_name);
-    const mtps::str&name() const noexcept;
+    const mtps::str& name() const noexcept;
 
     void onAttached() override;
 
@@ -29,6 +30,6 @@ private:
     class RandomNumbersComponentPrivate;
     mtps::uptr<RandomNumbersComponentPrivate> priv_;
 };
-} // namespace haf::rnd
+}  // namespace haf::rnd
 
 #endif
