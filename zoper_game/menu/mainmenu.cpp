9ca75765946daf72d304f3da0d51a5153caa7c06
Ismael Gonzalez Burgos <ismael.gonzalez@zenuity.com>
2020-05-27 20:48:32 +0200

    Small optimizations

--- zoper_game/menu/mainmenu.cpp
+++ zoper_game/menu/mainmenu.cpp
@@ -34,48 +34,43 @@ constexpr auto to_str(Antialiasing aa)
 {
     switch (aa)
     {
-    case Antialiasing::Worst:
-        return "Worst";
-        break;
-    case Antialiasing::Bad:
-        return "Bad";
-        break;
-    case Antialiasing::Normal:
-        return "Normal";
-        break;
-    case Antialiasing::Good:
-        return "Good";
-        break;
-    case Antialiasing::Best:
-        return "Best";
-        break;
-    default:
-        return "";
+        case Antialiasing::Worst:
+            return "Worst";
+            break;
+        case Antialiasing::Bad:
+            return "Bad";
+            break;
+        case Antialiasing::Normal:
+            return "Normal";
+            break;
+        case Antialiasing::Good:
+            return "Good";
+            break;
+        case Antialiasing::Best:
+            return "Best";
+            break;
+        default:
+            return "";
     }
 }
 
-void goGame(MenuPaged *scene_node,
+void goGame(rptr<MenuPaged> scene_node,
             const GameMode game_mode,
             vector<s32> menu_data)
 {
     {
-        auto game_shared_data_view = scene_node->dataWrapper<shdata::SharedDataUpdater>();
-        auto& game_shared_data = game_shared_data_view->dataAs<GameSharedData>();
+        auto game_shared_data_view =
+            scene_node->dataWrapper<shdata::SharedDataUpdater>();
+        auto& game_shared_data =
+            game_shared_data_view->dataAs<GameSharedData>();
 
         game_shared_data.startLevel = menu_data[0U];
-        game_shared_data.gameMode = game_mode;
+        game_shared_data.gameMode   = game_mode;
         DisplayLog::info(game_shared_data.to_str());
     }
 
     scene_node->terminate(1);
 }
-
-MainMenu::MainMenu(SceneNode *parent, str name)
-    : BaseClass{parent, std::move(name)}
-{
-}
-
-MainMenu::~MainMenu() = default;
 
 void MainMenu::onCreated()
 {
@@ -83,9 +78,11 @@ void MainMenu::onCreated()
 
     auto resources_viewer = dataWrapper<ResourceView>();
 
-    auto normalFont = resources_viewer->getTTFont(MainMenuResources::MenuFontId)->font(72);
+    auto normalFont =
+        resources_viewer->getTTFont(MainMenuResources::MenuFontId)->font(72);
 #ifdef TEST_BMP_FONT
-    auto normalFont = resources_viewer->getBMPFont(MainMenuResources::TestFontId);
+    auto normalFont =
+        resources_viewer->getBMPFont(MainMenuResources::TestFontId);
 #endif
 
     setNormalTextFont(normalFont);
@@ -103,25 +100,23 @@ void MainMenu::onCreated()
 
     // Create and register menu pages
     PageOptions main_page_options{true};
-    auto menuPageMain(
-        createAndConfigureMenuPage(
-            "menuPageMain",
-            vector_shared_pointers<MenuPagedOption>{
-                make_option("Play Token mode", RangeOption(), 1),
-                make_option("Play Time mode", RangeOption(), 2),
-                make_option("Options", RangeOption(), 3),
-                make_option("Exit", RangeOption(), MenuPagedOption::GoBack)},
-            main_page_options));
+    auto menuPageMain(createAndConfigureMenuPage(
+        "menuPageMain",
+        vector_shared_pointers<MenuPagedOption>{
+            make_option("Play Token mode", RangeOption(), 1),
+            make_option("Play Time mode", RangeOption(), 2),
+            make_option("Options", RangeOption(), 3),
+            make_option("Exit", RangeOption(), MenuPagedOption::GoBack)},
+        main_page_options));
 
     menu_steps.push_back(menuPageMain);
 
-    auto menuPageByToken(
-        createAndConfigureMenuPage(
-            "menuPageByToken",
-            vector_shared_pointers<MenuPagedOption>{
-                make_option("Start level Token Mode", RangeOption(1U, 10U)),
-                make_option("Play", RangeOption(), MenuPagedOption::Accept),
-                make_option("Back", RangeOption(), MenuPagedOption::GoBack)}));
+    auto menuPageByToken(createAndConfigureMenuPage(
+        "menuPageByToken",
+        vector_shared_pointers<MenuPagedOption>{
+            make_option("Start level Token Mode", RangeOption(1U, 10U)),
+            make_option("Play", RangeOption(), MenuPagedOption::Accept),
+            make_option("Back", RangeOption(), MenuPagedOption::GoBack)}));
 
     menu_steps.push_back(menuPageByToken);
 
@@ -129,13 +124,12 @@ void MainMenu::onCreated()
         goGame(this, GameMode::Token, std::move(menu_data));
     });
 
-    auto menuPageByTime(
-        createAndConfigureMenuPage(
-            "menuPageByTime",
-            vector_shared_pointers<MenuPagedOption>{
-                make_option("Start level Time Mode", RangeOption(1U, 10U)),
-                make_option("Play", RangeOption(), MenuPagedOption::Accept),
-                make_option("Back", RangeOption(), MenuPagedOption::GoBack)}));
+    auto menuPageByTime(createAndConfigureMenuPage(
+        "menuPageByTime",
+        vector_shared_pointers<MenuPagedOption>{
+            make_option("Start level Time Mode", RangeOption(1U, 10U)),
+            make_option("Play", RangeOption(), MenuPagedOption::Accept),
+            make_option("Back", RangeOption(), MenuPagedOption::GoBack)}));
 
     menu_steps.push_back(menuPageByTime);
 
@@ -147,7 +141,9 @@ void MainMenu::onCreated()
         "menuPageOptions",
         vector_shared_pointers<MenuPagedOption>{
             make_option("Antialiasing", RangeOption{Antialiasing{}}),
-            make_option("Resolution", RangeOption(string_vector{"Worst", "Bad", "Normal", "Good", "Best"})),
+            make_option("Resolution",
+                        RangeOption(string_vector{"Worst", "Bad", "Normal",
+                                                  "Good", "Best"})),
             make_option("Fullscreen", RangeOption(string_vector{"No", "Yes"})),
             make_option("VSync", RangeOption(string_vector{"No", "Yes"})),
             make_option("Redefine keyboard", RangeOption()),
@@ -159,4 +155,4 @@ void MainMenu::onCreated()
     configure_menu(std::move(menu_steps));
 }
 
-} // namespace zoper
+}  // namespace zoper
