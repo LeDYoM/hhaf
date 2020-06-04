e8ccf7a080c7ca235036bd44463ad33fc791bb4e
Ismael Gonzalez Burgos <ismael.gonzalez@zenuity.com>
2020-06-04 12:40:36 +0200

    Preparing documentation script for mtypes

--- mtypes/mtypes/include/lockablevector.hpp
+++ mtypes/mtypes/include/lockablevector.hpp
@@ -9,62 +9,79 @@
 
 namespace mtps
 {
-/// This class encapsulates the functionality of a vector with deferred operations.
-/// This class encapsulates 3 vectors<T>, providing a wrapper
-/// to add and remove elements without modifying the main vector.
-/// To access to the modifications, the user must call one of the available
-/// methods to do that. The user may also access the non updated data.
-/// The logic to hide the vectors internally is part of the provided
-/// functionality.
+/**
+ * @brief This class encapsulates the functionality of a vector with deferred
+ * operations. to add and remove elements without modifying the main vector.
+ * This class encapsulates 3 vectors<T>, providing a wrapper to access to the
+ * modifications, the user must call one of the available methods to do that.
+ * The user may also access the non updated data. The logic to hide the vectors
+ * internally is part of the provided functionality.
+ * @tparam T
+ */
 template <class T>
 class LockableVector final
 {
 public:
-    using iterator = typename vector<T>::iterator;
-    using const_iterator = typename vector<T>::const_iterator;
-    using reference = typename vector<T>::reference;
+    using iterator        = typename vector<T>::iterator;
+    using const_iterator  = typename vector<T>::const_iterator;
+    using reference       = typename vector<T>::reference;
     using const_reference = typename vector<T>::const_reference;
-    using value_type = typename vector<T>::value_type;
-    using pointer = typename vector<T>::pointer;
+    using value_type      = typename vector<T>::value_type;
+    using pointer         = typename vector<T>::pointer;
 
-    /// Add a new element. Overload for const references.
-    /// @param element The new element.
-    constexpr void push_back(const T &element)
+    /**
+     * @brief Add a new element. Overload for const references.
+     * @param element The new element.
+     */
+    constexpr void push_back(const T& element)
     {
         addingCache_.push_back(element);
     }
 
-    /// Add a new element. Overload for rvalues.
-    /// @param element The new element.
-    constexpr void push_back(T &&element)
+    /**
+     * @brief Add a new element. Overload for rvalues.
+     * @param element The new element.
+     */
+    constexpr void push_back(T&& element)
     {
         addingCache_.push_back(std::move(element));
     }
 
-    /// Create the new element in place.
-    /// @param args Arguments forwarded to the constructor of T.
+    /**
+     * @brief Create the new element in place.
+     *
+     * @tparam Args Types of the arguments
+     * @param args Arguments forwarded to the constructor of T.
+     */
     template <typename... Args>
-    constexpr void emplace_back(Args &&... args)
+    constexpr void emplace_back(Args&&... args)
     {
         addingCache_.emplace_back(std::forward<Args>(args)...);
     }
 
-    /// Remove an element. Overload for const references.
-    /// @param element The element to remove.
-    constexpr void erase_value(const T &element)
+    /**
+     * @brief Remove an element. Overload for const references.
+     * @param element The element to remove.
+     */
+    constexpr void erase_value(const T& element)
     {
         remove_cache_.push_back(element);
     }
 
-    /// Remove an element. Overload for rvalues
-    /// @param element The element to remove.
-    constexpr void erase_values(T &&element)
+    /**
+     * @brief Remove an element. Overload for rvalues
+     * @param element The element to remove.
+     */
+    constexpr void erase_values(T&& element)
     {
         remove_cache_.push_back(std::move(element));
     }
 
-    /// Explicitly call this method to force an update
-    /// (adding and removing values) to the container.
+    /**
+     * @brief Update method
+     * Explicitly call this method to force an update (adding and removing
+     * values) to the container.
+     */
     constexpr void update()
     {
         add_to_main_container();
@@ -77,7 +94,10 @@ public:
         update();
     }
 
-    /// Clear all containers, no pending adds or removes after that.
+    /**
+     * @brief Clear all containers, no elements, no pending adds and no pending
+     * removes after that.
+     */
     constexpr void clear()
     {
         main_container_.clear();
@@ -85,79 +105,99 @@ public:
         remove_cache_.clear();
     }
 
-    /// Retrieve a reference to the internal main container.
-    /// @return lvalue reference to the main container.
-    constexpr const vector<T> &deferred_current() const noexcept
+    /**
+     * @brief Retrieve a reference to the internal main container.
+     * @return const vector<T>& lvalue reference to the main container.
+     */
+    constexpr const vector<T>& deferred_current() const noexcept
     {
         return main_container_;
     }
 
-    /// Get a copy
+    /**
+     * @brief Get a copy
+     * @return constexpr vector<T>
+     */
     constexpr vector<T> next() const
     {
         LockableVector<T> temp(*this);
         return temp.current();
     }
 
-    /// Retrieve a constant reference to the updated internal main container.
-    /// @return const lvalue reference to the updated main container.
-    constexpr const vector<T> &current() noexcept
+    /**
+     * @brief Retrieve a constant reference to the updated internal main
+     * container.
+     * @return constexpr const vector<T>& const lvalue reference to the updated
+     * main container.
+     */
+    constexpr const vector<T>& current() noexcept
     {
         update();
         return main_container_;
     }
 
-    /// Retrieve the number of pending elements to be added.
-    /// @return The number of elements.
-    constexpr size_type pending_add() noexcept
+    /**
+     * @brief Retrieve the number of pending elements to be added.
+     * @return constexpr size_type The number of elements.
+     */
+    constexpr size_type pending_add() const noexcept
     {
         return addingCache_.size();
     }
 
-    /// Ask if there is any number of elements pending to be added.
-    /// @return true or false
-    constexpr bool are_pending_adds() noexcept
+    /**
+     * @brief Ask if there is any number of elements pending to be added.
+     * @return true
+     * @return false
+     */
+    constexpr bool are_pending_adds() const noexcept
     {
         return !addingCache_.empty();
     }
 
-    /// Retrieve the number of pending elements to be added.
-    /// @return The number of elements.
-    constexpr auto pending_remove() noexcept
+    /**
+     * @brief Retrieve the number of pending elements to be added.
+     * @return constexpr auto The number of elements.
+     */
+    constexpr auto pending_remove() const noexcept
     {
         return remove_cache_.size();
     }
 
-    /// Ask if there is any number of elements pending to be removed.
-    /// @return true or false
-    constexpr bool are_pending_removes() noexcept
+    /**
+     * @brief Ask if there is any number of elements pending to be removed.
+     * @return true
+     * @return false
+     */
+    constexpr bool are_pending_removes() const noexcept
     {
         return !remove_cache_.empty();
     }
 
-    /// Automatically perform an update of the elements of the current
-    /// container using a functor. The functor will be called once per
-    /// element in the current container after updating the pending elements
-    /// to add and remove.
-    /// @tparam F Type of the functor passed as parameter
-    /// @param[in] f Functor to be used for each element
+    /**
+     * @brief Automatically perform an update of the elements of the current
+     * container using a functor. The functor will be called once per element in
+     * the current container after updating the pending elements to add and
+     * remove.
+     *
+     * @tparam F Forwarding reference of type of the functor passed as parameter
+     * @param f Functor to be used for each element
+     */
     template <typename F>
     void performUpdate(F&& f)
     {
         update();
 
-        for_each_all(main_container_, [f](auto &element) {
-            f(element);
-        });
+        for_each_all(main_container_, [f](auto& element) { f(element); });
 
         update();
     }
 
 private:
     template <typename Container>
-    constexpr void remove_cache_elements_from(Container &c)
+    constexpr void remove_cache_elements_from(Container& c)
     {
-        for (const auto &element : remove_cache_)
+        for (const auto& element : remove_cache_)
         {
             c.erase_one(element);
         }
@@ -165,7 +205,7 @@ private:
 
     constexpr void remove_from_containers()
     {
-        if (!remove_cache_.empty())
+        if (are_pending_removes())
         {
             remove_cache_elements_from(main_container_);
             remove_cache_.clear();
@@ -174,7 +214,7 @@ private:
 
     constexpr void add_to_main_container()
     {
-        if (!addingCache_.empty())
+        if (are_pending_adds())
         {
             main_container_.insert(addingCache_);
             addingCache_.clear();
@@ -185,6 +225,6 @@ private:
     vector<T> addingCache_;
     vector<T> remove_cache_;
 };
-} // namespace mtps
+}  // namespace mtps
 
 #endif
