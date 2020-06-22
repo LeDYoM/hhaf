dba219eaa689b034d758e8cbee4530a1ab3d2c40
LeDYoM <ledyom@hotmail.com>
2020-06-22 12:15:12 +0200

    Removed createRenderizable. now builder

--- haf/include/haf/scene/include/renderizables.hpp
+++ haf/include/haf/scene/include/renderizables.hpp
@@ -19,8 +19,9 @@ extern template mtps::vector<mtps::sptr<Renderizable>>;
 class Renderizables
 {
 public:
-    Renderizables(mtps::rptr<SceneNode> scene_node) noexcept
-        : scene_node_{std::move(scene_node)} {}
+    Renderizables(mtps::rptr<SceneNode> scene_node) noexcept :
+        scene_node_{std::move(scene_node)}
+    {}
 
     /// Method to create a Renderizable
     /// @param name The name of the Renderizable node.
@@ -29,61 +30,24 @@ public:
     /// @param color Color of the Renderizable.
     /// @param num_points Number of points of the Renderizable.
     /// @returns The created Renderizable
-    mtps::sptr<Renderizable> createRenderizable(
-        mtps::str name, FigType_t figure_type, mtps::Rectf32 box, Color color,
-            mtps::sptr<ITexture> texture, mtps::sptr<IShader> shader, mtps::size_type num_points);
-
-    mtps::sptr<Renderizable> createRenderizable(mtps::str name, FigType_t figure_type)
-    {
-        return createRenderizable(std::move(name), std::move(figure_type),
-        mtps::Rectf32{}, colors::White, nullptr, nullptr, 4U);
-    }
-
-    mtps::sptr<Renderizable> createRenderizable(mtps::str name, FigType_t figure_type,
-        mtps::size_type num_points)
-    {
-        return createRenderizable(std::move(name), std::move(figure_type),
-        mtps::Rectf32{}, colors::White, nullptr, nullptr, std::move(num_points));
-    }
-
-    mtps::sptr<Renderizable> createRenderizable(mtps::str name, FigType_t figure_type,
-        mtps::Rectf32 box, Color color)
-    {
-        return createRenderizable(std::move(name), std::move(figure_type),
-        std::move(box), std::move(color), nullptr, nullptr, 4U);
-    }
-
-    mtps::sptr<Renderizable> createRenderizable(mtps::str name, FigType_t figure_type,
-        mtps::Rectf32 box, mtps::sptr<ITexture> texture)
-    {
-        return createRenderizable(std::move(name), std::move(figure_type),
-        std::move(box), colors::White, std::move(texture), nullptr, 4U);
-    }
-
-    mtps::sptr<Renderizable> createRenderizable(mtps::str name, FigType_t figure_type,
-        mtps::Rectf32 box)
-    {
-        return createRenderizable(std::move(name), std::move(figure_type),
-        std::move(box), nullptr);
-    }
-
-    mtps::sptr<Renderizable> createRenderizable(mtps::str name, FigType_t figure_type,
-        mtps::Rectf32 box, Color color, mtps::size_type num_points)
-    {
-        return createRenderizable(std::move(name), std::move(figure_type),
-        std::move(box), std::move(color), nullptr, nullptr, 
-        std::move(num_points));
-    }
+    mtps::sptr<Renderizable> createRenderizable(mtps::str name,
+                                                FigType_t figure_type,
+                                                mtps::Rectf32 box,
+                                                Color color,
+                                                mtps::sptr<ITexture> texture,
+                                                mtps::sptr<IShader> shader,
+                                                mtps::size_type num_points);
 
     RenderizableBuilder renderizableBuilder();
 
-    void removeRenderizable(const mtps::sptr<Renderizable> &element);
+    void removeRenderizable(const mtps::sptr<Renderizable>& element);
     void clearRenderizables();
 
     template <typename T>
-    constexpr void for_each_node_as(mtps::function<void(const mtps::sptr<T> &)> action)
+    constexpr void for_each_node_as(
+        mtps::function<void(const mtps::sptr<T>&)> action)
     {
-        for_each_node([&action](const mtps::sptr<Renderizable> &node) {
+        for_each_node([&action](const mtps::sptr<Renderizable>& node) {
             if (auto tnode = std::dynamic_pointer_cast<T>(node))
             {
                 action(tnode);
@@ -91,7 +55,8 @@ public:
         });
     }
 
-    void for_each_node(mtps::function<void(const mtps::sptr<Renderizable> &)> action) const;
+    void for_each_node(
+        mtps::function<void(const mtps::sptr<Renderizable>&)> action) const;
 
     void updateRenderizables();
 
@@ -100,6 +65,6 @@ private:
     mtps::rptr<SceneNode> scene_node_;
     mtps::vector<mtps::sptr<Renderizable>> render_nodes_;
 };
-} // namespace haf::scene
+}  // namespace haf::scene
 
 #endif
