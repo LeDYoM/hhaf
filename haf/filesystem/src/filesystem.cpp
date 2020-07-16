aa36d15a6b89a1ce9f83a47f3631a6bd4c42d833
Ismael González <ledyom@hotmail.com>
2020-07-16 11:51:20 +0200

    Small filesystem modifications

--- haf/filesystem/src/filesystem.cpp
+++ haf/filesystem/src/filesystem.cpp
@@ -17,14 +17,27 @@ uptr<InnerType[]> readBuffer(uptr<InnerType[]> buffer,
                              const Path& file_name,
                              const size_type file_size)
 {
+    // Alias for the char type we are going to use to read the file
     using char_type = typename std::ifstream::char_type;
+
+    // Check that the passed InnerType has the same size
     static_assert(sizeof(InnerType) == sizeof(char_type));
-    std::ifstream ifs(file_name.c_str(), std::ios::binary);
-    char_type* buffer_char = reinterpret_cast<char_type*>(buffer.get());
-    ifs.read(buffer_char, file_size);
+
+    {
+        // Open the file to read in binary mode
+        std::ifstream ifs(file_name.c_str(), std::ios::binary);
+        // Get a pointer to the buffer but transforming the type to
+        // the one we are going to read
+        char_type* buffer_char = reinterpret_cast<char_type*>(buffer.get());
+
+        // Read the buffer.
+        ifs.read(buffer_char, file_size);
+        ifs.close();
+    }
+
     return buffer;
 }
-}
+}  // namespace
 
 RawMemory FileSystem::loadBinaryFile(const Path& file_name)
 {
@@ -77,6 +90,7 @@ bool FileSystem::saveFile(const Path& file_name, const str& data)
         file << data.c_str();
         // Correct is the status of the file object after reading it
         correct = file.good();
+        file.close();
     }
 
     if (!file || !correct)
