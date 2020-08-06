531ad26f7f59a90f9f19ae142949893abcdff850
LeDYoM <ledyom@hotmail.com>
2020-08-06 22:24:40 +0200

    Fix compilation for windows

--- haf/include/haf/haf_export.hpp
+++ haf/include/haf/haf_export.hpp
@@ -3,9 +3,11 @@
 
 #ifdef _WIN32
 #ifdef haf_EXPORTS
-#define HAF_API __declspec(dllexport)
+//#define HAF_API __declspec(dllexport)
+#define HAF_API
 #else
-#define HAF_API __declspec(dllimport)
+//#define HAF_API __declspec(dllimport)
+#define HAF_API
 #endif
 #else
 #define HAF_API __attribute__((visibility("default")))
