fab19e9fe3b6f132876be97b9d8f8714cc63aca8
LeDYoM <ledyom@hotmail.com>
2020-10-12 12:05:04 +0200

    Added some documentation

--- haf/include/haf/shareddata/include/shareddata.hpp
+++ haf/include/haf/shareddata/include/shareddata.hpp
@@ -13,16 +13,44 @@
 namespace haf::shdata
 {
 /**
- * @brief Component to provide access to data resources.
+ * @brief DataWrapper to provide access to data resources.
+ * This wrapper can be used standalone or as a base class of other wrappers.
  */
 class SharedData : public sys::IDataWrapper
 {
 public:
-    using sys::IDataWrapper::IDataWrapper;
+    using BaseClass = sys::IDataWrapper;
 
+    /**
+     * @brief Use base class constructors.
+     */
+    using BaseClass::BaseClass;
+
+    /**
+     * @brief Store an IShareable data element in the system. If the address
+     * does not exist, it will be created.
+     * @param address The address where to add the element.
+     * @param data Element to store.
+     * @return true Successfully stored.
+     * @return false Not stored.
+     */
     bool store(Address const& address, IShareable const& data);
+
+    /**
+     * @brief Retrieve an element from the system.
+     * @param address Address where to look for the element.
+     * @param data Data where to retrieve the element.
+     * @return true Successfully retrieved.
+     * @return false Error retrieving the element.
+     */
     [[nodiscard]] bool retrieve(Address const& address, IShareable& data);
 
+    /**
+     * @brief Ask if the system contains any data.
+     * 
+     * @return true 
+     * @return false 
+     */
     [[nodiscard]] bool isEmpty();
     bool makeEmpty();
 };
