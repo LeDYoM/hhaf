4bbe0e7d22b5e8af3cab7efdd8206cd3a3f0f5bc
Ismael González <ledyom@hotmail.com>
2020-11-18 22:24:20 +0100

    Adding some comments

--- host/host/include/host.hpp
+++ host/host/include/host.hpp
@@ -4,6 +4,7 @@
 #define HAF_HOST_MAIN_INCLUDE_HPP
 
 #include <mtypes/include/types.hpp>
+
 namespace mtps
 {
 class str;
@@ -14,19 +15,36 @@ namespace haf
 class IApp;
 }
 
+/**
+ * @brief Class containing a Host from the haf framework.
+ */
 namespace haf::host
 {
-struct ManagedApp;
 
 class Host final
 {
 public:
+    /**
+     * @brief Construct a new Host object. It is recommended to pass the
+     * command line parameters in the constructor.
+     * @param argc Number of command line parameters.
+     * @param argv Pointers to the command line parameters.
+     */
     Host(int argc, char* argv[]);
+
+    /**
+     * @brief Destroy the Host object
+     */
     ~Host();
 
     bool loadApplication(mtps::str const& app_name);
     bool unloadApplication(mtps::str const& app_name);
 
+    /**
+     * @brief Main loop function. When it returns, you can safely destroy
+     * the Host object.
+     * @return int 
+     */
     int run();
 
 private:
