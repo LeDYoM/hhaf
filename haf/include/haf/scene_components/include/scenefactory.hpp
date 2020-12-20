72c1793d03c48d31c9a0e187b59a56b8f9f7a6fa
LeDYoM <ledyom@hotmail.com>
2020-12-20 17:02:35 +0100

    Add documentation

--- haf/include/haf/scene_components/include/scenefactory.hpp
+++ haf/include/haf/scene_components/include/scenefactory.hpp
@@ -1,21 +1,30 @@
 #ifndef HAF_SCENE_SCENE_FACTORY_INCLUDE_HPP
 #define HAF_SCENE_SCENE_FACTORY_INCLUDE_HPP
 
-#include <mtypes/include/types.hpp>
-#include <mtypes/include/str.hpp>
-#include <mtypes/include/function.hpp>
 #include <haf/system/include/idatawrapper.hpp>
 #include <haf/scene/include/scenenodefactory.hpp>
+#include <haf/scene/include/scene.hpp>
 
 namespace haf::scene
 {
-
+/**
+ * @brief Class to allow clients to register a class derived from @b Scene
+ * to make the @b SceneManager system able to create instances of it.
+ */
 class SceneFactory : public sys::IDataWrapper
 {
 public:
+    /**
+     * @brief Registed a specific type of class derived from @b Scene
+     * @tparam T The type to register
+     * @return true The registration was correct
+     * @return false A problem happened when registering the type
+     */
     template <typename T>
     bool registerSceneType()
     {
+        static_assert(std::is_base_of_v<Scene, T>,
+                      "The template type must be inherited from Scene");
         return sceneNodeFactory().registerSceneNodeType<T>();
     }
 
