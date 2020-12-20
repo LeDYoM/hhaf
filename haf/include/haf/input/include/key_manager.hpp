8ee7f1b75158ca576f9549d3eea5288a9c6307bc
LeDYoM <ledyom@hotmail.com>
2020-12-20 17:20:00 +0100

    Added KeyManager files

---
+++ haf/include/haf/input/include/key_manager.hpp
@@ -0,0 +1,10 @@
+#ifndef HAF_INPUT_KEY_MANAGER_INCLUDE_HPP
+#define HAF_INPUT_KEY_MANAGER_INCLUDE_HPP
+
+#include <mtypes/include/types.hpp>
+
+namespace haf::input
+{
+}  // namespace haf::input
+
+#endif
---
+++ haf/input/src/key_manager.cpp
@@ -0,0 +1,10 @@
+#include <haf/input/include/key_manager.hpp>
+#include <input/i_include/inputsystem.hpp>
+
+#include <hlog/include/hlog.hpp>
+#include <haf/input/include/key.hpp>
+#include <system/i_include/get_system.hpp>
+
+namespace haf::input
+{
+}  // namespace haf::input
