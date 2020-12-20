e503515b73f1ef4630f461abeb4afbf70229c636
LeDYoM <ledyom@hotmail.com>
2020-12-20 17:42:02 +0100

    Basic implementation of KeyManager completed

--- haf/include/haf/input/include/key_manager.hpp
+++ haf/include/haf/input/include/key_manager.hpp
@@ -2,9 +2,23 @@
 #define HAF_INPUT_KEY_MANAGER_INCLUDE_HPP
 
 #include <mtypes/include/types.hpp>
+#include <mtypes/include/vector.hpp>
+#include <haf/system/include/idatawrapper.hpp>
+
+#include <haf/input/include/key.hpp>
 
 namespace haf::input
 {
+class KeyManager : public sys::IDataWrapper
+{
+public:
+    using KeyIndex = mtps::u16;
+
+    bool registerKey(KeyIndex const key_index, Key const key) noexcept;
+    Key keyForIndex(KeyIndex const key_index) const noexcept;
+private:
+    mtps::vector<Key> keys_;
+};
 }  // namespace haf::input
 
 #endif
--- haf/input/src/key_manager.cpp
+++ haf/input/src/key_manager.cpp
@@ -7,4 +7,33 @@
 
 namespace haf::input
 {
+bool KeyManager::registerKey(KeyIndex const key_index, Key const key) noexcept
+{
+    if (keys_.size() <= key_index)
+    {
+        auto const current_size = keys_.size();
+        auto const desired_size = key_index + 1U;
+
+        keys_.reserve(desired_size);
+
+        std::fill(&keys_[current_size], std::end(keys_), Key::Unknown);
+    }
+
+
+    if (keys_[key_index] == Key::Unknown)
+    {
+        // The slot is free to register
+        keys_[key_index] = key;
+        return true;
+    }
+
+    return false;
+
+}
+
+Key KeyManager::keyForIndex(KeyIndex const key_index) const noexcept
+{
+    return ((keys_.size() > key_index) ? Key::Unknown : keys_[key_index]);
+}
+
 }  // namespace haf::input
