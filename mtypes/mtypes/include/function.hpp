d43f77c25df949340a833093bd83bbe9f14ab492
LeDYoM <ledyom@hotmail.com>
2021-01-04 08:08:28 +0100

    Added test and functionality to function

--- mtypes/mtypes/include/function.hpp
+++ mtypes/mtypes/include/function.hpp
@@ -19,20 +19,28 @@ public:
     constexpr operator bool() const noexcept { return m_callable != nullptr; }
 
     template <typename T, typename V>
-    constexpr function(T *const t, V function) : m_callable{msptr<CallableMethodPointerT<T>>(t, function)} {}
+    constexpr function(T* const t, V function) :
+        m_callable{msptr<CallableMethodPointerT<T>>(t, function)}
+    {}
 
     template <typename T>
-    constexpr function(T t) : m_callable{msptr<CallableT<T>>(std::move(t))} {}
+    constexpr function(T t) : m_callable{msptr<CallableT<T>>(std::move(t))}
+    {}
 
-    constexpr ReturnValue operator()(Args... args) const
+    constexpr ReturnValue operator()(Args&&... args) const
     {
         assert(m_callable);
         return m_callable->Invoke(std::forward<Args>(args)...);
     }
 
-    constexpr bool operator==(const function &other) const noexcept
+    constexpr bool operator==(const function& other) const noexcept
     {
         return m_callable == other.m_callable;
+    }
+
+    constexpr bool equals(function const& other) const noexcept
+    {
+        return m_callable->equals(*(other.m_callable));
     }
 
 private:
@@ -40,7 +48,8 @@ private:
     {
     public:
         virtual ~ICallable() {}
-        virtual ReturnValue Invoke(Args...) = 0;
+        virtual ReturnValue Invoke(Args&&...)                = 0;
+        virtual bool equals(ICallable const&) const noexcept = 0;
     };
 
     template <typename T>
@@ -48,11 +57,27 @@ private:
     {
     public:
         template <typename Y>
-        constexpr CallableT(Y &&t) noexcept : m_t{std::forward<Y>(t)} {}
+        constexpr CallableT(Y&& t) noexcept : m_t{std::forward<Y>(t)}
+        {}
 
-        inline ReturnValue Invoke(Args... args) override
+        inline ReturnValue Invoke(Args&&... args) override
         {
             return m_t(std::forward<Args>(args)...);
+        }
+
+        inline bool equals(CallableT const& other) const noexcept
+        {
+            return &m_t == &other.m_t;
+        }
+
+        inline bool equals(ICallable const& other) const noexcept override
+        {
+            CallableT const* const tmp = dynamic_cast<CallableT const*>(&other);
+            if (tmp == nullptr)
+            {
+                return false;
+            }
+            return equals(*tmp);
         }
 
     private:
@@ -65,22 +90,53 @@ private:
     public:
         typedef ReturnValue (T::*HandlerFunctionPtr)(Args...);
 
-        constexpr CallableMethodPointerT(T *const receiver,
-                                         const HandlerFunctionPtr function)
-            : obj{receiver}, function_{function} {}
+        constexpr CallableMethodPointerT(T* const receiver,
+                                         const HandlerFunctionPtr function) :
+            obj{receiver}, function_{function}
+        {}
 
-        inline ReturnValue Invoke(Args... args) override
+        inline ReturnValue Invoke(Args&&... args) override
         {
             return (obj->*function_)(std::forward<Args>(args)...);
         }
 
+        inline bool equals(CallableMethodPointerT const& other) const noexcept
+        {
+            return obj == other.obj && function_ == other.function_;
+        }
+
+        inline bool equals(ICallable const& other) const noexcept override
+        {
+            function::CallableMethodPointerT<T> const* const tmp =
+                dynamic_cast<function::CallableMethodPointerT<T> const*>(
+                    &other);
+            if (tmp == nullptr)
+            {
+                return false;
+            }
+            return equals(*tmp);
+        }
+
     private:
-        T *const obj;
+        T* const obj;
         const HandlerFunctionPtr function_;
     };
 
     sptr<ICallable> m_callable;
 };
-} // namespace mtps
+
+template<typename ReturnType, typename... Args>
+constexpr auto make_function(ReturnType(*p)(Args...))
+{
+  return function<ReturnType(Args...)>(p);
+}
+
+template<typename ReturnType, typename T, typename... Args>
+constexpr auto make_function(T *obj, ReturnType(T::*p)(Args...))
+{
+  return function<ReturnType(Args...)>(obj, p);
+}
+
+}  // namespace mtps
 
 #endif
---
+++ mtypes/tests/function.test.cpp
@@ -0,0 +1,112 @@
+#include "catch.hpp"
+
+#include <mtypes/include/types.hpp>
+#include <mtypes/include/function.hpp>
+
+using namespace mtps;
+
+TEST_CASE("function::function", "[function]")
+{
+    function<s32(s32 const)> a;
+    CHECK_FALSE(a);
+}
+
+TEST_CASE("function::function with lambda", "[function]")
+{
+    function<s32(s32 const)> b = [](s32 const n) { return n + 1; };
+    CHECK(b);
+
+    CHECK(4 == b(3));
+}
+
+TEST_CASE("function::operator==() for lambdas", "[function]")
+{
+    function<s32(s32 const)> a = [](s32 const n) { return n + 1; };
+    function<s32(s32 const)> b = [](s32 const n) { return n + 1; };
+
+    // The lambda contains the same code, but the addresses are different
+    CHECK_FALSE(a == b);
+    CHECK_FALSE(a.equals(b));
+
+    b = a;
+    CHECK(a == b);
+    CHECK(a.equals(b));
+}
+
+struct TestObject
+{
+    s32 add2(s32 const n)
+    {
+        return n + 2;
+    }
+
+    s32 add3(s32 const n)
+    {
+        return n + 3;
+    }
+};
+
+TEST_CASE("function::equals for pointer to members", "[function]")
+{
+    TestObject a_obj;
+    TestObject b_obj;
+
+    {
+        auto a = function<s32(s32 const)>(&a_obj, &TestObject::add2);
+        auto b = function<s32(s32 const)>(&b_obj, &TestObject::add2);
+
+        // The lambda contains the same code, but the addresses are different
+        CHECK_FALSE(a == b);
+        CHECK_FALSE(a.equals(b));
+        CHECK(a(5) == 7);
+        CHECK(b(50) == 52);
+    }
+
+    {
+        auto a = function<s32(s32 const)>(&a_obj, &TestObject::add2);
+        auto b = function<s32(s32 const)>(&a_obj, &TestObject::add2);
+
+        CHECK_FALSE(a == b);
+        CHECK(a.equals(b));
+        CHECK(a(5) == 7);
+        CHECK(b(50) == 52);
+    }
+
+    {
+        auto a = function<s32(s32 const)>(&a_obj, &TestObject::add2);
+        auto b = function<s32(s32 const)>(&a_obj, &TestObject::add3);
+
+        CHECK_FALSE(a == b);
+        CHECK_FALSE(a.equals(b));
+        CHECK(a(5) == 7);
+        CHECK(b(50) == 53);
+    }
+}
+
+TEST_CASE("make_function", "[function]")
+{
+    TestObject a_obj;
+    TestObject b_obj;
+
+    {
+        auto a = make_function(&a_obj, &TestObject::add2);
+        auto b = make_function(&b_obj, &TestObject::add2);
+
+        // The lambda contains the same code, but the addresses are different
+        CHECK_FALSE(a == b);
+        CHECK_FALSE(a.equals(b));
+        CHECK(a(5) == 7);
+        CHECK(b(50) == 52);
+    }
+
+    {
+        auto a = make_function(&a_obj, &TestObject::add2);
+        auto b = make_function(&a_obj, &TestObject::add2);
+
+        // The lambda contains the same code, but the addresses are different
+        CHECK_FALSE(a == b);
+        CHECK(a.equals(b));
+        CHECK(a(5) == 7);
+        CHECK(b(50) == 52);
+    }
+}
