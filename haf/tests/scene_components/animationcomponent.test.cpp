2b840381da2e0f7d91b04aa37bc85e3437ce2d68
LeDYoM <ledyom@hotmail.com>
2021-01-24 15:26:43 +0100

    Fix test for animation component

--- haf/tests/scene_components/animationcomponent.test.cpp
+++ haf/tests/scene_components/animationcomponent.test.cpp
@@ -2,22 +2,24 @@
 
 #include <mtypes/include/types.hpp>
 #include <haf/scene_components/include/animationcomponent.hpp>
-#include <haf/scene_components/include/animationcomponent.hpp>
 
 using namespace mtps;
 using namespace haf;
 using namespace haf::scene;
+
+namespace
+{
+struct CommonData
+{
+    sptr<AnimationComponent> animation_component;
+};
+}
 
 TEST_CASE("haf::scene::AnimationComponent",
           "[StatesController][lib][scene][component][AnimationComponent][IComponent]")
 {
     constexpr u32 Start_State  = 0U;
     constexpr u32 Finish_State = 1U;
-
-    struct CommonData
-    {
-        sptr<AnimationComponent> animation_component;
-    };
 
     CommonData common;
     common.animation_component = msptr<AnimationComponent>();
