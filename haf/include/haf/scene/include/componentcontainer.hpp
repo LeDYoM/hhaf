295f7a0bf085dfb76bd04dccbd850226fb8214bc
LeDYoM <ledyom@hotmail.com>
2021-03-31 18:51:20 +0200

    Created new cpp woth code

--- haf/include/haf/scene/include/componentcontainer.hpp
+++ haf/include/haf/scene/include/componentcontainer.hpp
@@ -16,21 +16,11 @@ public:
         typename ComponentContainerPart<false>::ComponentType;
     using AttachableType = ComponentContainerPart<true>::AttachableType;
 
-    explicit ComponentContainer(htps::rptr<AttachableType> init) :
-        component_container_{init}, static_component_container_{init}
-    {}
+    explicit ComponentContainer(htps::rptr<AttachableType> init);
 
-    void updateComponents()
-    {
-        component_container_.updateComponents();
-        static_component_container_.updateComponents();
-    }
+    void updateComponents();
 
-    void clearComponents()
-    {
-        component_container_.clearComponents();
-        static_component_container_.clearComponents();
-    }
+    void clearComponents();
 
     template <typename T>
     htps::sptr<T> componentOfType() const
