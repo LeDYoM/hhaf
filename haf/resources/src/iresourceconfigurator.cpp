e548f892a53c99a9e8e07667c93fe2a94d659cae
Ismael González <ledyom@hotmail.com>
2021-04-29 00:15:07 +0200

    Reorder includes

--- haf/resources/src/iresourceconfigurator.cpp
+++ haf/resources/src/iresourceconfigurator.cpp
@@ -1,8 +1,9 @@
 #include <haf/system/include/interfaceaccess.hpp>
-#include <haf/resources/include/iresourceconfigurator.hpp>
 #include <system/i_include/get_system.hpp>
 #include <system/i_include/interfaceaccess_imp.hpp>
+
 #include <resources/i_include/resourcemanager.hpp>
+#include <resources/include/iresourceconfigurator.hpp>
 
 namespace haf::sys
 {
