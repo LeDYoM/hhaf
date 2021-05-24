202f2776cffa5d94b4f77cbc49a196009695232e
LeDYoM <ledyom@hotmail.com>
2021-05-24 17:39:21 +0200

    Creating export files

--- cmake_modules/functions.cmake
+++ cmake_modules/functions.cmake
@@ -83,57 +83,6 @@ function(build_lib_component)
 
 endfunction()
 
-# Function to build different components from the project in an unified way.
-function(build_lib_component_modern)
-
-  cmake_parse_arguments(LC_BUILD "EXPORT_ALL;STATIC" "HEADER_DIRECTORY" "SOURCES"
-                        ${ARGN})
-
-  if(LC_BUILD_STATIC)
-    message(STATUS "Add library static: ${CURRENT_TARGET}")
-    add_library(${CURRENT_TARGET} STATIC ${LC_BUILD_SOURCES})
-  else()
-    message(STATUS "Add library shared: ${CURRENT_TARGET}")
-    add_library(${CURRENT_TARGET} SHARED ${LC_BUILD_SOURCES})
-  endif()
-
-  set_compile_warning_level_and_cxx_properties(${CURRENT_TARGET})
-
-  if(LC_BUILD_EXPORT_ALL)
-    if (LC_BUILD_STATIC)
-      message(WARN "STATIC and EXPORT_ALL together makes no sense")
-    endif()
-    set_target_properties(${CURRENT_TARGET}
-                          PROPERTIES WINDOWS_EXPORT_ALL_SYMBOLS true)
-  endif()
-
-  target_include_directories(${CURRENT_TARGET}
-      PUBLIC
-      "$<INSTALL_INTERFACE:include>"
-      "$<BUILD_INTERFACE:${LC_BUILD_HEADER_DIRECTORY}>"
-  )
-
-  file(GLOB_RECURSE public_headers 
-    RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}
-    ${CMAKE_CURRENT_SOURCE_DIR}/${CURRENT_TARGET}/include/*.hpp
-    )
-
-  include(GNUInstallDirs)
-  install(TARGETS ${CURRENT_TARGET}
-      EXPORT ${CURRENT_TARGET}Targets
-      INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
-      RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
-      ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
-      LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
-  )
-
-#  install(EXPORT ${CURRENT_TARGET}Targets
-#    FILE ${CURRENT_TARGET}Targets.cmake
-#    NAMESPACE hhaf::
-#  DESTINATION
-#    ${CMAKE_INSTALL_LIBDIR}/cmake/hhaf)
-
-endfunction()
 
 function(build_lib_ext)
 
@@ -155,13 +104,16 @@ endfunction()
 # Function to build different components from the project in an unified way.
 function(build_lib_interface_component)
 
-  cmake_parse_arguments(LC_BUILD "" "HEADER_DIRECTORY" "" ${ARGN})
+  cmake_parse_arguments(LC_BUILD "" "HEADER_DIRECTORY" "SOURCES" ${ARGN})
 
   set(CURRENT_TARGET ${PROJECT_NAME})
 
   add_library(${CURRENT_TARGET} INTERFACE)
+  target_sources(${CURRENT_TARGET} INTERFACE ${LC_BUILD_SOURCES})
   target_include_directories(${CURRENT_TARGET}
                              INTERFACE ${LC_BUILD_HEADER_DIRECTORY})
+
+  export(TARGETS ${CURRENT_TARGET} FILE ${CURRENT_TARGET}_target.cmake)
 
 endfunction()
 
@@ -175,36 +127,6 @@ function(build_concrete_backend)
   target_link_libraries(${CURRENT_TARGET} PRIVATE backend_dev)
   target_link_libraries(${CURRENT_TARGET} PRIVATE backend_client)
 
-endfunction(build_concrete_backend)
-
-function(add_development_dependency _source _dependency)
-    add_dependencies(${_source} ${_dependency})
-endfunction()
-
-function(build_doc_old _base_name)
-  # check if Doxygen is installed
-  find_package(Doxygen)
-  if(DOXYGEN_FOUND)
-    message("Doxygen found")
-    message("Generating doxygen files for " ${_base_name})
-    # set input and output files
-    set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/docs/Doxyfile.in)
-    set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
-
-    # request to configure the file
-    configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
-
-    add_custom_target(
-      ${_base_name}_doc_doxygen
-      COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
-      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-      COMMENT "Generating API documentation with Doxygen for ${_base_name}"
-      VERBATIM)
-  else()
-    message(
-      "Doxygen needs to be installed to generate the doxygen documentation for "
-      ${_base_name})
-  endif()
 endfunction()
 
 function(build_doc CURRENT_TARGET)
