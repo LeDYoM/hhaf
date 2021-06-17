c960948b482bf677bc224171539339fd6dbce466
LeDYoM <ledyom@hotmail.com>
2021-06-17 21:15:58 +0200

    Small refractors and docu addition

--- haf/include/haf/include/scene/color_imp.hpp
+++ haf/include/haf/include/scene/color_imp.hpp
@@ -10,13 +10,15 @@ namespace haf::scene
 namespace detail
 {
 template <typename value_type>
-static constexpr value_type value_max = std::numeric_limits<value_type>::max();
+static constexpr value_type const value_max =
+    std::numeric_limits<value_type>::max();
 
 template <typename value_type>
-static constexpr value_type value_min = std::numeric_limits<value_type>::min();
+static constexpr value_type const value_min =
+    std::numeric_limits<value_type>::min();
 
 template <typename Dest, typename Source>
-constexpr Dest ensureLimits(Source source) noexcept
+constexpr Dest ensureLimits(Source const source) noexcept
 {
     return static_cast<Dest>(
         std::min(static_cast<Source>(value_max<Dest>),
@@ -24,23 +26,40 @@ constexpr Dest ensureLimits(Source source) noexcept
 }
 
 template <typename value_type>
-constexpr float normalize(const value_type v) noexcept
+constexpr htps::f32 normalize(value_type const v) noexcept
 {
-    return static_cast<float>(static_cast<float>(v) /
-                              static_cast<float>(value_max<value_type>));
+    return static_cast<htps::f32>(
+        static_cast<htps::f32>(v) /
+        static_cast<htps::f32>(value_max<value_type>));
 }
 
 template <typename value_type>
-constexpr void denormalize(const float v, value_type& d) noexcept
+constexpr void denormalize(const htps::f32 v, value_type& d) noexcept
 {
     d = static_cast<value_type>(v * value_max<value_type>);
 }
+
 }  // namespace detail
 
+/**
+ * @brief Class representing a color. The suffix helps to
+ * create custom specializations with more meaningful names.
+ * This class contains red, green, value and alpha values
+ * representing a color in the (r,g,b,a) space.
+ *
+ * @tparam value_type Type to represent the interal data values.
+ */
 template <typename value_type>
 struct ColorImp
 {
+    /**
+     * @brief Maximum value for each coordinate
+     */
     static constexpr value_type value_max = detail::value_max<value_type>;
+
+    /**
+     * @brief Minimum value for each coordinate
+     */
     static constexpr value_type value_min = detail::value_min<value_type>;
 
     template <typename Source>
@@ -58,16 +77,17 @@ struct ColorImp
         r{red}, g{green}, b{blue}, a{alpha}
     {}
 
-    constexpr ColorImp(const ColorImp&) noexcept = default;
-    constexpr ColorImp& operator=(const ColorImp&) noexcept = default;
+    constexpr ColorImp(ColorImp const&) noexcept = default;
+    constexpr ColorImp& operator=(ColorImp const&) noexcept = default;
     constexpr ColorImp(ColorImp&&) noexcept                 = default;
     constexpr ColorImp& operator=(ColorImp&&) noexcept = default;
 
+    // TODO: Enable_if for value_type = u8
     constexpr explicit ColorImp(const htps::u32 color) noexcept :
-        r{static_cast<value_type>((color & 0xff000000) >> 24U)},
-        g{static_cast<value_type>((color & 0x00ff0000) >> 16U)},
-        b{static_cast<value_type>((color & 0x0000ff00) >> 8U)},
-        a{static_cast<value_type>((color & 0x000000ff) >> 0U)}
+        r{static_cast<value_type>((color & 0xFF000000) >> 24U)},
+        g{static_cast<value_type>((color & 0x00FF0000) >> 16U)},
+        b{static_cast<value_type>((color & 0x0000FF00) >> 8U)},
+        a{static_cast<value_type>((color & 0x000000FF) >> 0U)}
     {}
 
     template <typename vt>
@@ -79,10 +99,10 @@ struct ColorImp
         a = ensureLimits(static_cast<value_type>(other.a));
     }
 
-    static constexpr ColorImp fromFloats(const float red,
-                                         const float green,
-                                         const float blue,
-                                         const float alpha = 1.0F) noexcept
+    static constexpr ColorImp fromFloats(htps::f32 const red,
+                                         htps::f32 const green,
+                                         htps::f32 const blue,
+                                         htps::f32 const alpha = 1.0F) noexcept
     {
         return ColorImp{static_cast<value_type>(red * value_max),
                         static_cast<value_type>(green * value_max),
@@ -91,44 +111,43 @@ struct ColorImp
     }
 
     template <typename vt>
-    constexpr ColorImp(const ColorImp<vt>& other) noexcept
-    {
-        r = ensureLimits(static_cast<value_type>(other.r));
-        g = ensureLimits(static_cast<value_type>(other.g));
-        b = ensureLimits(static_cast<value_type>(other.b));
-        a = ensureLimits(static_cast<value_type>(other.a));
-    }
+    constexpr ColorImp(ColorImp<vt> const& other) noexcept :
+        r{ensureLimits(static_cast<value_type>(other.r))},
+        g{ensureLimits(static_cast<value_type>(other.g))},
+        b{ensureLimits(static_cast<value_type>(other.b))},
+        a{ensureLimits(static_cast<value_type>(other.a))}
+    {}
 
-    constexpr bool operator==(const ColorImp& right) const noexcept
+    constexpr bool operator==(ColorImp const& right) const noexcept
     {
         return (r == right.r && g == right.g && b == right.b && a == right.a);
     }
 
-    constexpr bool operator!=(const ColorImp& right) const noexcept
+    constexpr bool operator!=(ColorImp const& right) const noexcept
     {
         return !(*this == right);
     }
 
     template <typename VT>
-    constexpr ColorImp operator+(const ColorImp<VT>& right) const noexcept
+    constexpr ColorImp operator+(ColorImp<VT> const& right) const noexcept
     {
         return (ColorImp(*this) += right);
     }
 
     template <typename VT>
-    constexpr ColorImp operator-(const ColorImp<VT>& right) const noexcept
+    constexpr ColorImp operator-(ColorImp<VT> const& right) const noexcept
     {
         return (ColorImp(*this) -= right);
     }
 
     template <typename VT>
-    constexpr ColorImp operator*(const ColorImp<VT>& right) const noexcept
+    constexpr ColorImp operator*(ColorImp<VT> const& right) const noexcept
     {
         return (ColorImp(*this) *= right);
     }
 
     template <typename vt>
-    constexpr ColorImp& operator+=(const ColorImp<vt>& right) noexcept
+    constexpr ColorImp& operator+=(ColorImp<vt> const& right) noexcept
     {
         r = ensureLimits(static_cast<vt>(r) + right.r);
         g = ensureLimits(static_cast<vt>(g) + right.g);
@@ -159,7 +178,7 @@ struct ColorImp
         return *this;
     }
 
-    constexpr ColorImp& operator*=(const htps::f32 delta) noexcept
+    constexpr ColorImp& operator*=(htps::f32 const delta) noexcept
     {
         r = detail::ensureLimits<value_type>(static_cast<htps::f32>(r) * delta);
         g = detail::ensureLimits<value_type>(static_cast<htps::f32>(g) * delta);
@@ -182,8 +201,8 @@ struct ColorImp
     constexpr value_type blue() const noexcept { return b; }
     constexpr value_type alpha() const noexcept { return a; }
 
-  template<typename vt>
-  friend struct ColorImp;
+    template <typename vt>
+    friend struct ColorImp;
 
 private:
     value_type r;
@@ -194,13 +213,13 @@ private:
 
 template <typename vt>
 constexpr ColorImp<vt> operator*(ColorImp<vt> color,
-                                 const htps::f32 delta) noexcept
+                                 htps::f32 const delta) noexcept
 {
     return ColorImp<vt>{std::move(color)} *= delta;
 }
 
 template <typename vt>
-constexpr ColorImp<vt> operator*(const htps::f32 delta,
+constexpr ColorImp<vt> operator*(htps::f32 const delta,
                                  ColorImp<vt> color) noexcept
 {
     return std::move(color) * delta;
@@ -208,13 +227,13 @@ constexpr ColorImp<vt> operator*(const htps::f32 delta,
 
 template <typename vt>
 constexpr ColorImp<vt> operator/(ColorImp<vt> color,
-                                 const htps::f32 delta) noexcept
+                                 htps::f32 const delta) noexcept
 {
     return ColorImp<vt>{std::move(color)} /= delta;
 }
 
 template <typename vt>
-constexpr ColorImp<vt> operator/(const htps::f32 delta,
+constexpr ColorImp<vt> operator/(htps::f32 const delta,
                                  ColorImp<vt> color) noexcept
 {
     return std::move(color) / delta;
