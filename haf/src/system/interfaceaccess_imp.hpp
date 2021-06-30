593e6117d66afa7149096f289f934dd301c585a6
Ismael González <ismael.gonzalez@wallbox.com>
2021-06-30 09:42:31 +0200

    Aligned code

--- haf/src/system/interfaceaccess_imp.hpp
+++ haf/src/system/interfaceaccess_imp.hpp
@@ -3,20 +3,18 @@
 
 namespace haf::sys
 {
-#define GET_INTERFACE_IMP(INTCLASS, SYSTEMCLASS)    \
-template <> \
-INTCLASS& getInterface(SystemAccess& system_access) \
-{   \
-    return static_cast<INTCLASS&>(  \
-        getSystem<SYSTEMCLASS>(&system_access));    \
-}   \
-    \
-template <> \
-INTCLASS const& getInterface(   \
-    SystemAccess const& system_access)  \
-{   \
-    return static_cast<INTCLASS const&>(    \
-        getSystem<SYSTEMCLASS>(&system_access));    \
-}
+#define GET_INTERFACE_IMP(INTCLASS, SYSTEMCLASS) \
+    template <> \
+    INTCLASS& getInterface(SystemAccess& system_access) \
+    { \
+        return static_cast<INTCLASS&>(getSystem<SYSTEMCLASS>(&system_access)); \
+    } \
+\
+    template <> \
+    INTCLASS const& getInterface(SystemAccess const& system_access) \
+    { \
+        return static_cast<INTCLASS const&>( \
+            getSystem<SYSTEMCLASS>(&system_access)); \
+    }
 
-}
+}  // namespace haf::sys
