70b6f73091874a82c16cc307b3dec9a2eca3cd56
Ismael González <ismael.gonzalez@wallbox.com>
2021-07-08 13:53:31 +0200

    Small refactor

--- backend/src/backendfactory.cpp
+++ backend/src/backendfactory.cpp
@@ -4,42 +4,12 @@
 
 #include <agloader/include/loader.hpp>
 
+#include "backend_factory_utils.hpp"
+
 using namespace htps;
 
 namespace haf::backend
 {
-namespace
-{
-template <typename FactoryType>
-bool fillFactory(const uptr<BackendRegister>& backend_register,
-                 FactoryType** factory_to_fill)
-{
-    if (auto factory(backend_register->getFactory<IFactoryOf<FactoryType>>());
-        factory != nullptr)
-    {
-        (*factory_to_fill) = factory->create();
-        return (*factory_to_fill) != nullptr;
-    }
-    return false;
-}
-
-template <typename FactoryType>
-bool emptyFactory(const uptr<BackendRegister>& backend_register,
-                  FactoryType** factory_to_empty)
-{
-    if (auto factory(backend_register->getFactory<IFactoryOf<FactoryType>>());
-        factory != nullptr)
-    {
-        if (factory_to_empty != nullptr && *factory_to_empty != nullptr)
-        {
-            factory->destroy(*factory_to_empty);
-            (*factory_to_empty) = nullptr;
-            return true;
-        }
-    }
-    return false;
-}
-}  // namespace
 
 BackendFactory::BackendFactory() :
     window_{nullptr},
@@ -53,6 +23,7 @@ BackendFactory::BackendFactory() :
     static const char* sh_name = "bsfml";
     if (loader->loadModule(sh_name))
     {
+        // Get the function pointer to load and unload the backend module
         const auto fp_init_haf = reinterpret_cast<p_initBackendClient>(
             loader->loadMethod(sh_name, "init_lib"));
         const auto fp_finish_haf = reinterpret_cast<p_initBackendClient>(
@@ -65,21 +36,16 @@ BackendFactory::BackendFactory() :
             backend_register_->init();
         }
 
-        bool result{fillFactory(backend_register_, &window_)};
-        result &= fillFactory(backend_register_, &ttfontFactory_);
-        result &= fillFactory(backend_register_, &textureFactory_);
-        result &= fillFactory(backend_register_, &shaderFactory_);
+        fillFactories(backend_register_, &window_, &ttfontFactory_,
+                      &textureFactory_, &shaderFactory_);
     }
 }
 
 BackendFactory::~BackendFactory()
 {
-    bool result{emptyFactory(backend_register_, &window_)};
-    result &= emptyFactory(backend_register_, &textureFactory_);
-    result &= emptyFactory(backend_register_, &ttfontFactory_);
-    result &= emptyFactory(backend_register_, &shaderFactory_);
+    emptyFactories(backend_register_, &window_, &textureFactory_,
+                   &ttfontFactory_, &shaderFactory_);
     backend_register_->finish();
-
     backend_register_.reset();
 
     agloader::destroyLoader();
