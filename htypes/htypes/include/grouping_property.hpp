1c6fdad37f2c1319d2ebf09b4fd1ac204d913533
Ismael Gonzalez <ismael.gonzalez@wallbox.com>
2021-08-10 09:56:55 +0200

    Allow basicproperties in propertygroupimpl

--- htypes/htypes/include/grouping_property.hpp
+++ htypes/htypes/include/grouping_property.hpp
@@ -4,151 +4,10 @@
 #include "properties.hpp"
 #include "propertystate.hpp"
 #include <type_traits>
+#include "grouping_property_impl.hpp"
 
 namespace htps
 {
-/**
- * @brief Alias to encapsulate a @b PropertyState given a Tag
- * 
- * @tparam Tag Type encapsulated in the PropertyState.
- * This Tag should export a value_type with the tyè contained
- * in this Tag
- */
-template <typename Tag>
-using GroupableProperty = PropertyState<typename Tag::value_type, Tag>;
-
-template <typename Tag>
-struct GroupablePropertyImpl
-{
-    template <typename Tag_>
-    struct ContainsTag
-    {
-        static constexpr bool value = std::is_same_v<Tag_, Tag>;
-    };
-
-    template <typename Tag_>
-    static constexpr bool ContainsTag_v = ContainsTag<Tag_>::value;
-
-    GroupablePropertyImpl() = default;
-    GroupablePropertyImpl(typename Tag::value_type const& value) noexcept :
-        prop_{value}
-    {}
-
-    GroupablePropertyImpl(typename Tag::value_type&& value) noexcept :
-        prop_{std::move(value)}
-    {}
-
-    template <typename Tag_>
-    std::enable_if_t<GroupablePropertyImpl::ContainsTag_v<Tag_>,
-                     GroupableProperty<Tag_>&>
-    get_property_reference() noexcept
-    {
-        return prop_;
-    }
-
-    template <typename Tag_>
-    std::enable_if_t<GroupablePropertyImpl::ContainsTag_v<Tag_>,
-                     GroupableProperty<Tag_> const&>
-    get_property_reference() const noexcept
-    {
-        return prop_;
-    }
-
-private:
-    GroupableProperty<Tag> prop_;
-};
-
-/**
- * @brief Base class for @b GroupableProperty It contains some common
- * 
- * @tparam FirstTag First element of the group of Tag types
- * @tparam Tag... Group of tags to include inside this type. Each Tag element
- * should contain a @b Tag::value_type exporting the type it exposes.
- */
-template <typename FirstTag, typename... Tag>
-struct PropertyGroupImpl : public GroupablePropertyImpl<FirstTag>,
-                           public PropertyGroupImpl<Tag...>
-{
-    using PropertyGroupImplBase = PropertyGroupImpl<Tag...>;
-    PropertyGroupImpl()         = default;
-
-    PropertyGroupImpl(typename FirstTag::value_type const& value,
-                      typename Tag::value_type const&... values) :
-        GroupablePropertyImpl<FirstTag>{value},
-        PropertyGroupImpl<Tag...>::PropertyGroupImpl(values...)
-    {}
-
-    template <typename Tag_>
-    struct ContainsTag
-    {
-        static constexpr bool base_value =
-            PropertyGroupImplBase::template ContainsTag<Tag_>::value;
-        static constexpr bool value =
-            std::is_same_v<Tag_, FirstTag> || base_value;
-    };
-
-    template <typename Tag_>
-    static constexpr bool ContainsTag_v =
-        PropertyGroupImpl::ContainsTag<Tag_>::value;
-
-    template <
-        typename Tag_,
-        std::enable_if_t<PropertyGroupImpl::ContainsTag_v<Tag_>>* = nullptr>
-    GroupableProperty<Tag_> const& get_property_reference() const noexcept
-    {
-        if constexpr (std::is_same_v<Tag_, FirstTag>)
-        {
-            return GroupablePropertyImpl<
-                FirstTag>::template get_property_reference<Tag_>();
-        }
-        else
-        {
-            return PropertyGroupImpl<Tag...>::template get_property_reference<
-                Tag_>();
-        }
-    }
-
-    template <
-        typename Tag_,
-        std::enable_if_t<PropertyGroupImpl::ContainsTag_v<Tag_>>* = nullptr>
-    GroupableProperty<Tag_>& get_property_reference() noexcept
-    {
-        if constexpr (std::is_same_v<Tag_, FirstTag>)
-        {
-            return GroupablePropertyImpl<
-                FirstTag>::template get_property_reference<Tag_>();
-        }
-        else
-        {
-            return PropertyGroupImpl<Tag...>::template get_property_reference<
-                Tag_>();
-        }
-    }
-};
-
-/**
- * @brief Specialization for one type only
- * @tparam FirstTag Unique Tag
- */
-template <typename FirstTag>
-struct PropertyGroupImpl<FirstTag> : public GroupablePropertyImpl<FirstTag>
-{
-    template <typename Tag_>
-    struct ContainsTag
-    {
-        static constexpr bool value =
-            GroupablePropertyImpl<FirstTag>::template ContainsTag<Tag_>::value;
-    };
-
-    template <typename Tag_>
-    static constexpr bool ContainsTag_v = ContainsTag<Tag_>::value;
-
-    PropertyGroupImpl() = default;
-
-    using GroupablePropertyImpl<FirstTag>::GroupablePropertyImpl;
-    using GroupablePropertyImpl<FirstTag>::get_property_reference;
-};
-
 /**
  * @brief Class exporting the functionality to group some properties and their
  * most important actions on them.
@@ -157,8 +16,9 @@ struct PropertyGroupImpl<FirstTag> : public GroupablePropertyImpl<FirstTag>
 template <typename... Tag>
 struct PropertyGroup : public PropertyGroupImpl<Tag...>
 {
+private:
     using Base = PropertyGroupImpl<Tag...>;
-
+public:
     PropertyGroup()                     = default;
     PropertyGroup(PropertyGroup const&) = delete;
     PropertyGroup& operator=(PropertyGroup const&) = delete;
