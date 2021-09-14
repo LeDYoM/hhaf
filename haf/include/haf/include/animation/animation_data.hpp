cbc207a916f07011bd2e55f73b360a945dfb4d73
Ismael Gonzalez <ledyom@hotmail.com>
2021-09-14 14:21:48 +0200

    wip

--- haf/include/haf/include/animation/animation_data.hpp
+++ haf/include/haf/include/animation/animation_data.hpp
@@ -18,8 +18,8 @@ using ActionFunc = htps::function<void()>;
 
 enum class AnimationDirection : htps::u8
 {
-    Forward   = 0U,  //< Animate increasing the delta value
-    Backward  = 1U,  //< Animate decreasing the delta value
+    Forward  = 0U,  //< Animate increasing the delta value
+    Backward = 1U,  //< Animate decreasing the delta value
 };
 
 enum class AnimationDeltaMode : htps::u8
@@ -33,15 +33,15 @@ public:
     htps::uptr<time::Timer> timer_;  //< Timer to use to animate
     time::TimePoint duration_;  //< @b time::TimePoint containing the time the
                                 // animation is going to last
-    ActionFunc end_action_;     //< Functor containing an action to perform when
-                                // the animation finishes
+    ActionFunc end_action_;  //< Functor containing an action to perform when
+                             //< the animation finishes
     AnimationDirection animation_direction_{
         AnimationDirection::Forward};  //< Direction of the animation
     AnimationDeltaMode animation_delta_mode_{
         AnimationDeltaMode::Linear};  //< Select equation to update the delta
                                       // value
     htps::s32 times_{1U};  //< Number of times to perform the animation
-    bool switch_{false};    //< Switch between forward and backward
+    bool switch_{false};   //< Switch between forward and backward
 };
 
 }  // namespace haf::anim
--- haf/include/haf/include/animation/property_animation_data.hpp
+++ haf/include/haf/include/animation/property_animation_data.hpp
@@ -2,16 +2,16 @@
 #define HAF_ANIMATION_PROPERTY_ANIMATION_DATA_INCLUDE_HPP
 
 #include <htypes/include/types.hpp>
-#include <htypes/include/properties.hpp>
 #include <haf/include/animation/animation_data.hpp>
-
-namespace haf::scene
-{
-class SceneNode;
-}
 
 namespace haf::anim
 {
+enum class AnimationEndAction
+{
+    PerformUserAction  = 0U,
+    RemoveAnimatedNode = 1U
+};
+
 template <typename PropertyTag, typename SceneNodeType>
 class PropertyAnimationData
 {
@@ -19,6 +19,9 @@ public:
     htps::rptr<SceneNodeType> scene_node_{nullptr};
     typename PropertyTag::value_type startValue_;
     typename PropertyTag::value_type endValue_;
+    AnimationEndAction animation_end_action_{
+        AnimationEndAction::PerformUserAction};  //< Type of action to perform
+                                                 //< when animation ends
 };
 
 }  // namespace haf::anim
