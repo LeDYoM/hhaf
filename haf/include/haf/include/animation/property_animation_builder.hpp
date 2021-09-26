8dc6b3a6a9c75f14adafe3149ae864ba2bbe35c8
LeDYoM <ledyom@hotmail.com>
2021-09-26 16:06:28 +0200

    Addind documentation

--- haf/include/haf/include/animation/property_animation_builder.hpp
+++ haf/include/haf/include/animation/property_animation_builder.hpp
@@ -6,57 +6,64 @@
 
 namespace haf::anim
 {
+/**
+ * @brief Builder class to help the creation of animations. It wraps
+ * a @b PropertyAnimationData<PropertyTag, SceneNodeT> and modifies
+ * it providing an easy interface.
+ * @tparam PropertyTag Tag fpr the property
+ * @tparam SceneNodeT Type of the SceneNode to animate
+ */
 template <typename PropertyTag, typename SceneNodeT>
 class PropertyAnimationBuilder
 {
 public:
-    PropertyAnimationBuilder() = default;
-
+    /**
+     * @brief Construct a new Property Animation Builder object
+     * @param data An already created @b PropertyAnimationData
+     */
     explicit PropertyAnimationBuilder(
         PropertyAnimationData<PropertyTag, SceneNodeT> data) :
         data_{std::move(data)}
     {}
 
     PropertyAnimationBuilder& startValue(
-        StartValue<PropertyTag>::value_type value)
+        StartValue<PropertyTag>::value_type const& value)
     {
-        data_.prop<StartValue<PropertyTag>>() = std::move(value);
+        data_.prop<StartValue<PropertyTag>>() = value;
         return *this;
     }
 
-    PropertyAnimationBuilder& endValue(EndValue<PropertyTag>::value_type value)
+    PropertyAnimationBuilder& endValue(
+        EndValue<PropertyTag>::value_type const& value)
     {
-        data_.prop<EndValue<PropertyTag>>() = std::move(value);
+        data_.prop<EndValue<PropertyTag>>() = value;
         return *this;
     }
 
-    PropertyAnimationBuilder& duration(Duration::value_type value)
+    PropertyAnimationBuilder& duration(Duration::value_type const& value)
     {
-        data_.prop<Duration>() = std::move(value);
+        data_.prop<Duration>() = value;
         return *this;
     }
 
-    PropertyAnimationBuilder& times(Times::value_type value)
+    PropertyAnimationBuilder& times(Times::value_type const& value)
     {
-        data_.prop<Times>() = std::move(value);
+        data_.prop<Times>() = value;
         return *this;
     }
 
-    PropertyAnimationBuilder& switchAnimation(Switch::value_type value)
+    PropertyAnimationBuilder& switchAnimation(Switch::value_type const& value)
     {
-        data_.prop<Switch>() = std::move(value);
+        data_.prop<Switch>() = value;
         return *this;
     }
 
-    PropertyAnimationBuilder& continuous()
-    {
-        return times(-1);
-    }
+    PropertyAnimationBuilder& continuous() { return times(-1); }
 
     PropertyAnimationBuilder& actionWhenFinished(
-        ActionWhenFinished::value_type value)
+        ActionWhenFinished::value_type const& value)
     {
-        data_.prop<ActionWhenFinished>() = std::move(value);
+        data_.prop<ActionWhenFinished>() = value;
         return *this;
     }
 
