7fca074172e95d586ab171507d528c4588a3eace
Ismael Gonzalez <ledyom@hotmail.com>
2021-10-28 09:54:04 +0200

    Created dummy version of TimeSystemAcceleration for no acceleration

--- haf/src/time/time_system_acceleration.hpp
+++ haf/src/time/time_system_acceleration.hpp
@@ -6,6 +6,8 @@
 
 namespace haf::sys
 {
+#ifdef HAF_ALLOW_ACCELERATION
+
 class TimeSystemAcceleration
 {
 public:
@@ -28,15 +30,16 @@ public:
         acceleration_    = 1.0F;
     }
 
-    time::TimePoint&& accelerate(time::TimePoint&& time_point) const noexcept
+    constexpr time::TimePoint&& accelerate(
+        time::TimePoint&& time_point) const noexcept
     {
-        if (!use_acceleration)
+        if (use_acceleration)
         {
-            return std::move(time_point);
+            return std::move(time_point * acceleration_);
         }
         else
         {
-            return std::move(time_point * acceleration_);
+            return std::move(time_point);
         }
     }
 
@@ -44,8 +47,24 @@ private:
     AccelerationFactor acceleration_ = 1.0f;
     bool use_acceleration{false};
 };
+#else
 
+class TimeSystemAcceleration
+{
+public:
+    using AccelerationFactor = htps::f32;
 
+    constexpr void setAcceleration(AccelerationFactor const) const noexcept {}
+    constexpr void resetAcceleration() const noexcept {}
+
+    constexpr time::TimePoint&& accelerate(
+        time::TimePoint&& time_point) const noexcept
+    {
+        return std::move(time_point);
+    }
+};
+
+#endif
 }  // namespace haf::sys
 
 #endif
