1ce066798dcf3591dc9b21b369aeb5d7c624f59c
Ismael Gonzalez <ledyom@hotmail.com>
2021-11-03 08:45:39 +0100

    Make private classes private explicitly

---
+++ haf/src/haf_private.hpp
@@ -0,0 +1,9 @@
+#ifndef HAF_PRIVATE_API_INCLUDE_HPP
+#define HAF_PRIVATE_API_INCLUDE_HPP
+
+#ifdef _WIN32
+#define HAF_PRIVATE
+#else
+#define HAF_PRIVATE __attribute__((visibility("hidden")))
+#endif
+#endif
