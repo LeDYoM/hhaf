dbd65fccce9ba1fcf17f0297e5fb90e2fe162c60
LeDYoM <ledyom@hotmail.com>
2021-12-19 20:09:16 +0100

    SharedDataViewer tests fixed

--- haf/tests/shareddata/shared_data_viewer.test.cpp
+++ haf/tests/shareddata/shared_data_viewer.test.cpp
@@ -19,33 +19,34 @@ TEST_CASE("SharedDataSystemViewer::SharedDataSystemViewer",
 
     SECTION("View from empty")
     {
-        auto shared_data_viewer{SharedDataViewer<ShareableTestData>{ssv.subSystem<ISharedData>()}};
+        auto shared_data_viewer{
+            SharedDataViewer<ShareableTestData>{ssv.subSystem<ISharedData>()}};
         auto const retrieve_result{
             shared_data_viewer.view(ShareableTestData::address())};
 
         CHECK(retrieve_result == nullptr);
     }
-/*
+
     SECTION("Store and view")
     {
         shareable_test_data.a = 42;
         shareable_test_data.b = 123.33F;
         shareable_test_data.c = "hello test";
 
-        auto shared_data_wrapper{
-            dwc.dataWrapper<SharedDataViewer<ShareableTestData>>()};
-        bool const store_result = shared_data_wrapper->store(
+        auto ishared_data{ssv.subSystem<ISharedData>()};
+        bool const store_result = ishared_data->store(
             ShareableTestData::address(), shareable_test_data);
 
         CHECK(store_result);
 
-        auto const view_result =
-            shared_data_wrapper->view(ShareableTestData::address());
+        auto shared_data_viewer{
+            SharedDataViewer<ShareableTestData>{ssv.subSystem<ISharedData>()}};
+        auto const view_result{
+            shared_data_viewer.view(ShareableTestData::address())};
 
         CHECK(view_result != nullptr);
         CHECK(view_result->a == 42);
         CHECK(view_result->b == 123.33F);
         CHECK(view_result->c == "hello test");
     }
-    */
 }
