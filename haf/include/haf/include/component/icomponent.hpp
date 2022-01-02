0daf94921c27d0299099fe77a9a523061a68d0cb
LeDYoM <ledyom@hotmail.com>
2022-01-02 15:03:04 +0100

    Got rid of attachables

--- haf/include/haf/include/component/icomponent.hpp
+++ haf/include/haf/include/component/icomponent.hpp
@@ -3,7 +3,6 @@
 
 #include <htypes/include/types.hpp>
 #include <haf/include/types/basic_types.hpp>
-#include <haf/include/utils/attachable.hpp>
 
 namespace haf::scene
 {
@@ -13,17 +12,79 @@ class SceneNode;
 namespace haf::component
 {
 
-class IComponent : public utils::Attachable<scene::SceneNode>
+class IComponent
 {
-    using BaseClass = utils::Attachable<scene::SceneNode>;
-
 public:
-    using AttachedNodeType = BaseClass::AttachedNodeType;
-
     /**
      * @brief Interface to be implemented to update the component
      */
     virtual void update() {}
+
+    /**
+     * @brief Destroy the Attachable object
+     */
+    virtual ~IComponent() = default;
+
+    /**
+     * @brief Method called after the component is attached to a node.
+     * Override it to perform initialization
+     */
+    virtual void onAttached() {}
+
+    void setAttachedNode(htps::rptr<scene::SceneNode> attachedNode) noexcept
+    {
+        attachedNode_ = std::move(attachedNode);
+        onAttached();
+    }
+
+protected:
+    /**
+     * @brief Get the attached node.
+     * @return The pointer to const attached node.
+     */
+    constexpr htps::rptr<scene::SceneNode> attachedNode() const noexcept
+    {
+        return attachedNode_;
+    }
+
+    /**
+     * @brief Get the attached node.
+     * @return The pointer to the attached node.
+     */
+    constexpr htps::rptr<scene::SceneNode> attachedNode() noexcept
+    {
+        return attachedNode_;
+    }
+
+    /**
+     * @brief Shortcut method to get the attached node converted to a type.
+     *
+     * @tparam Y Dest type to convert
+     * @return htps::rptr<Y> containing the attached node or nullptr if no
+     * conversion was possible.
+     */
+    template <typename Y>
+    htps::rptr<Y> attachedNodeAs() noexcept
+    {
+        return dynamic_cast<Y*>(attachedNode());
+    }
+
+    /**
+     * @brief Shortcut method to get the attached node converted to a type.
+     * Const version
+     *
+     * @tparam Y Dest type to convert
+     * @return htps::rptr<Y> containing the attached node or nullptr if no
+     * conversion was possible.
+     */
+    template <typename Y>
+    htps::rptr<Y const> attachedNodeAs() const noexcept
+    {
+        return dynamic_cast<Y const*>(attachedNode());
+    }
+
+private:
+    htps::rptr<scene::SceneNode> attachedNode_{nullptr};
 };
 
 }  // namespace haf::component
