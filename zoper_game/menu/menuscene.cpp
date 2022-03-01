52d3e15d4639d32b2c87cb86af1e00bacd35bece
Ismael Gonzalez <ledyom@hotmail.com>
2022-03-01 09:42:23 +0100

    Set default scene size

--- zoper_game/menu/menuscene.cpp
+++ zoper_game/menu/menuscene.cpp
@@ -42,7 +42,7 @@ void MenuScene::onCreated()
     // Set the default view for this scene
 //    cameraComponent()->view = DefaultView;
 //    cameraComponent()->view = SceneBox{-1000.0F, -1000.0F, 2000.0F, 2000.0F};
-    cameraComponent()->view = SceneBox{0.0F, 0.0F, 1.0F, 1.0F};
+    cameraComponent()->view = SceneBox{-0.5F, -0.5F, 1.0F, 1.0F};
 
     // Load the necessary resources
     auto resources_configurator{subSystem<res::IResourcesConfigurator>()};
@@ -57,9 +57,9 @@ void MenuScene::onCreated()
 
     // Create the logo
     auto main_menu_background_logo{
-        createSceneNode<RenderizablesSceneNode>("main_menu_background_logo")};
-//    main_menu_background_logo->prop<Position>() = vector2df{1000.0F, 300.0F};
-    main_menu_background_logo->prop<Scale>() = vector2d{0.5F, 0.5F};
+        createSceneNode<RenderizableSceneNode>("main_menu_background_logo")};
+    main_menu_background_logo->prop<Position>() = vector2df{0.F, -0.28F};
+    main_menu_background_logo->prop<Scale>() = vector2d{0.5F, 0.4F};
     main_menu_background_logo->renderizableBuilder()
         .name("mainLogo")
         .figType(FigType_t::Sprite)
