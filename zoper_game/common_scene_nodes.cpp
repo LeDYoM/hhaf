47888e9d271dd90cb9f903bc12723f93e6643af3
LeDYoM <ledyom@hotmail.com>
2022-04-01 16:13:53 +0200

    Fixed create standard background

--- zoper_game/common_scene_nodes.cpp
+++ zoper_game/common_scene_nodes.cpp
@@ -8,7 +8,6 @@
 #include <haf/include/render/renderizables.hpp>
 #include <haf/include/render/renderizable_modifier_context.hpp>
 
-using namespace htps;
 using namespace haf;
 using namespace haf::types;
 using namespace haf::scene;
@@ -16,38 +15,19 @@ using namespace haf::render;
 
 namespace zoper
 {
-void createStandardBackground(RenderizableBuilder&& renderizable_builder)
-{
-    auto background{
-        renderizable_builder.name("background")
-            .figType(FigType_t::Quad)
-            .box(rectFromSize(2000.0f, 2000.0f))
-            .colorModifier([](const RenderizableModifierContext& context) {
-                const auto n = context.normalizedVertexInBox();
-                static constexpr auto decrease_ratio = 0.5F;
-                return Color::fromFloats(n.y * decrease_ratio,
-                                         n.y * decrease_ratio,
-                                         n.y * decrease_ratio);
-            })
-            .create()};
-}
-
 void createStandardBackground(
-    haf::types::sptr<haf::scene::RenderizableSceneNode> const&
-        renderizable_scene_node)
+    sptr<RenderizableSceneNode> const& renderizable_scene_node)
 {
-    auto background{
-        renderizable_scene_node->renderizableBuilder()
-            .name("background")
-            .figType(FigType_t::Sprite)
-            .colorModifier([](const RenderizableModifierContext& context) {
-                const auto n = context.normalizedVertexInBox();
-                static constexpr auto decrease_ratio = 0.4F;
-                return Color::fromFloats(n.y * decrease_ratio,
-                                         n.y * decrease_ratio,
-                                         n.y * decrease_ratio);
-            })
-            .create()};
+    renderizable_scene_node->renderizableBuilder()
+        .name("background")
+        .figType(FigType_t::Sprite)
+        .colorModifier([](const RenderizableModifierContext& context) {
+            const auto n = context.normalizedVertexInBox();
+            static constexpr auto decrease_ratio = 0.4F;
+            return Color::fromFloats(n.y * decrease_ratio, n.y * decrease_ratio,
+                                     n.y * decrease_ratio);
+        })
+        .create();
 }
 
 }  // namespace zoper
