08def306a24fa945ba60c1ab7a52cc90cb497e5c
LeDYoM <ledyom@hotmail.com>
2022-04-18 12:07:17 +0200

    Now using VertexBuffer

--- backend_sfml/render_element.cpp
+++ backend_sfml/render_element.cpp
@@ -4,7 +4,7 @@
 namespace haf::backend::sfmlb
 {
 RenderElement::~RenderElement() = default;
-
+#ifdef USE_VERTEX_ARRAY
 void RenderElement::setPositions(iPosition const* const positions)
 {
     iPosition const* vtemp{positions};
@@ -79,5 +79,76 @@ void RenderElement::render(sf::RenderTarget& target) const
 
     target.draw(vertex_array_, render_states_);
 }
+#else
+void RenderElement::setPositions(iPosition const* const positions)
+{
+    iPosition const* vtemp{positions};
+    for (auto i{0U}; i < vertex_buffer_origin_.size(); ++i)
+    {
+        vertex_buffer_origin_[i].position = to_sf_type(*vtemp);
+        ++vtemp;
+    }
+    needs_update_ = true;
+}
 
+void RenderElement::setSize(htps::size_type const size)
+{
+    vertex_buffer_.create(static_cast<std::size_t>(size));
+    vertex_buffer_origin_.resize(size);
+    needs_update_ = true;
+}
+
+void RenderElement::setColors(iColor const* const colors)
+{
+    iColor const* vtemp{colors};
+    for (auto i{0U}; i < vertex_buffer_origin_.size(); ++i)
+    {
+        vertex_buffer_origin_[i].color = to_sf_type(*vtemp);
+        ++vtemp;
+    }
+    needs_update_ = true;
+}
+
+void RenderElement::setTexturecoordinates(
+    iTextureCoordinates const* const texture_coordinates)
+{
+    iTextureCoordinates const* vtemp{texture_coordinates};
+    for (auto i{0U}; i < vertex_buffer_origin_.size(); ++i)
+    {
+        vertex_buffer_origin_[i].texCoords = to_sf_type(*vtemp);
+        ++vtemp;
+    }
+    needs_update_ = true;
+}
+
+void RenderElement::setTexture(ITexture const* const texture)
+{
+    render_states_.texture = to_sf_type(texture);
+}
+
+void RenderElement::setModelViewMatrix(htps::f32 const* const model_view_matrix)
+{
+    render_states_.transform = to_sf_type(model_view_matrix);
+}
+
+void RenderElement::setProjectionMatrix(
+    htps::f32 const* const /*projection_matrix*/)
+{}
+
+void RenderElement::setShader(IShader const* const shader)
+{
+    render_states_.shader = to_sf_type(shader);
+}
+
+void RenderElement::render(sf::RenderTarget& target) const
+{
+    if (needs_update_)
+    {
+        vertex_buffer_.update(vertex_buffer_origin_.cbegin());
+        needs_update_ = false;
+    }
+    target.draw(vertex_buffer_, render_states_);
+}
+
+#endif
 }  // namespace haf::backend::sfmlb
--- backend_sfml/render_element.hpp
+++ backend_sfml/render_element.hpp
@@ -2,10 +2,11 @@
 #define HAF_BACKEND_SFMLB_RENDER_ELEMENT_INCLUDE_HPP
 
 #include <backend_dev/include/irender_element.hpp>
-#include <SFML/Graphics/VertexArray.hpp>
 #include <SFML/Graphics/RenderStates.hpp>
 #include <SFML/Graphics/RenderTarget.hpp>
 
+#ifdef USE_VERTEX_ARRAY
+#include <SFML/Graphics/VertexArray.hpp>
 namespace haf::backend::sfmlb
 {
 class RenderElement : public IRenderElement
@@ -29,6 +30,37 @@ private:
     sf::VertexArray vertex_array_{sf::PrimitiveType::Triangles};
     mutable sf::RenderStates render_states_ = sf::RenderStates::Default;
 };
+}  // namespace haf::backend::sfmlb
+
+#else
+#include <SFML/Graphics/VertexBuffer.hpp>
+
+namespace haf::backend::sfmlb
+{
+class RenderElement : public IRenderElement
+{
+public:
+    ~RenderElement() override;
+
+    void setSize(htps::size_type const size) override;
+    void setPositions(iPosition const* const positions) override;
+    void setColors(iColor const* const colors) override;
+    void setTexturecoordinates(
+        iTextureCoordinates const* const texture_coordinates) override;
+    void setTexture(ITexture const* const texture) override;
+    void setModelViewMatrix(htps::f32 const* const model_view_matrix) override;
+    void setProjectionMatrix(htps::f32 const* const projection_matrix) override;
+    void setShader(IShader const* const shader) override;
+
+    void render(sf::RenderTarget& target) const;
+
+private:
+    mutable sf::VertexBuffer vertex_buffer_{sf::PrimitiveType::Triangles, sf::VertexBuffer::Usage::Static};
+    htps::vector<sf::Vertex> vertex_buffer_origin_;
+    mutable bool needs_update_{true};
+    mutable sf::RenderStates render_states_ = sf::RenderStates::Default;
+};
+#endif
 }  // namespace haf::backend::sfmlb
 
 #endif
