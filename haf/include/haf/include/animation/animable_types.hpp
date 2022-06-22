0eda99eb08b35b1a6c006c3105ee7ddc707185bf
LeDYoM <ledyom@hotmail.com>
2022-06-22 10:18:58 +0200

    Temporary: Fixing color

--- haf/include/haf/include/animation/animable_types.hpp
+++ haf/include/haf/include/animation/animable_types.hpp
@@ -3,7 +3,6 @@ HTPS_PRAGMA_ONCE
 #define HAF_ANIM_ANIMABLETYPE_INCLUDE_HPP
 
 #include <htypes/include/types.hpp>
-#include <haf/include/scene/color.hpp>
 
 namespace haf::anim
 {
@@ -13,25 +12,6 @@ PropertyType interpolate(PropertyType const& begin,
                          htps::f32 const delta)
 {
     return begin + ((end - begin) * delta);
-}
-
-template <>
-inline scene::Color interpolate<scene::Color>(scene::Color const& begin,
-                                              scene::Color const& end,
-                                              htps::f32 const delta)
-{
-    using namespace htps;
-    f32 my_r{interpolate(static_cast<f32>(begin.red()),
-                         static_cast<f32>(end.red()), delta)};
-    f32 my_g{interpolate(static_cast<f32>(begin.green()),
-                         static_cast<f32>(end.green()), delta)};
-    f32 my_b{interpolate(static_cast<f32>(begin.blue()),
-                         static_cast<f32>(end.blue()), delta)};
-    f32 my_a{interpolate(static_cast<f32>(begin.alpha()),
-                         static_cast<f32>(end.alpha()), delta)};
-
-    return scene::Color{static_cast<u8>(my_r), static_cast<u8>(my_g),
-                        static_cast<u8>(my_b), static_cast<u8>(my_a)};
 }
 
 }  // namespace haf::anim
