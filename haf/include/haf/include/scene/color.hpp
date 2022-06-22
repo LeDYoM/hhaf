e703d31e360c34184e5ccc4380c4276475d47b3c
LeDYoM <ledyom@hotmail.com>
2022-06-22 10:38:07 +0200

    ColorImp deleted

--- haf/include/haf/include/scene/color.hpp
+++ haf/include/haf/include/scene/color.hpp
@@ -52,9 +52,10 @@ constexpr void denormalize(const htps::f32 v, value_type& d) noexcept
  *
  * @tparam value_type Type to represent the interal data values.
  */
-template <typename value_type>
-struct ColorImp
+struct Color
 {
+    using value_type = htps::u8;
+
     /**
      * @brief Maximum value for each coordinate
      */
@@ -70,7 +71,7 @@ struct ColorImp
      *
      * @tparam Source Type of the source value
      * @param source Source value of type Source
-     * @return value_type Value of the type required for this ColorImp
+     * @return value_type Value of the type required for this Color
      */
     template <typename Source>
     static constexpr value_type ensureLimits(Source source) noexcept
@@ -91,104 +92,77 @@ struct ColorImp
     /**
      * @brief Default constructor. Sets color to black opaque
      */
-    constexpr ColorImp() noexcept :
+    constexpr Color() noexcept :
         r{value_min}, g{value_min}, b{value_min}, a{value_max}
     {}
 
     /**
-     * @brief Construct a ColorImp object from four values
+     * @brief Construct a Color object from four values
      * @param red Red component of the color
      * @param green Green component of the color
      * @param blue Blue component of the color
      * @param alpha Alpha component of the color (Defaulted to opaque)
      */
-    constexpr ColorImp(value_type const red,
-                       value_type const green,
-                       value_type const blue,
-                       value_type const alpha = value_max) noexcept :
+    constexpr Color(value_type const red,
+                    value_type const green,
+                    value_type const blue,
+                    value_type const alpha = value_max) noexcept :
         r{red}, g{green}, b{blue}, a{alpha}
     {}
 
-    constexpr ColorImp(ColorImp const&) noexcept = default;
-    constexpr ColorImp& operator=(ColorImp const&) noexcept = default;
-    constexpr ColorImp(ColorImp&&) noexcept                 = default;
-    constexpr ColorImp& operator=(ColorImp&&) noexcept = default;
+    constexpr Color(Color const&) noexcept = default;
+    constexpr Color& operator=(Color const&) noexcept = default;
+    constexpr Color(Color&&) noexcept                 = default;
+    constexpr Color& operator=(Color&&) noexcept = default;
 
     /**
-     * @brief Construct a ColorImp object from another color, but with a
+     * @brief Construct a Color object from another color, but with a
      * different alpha.
      * @param base_color The color that will give the r,g,b values
      * @param alpha The alpha value
      */
-    constexpr ColorImp(ColorImp const& base_color,
-                       value_type const alpha) noexcept :
+    constexpr Color(Color const& base_color, value_type const alpha) noexcept :
         r{base_color.r}, g{base_color.g}, b{base_color.b}, a{alpha}
     {}
 
-    // TODO: Enable_if for value_type = u8
-    constexpr explicit ColorImp(const htps::u32 color) noexcept :
+    constexpr explicit Color(htps::u32 const color) noexcept :
         r{static_cast<value_type>((color & 0xFF000000) >> 24U)},
         g{static_cast<value_type>((color & 0x00FF0000) >> 16U)},
         b{static_cast<value_type>((color & 0x0000FF00) >> 8U)},
         a{static_cast<value_type>((color & 0x000000FF) >> 0U)}
     {}
 
-    /**
-     * @brief Copy assignment from a color from a different value_type
-     *
-     * @tparam vt Value type of the other color
-     * @param other Other color
-     * @return ColorImp& This color
-     */
-    template <typename vt>
-    constexpr ColorImp& operator=(const ColorImp<vt>& other) noexcept
-    {
-        r = ensureLimits(static_cast<value_type>(other.r));
-        g = ensureLimits(static_cast<value_type>(other.g));
-        b = ensureLimits(static_cast<value_type>(other.b));
-        a = ensureLimits(static_cast<value_type>(other.a));
-        return *this;
-    }
-
-    static constexpr ColorImp fromFloats(htps::f32 const red,
+    static constexpr Color fromFloats(htps::f32 const red,
                                          htps::f32 const green,
                                          htps::f32 const blue,
                                          htps::f32 const alpha = 1.0F) noexcept
     {
-        return ColorImp{static_cast<value_type>(red * value_max),
-                        static_cast<value_type>(green * value_max),
-                        static_cast<value_type>(blue * value_max),
-                        static_cast<value_type>(alpha * value_max)};
+        return Color{static_cast<value_type>(red * value_max),
+                     static_cast<value_type>(green * value_max),
+                     static_cast<value_type>(blue * value_max),
+                     static_cast<value_type>(alpha * value_max)};
     }
 
-    template <typename vt>
-    constexpr ColorImp(ColorImp<vt> const& other) noexcept :
-        r{ensureLimits(static_cast<value_type>(other.r))},
-        g{ensureLimits(static_cast<value_type>(other.g))},
-        b{ensureLimits(static_cast<value_type>(other.b))},
-        a{ensureLimits(static_cast<value_type>(other.a))}
-    {}
-
     /**
-     * @brief Equality comparation with another ColorImp of the same type
+     * @brief Equality comparation with another Color of the same type
      *
      * @param rhs Right part of the comparison
      * @return The colors are equal
      * @return The colors are not equal
      */
-    constexpr bool operator==(ColorImp const& rhs) const noexcept
+    constexpr bool operator==(Color const& rhs) const noexcept
     {
         return (r == rhs.r && g == rhs.g && b == rhs.b && a == rhs.a);
     }
 
     /**
-     * @brief Unequality comparation with another ColorImp of the same type
+     * @brief Unequality comparation with another Color of the same type
      *
      * @param rhs Right part of the comparison
      * @return The colors are not equal
      * @return The colors are equal
      */
-    constexpr bool operator!=(ColorImp const& rhs) const noexcept
+    constexpr bool operator!=(Color const& rhs) const noexcept
     {
         return !(*this == rhs);
     }
@@ -197,41 +171,36 @@ struct ColorImp
      * @brief Substract two colors (They might be of different type). The result
      * is ensured to be a valid color of the source type
      *
-     * @tparam VT Value type of the second color
      * @param rhs  Right hand side operand
-     * @return ColorImp with the resulting color
+     * @return Color with the resulting color
      */
-    template <typename VT>
-    constexpr ColorImp operator-(ColorImp<VT> const& rhs) const noexcept
+    constexpr Color operator-(Color const& rhs) const noexcept
     {
-        return (ColorImp{*this} -= rhs);
+        return Color{*this} -= rhs;
     }
 
     /**
      * @brief Multiply two colors (They might be of different type). The result
      * is ensured to be a valid color of the source type
      *
-     * @tparam VT Value type of the second color
      * @param rhs  Right hand side operand
-     * @return ColorImp with the resulting color
+     * @return Color with the resulting color
      */
-    template <typename VT>
-    constexpr ColorImp operator*(ColorImp<VT> const& rhs) const noexcept
+    constexpr Color operator*(Color const& rhs) const noexcept
     {
-        return (ColorImp{*this} *= rhs);
+        return Color{*this} *= rhs;
     }
 
-    template <typename vt>
-    constexpr ColorImp& operator+=(ColorImp<vt> const& right) noexcept
+    constexpr Color& operator+=(Color const& right) noexcept
     {
-        r = ensureLimits(static_cast<vt>(r) + right.r);
-        g = ensureLimits(static_cast<vt>(g) + right.g);
-        b = ensureLimits(static_cast<vt>(b) + right.b);
-        a = ensureLimits(static_cast<vt>(a) + right.a);
+        r = ensureLimits(r + right.r);
+        g = ensureLimits(g + right.g);
+        b = ensureLimits(b + right.b);
+        a = ensureLimits(a + right.a);
         return *this;
     }
 
-    constexpr ColorImp& operator-=(const ColorImp& right) noexcept
+    constexpr Color& operator-=(Color const& right) noexcept
     {
         r = ensureLimits(static_cast<htps::s32>(r) - right.r);
         g = ensureLimits(static_cast<htps::s32>(g) - right.g);
@@ -240,7 +209,7 @@ struct ColorImp
         return *this;
     }
 
-    constexpr ColorImp& operator*=(const ColorImp& right) noexcept
+    constexpr Color& operator*=(Color const& right) noexcept
     {
         r = static_cast<value_type>((static_cast<htps::u32>(r) * right.r) /
                                     value_max);
@@ -253,7 +222,7 @@ struct ColorImp
         return *this;
     }
 
-    constexpr ColorImp& operator*=(htps::f32 const delta) noexcept
+    constexpr Color& operator*=(htps::f32 const delta) noexcept
     {
         r = detail::ensureLimits<value_type>(static_cast<htps::f32>(r) * delta);
         g = detail::ensureLimits<value_type>(static_cast<htps::f32>(g) * delta);
@@ -262,7 +231,7 @@ struct ColorImp
         return *this;
     }
 
-    constexpr ColorImp& operator/=(const htps::f32 delta) noexcept
+    constexpr Color& operator/=(htps::f32 const delta) noexcept
     {
         r = detail::ensureLimits<value_type>(static_cast<htps::f32>(r) / delta);
         g = detail::ensureLimits<value_type>(static_cast<htps::f32>(g) / delta);
@@ -276,9 +245,6 @@ struct ColorImp
     constexpr value_type blue() const noexcept { return b; }
     constexpr value_type alpha() const noexcept { return a; }
 
-    template <typename vt>
-    friend struct ColorImp;
-
 private:
     value_type r;
     value_type g;
@@ -286,43 +252,31 @@ private:
     value_type a;
 };
 
-template <typename vt>
-constexpr ColorImp<vt> operator*(ColorImp<vt> const& color,
-                                 htps::f32 const delta) noexcept
+constexpr Color operator*(Color const& color, htps::f32 const delta) noexcept
 {
-    return ColorImp<vt>{color} *= delta;
+    return Color{color} *= delta;
 }
 
-template <typename vt>
-constexpr ColorImp<vt> operator*(htps::f32 const delta,
-                                 ColorImp<vt> const& color) noexcept
+constexpr Color operator*(htps::f32 const delta, Color const& color) noexcept
 {
-    return ColorImp<vt>{color} *= delta;
+    return Color{color} *= delta;
 }
 
-template <typename vt>
-constexpr ColorImp<vt> operator/(ColorImp<vt> const& color,
-                                 htps::f32 const delta) noexcept
+constexpr Color operator/(Color const& color, htps::f32 const delta) noexcept
 {
-    return ColorImp<vt>{color} /= delta;
+    return Color{color} /= delta;
 }
 
-template <typename vt>
-constexpr ColorImp<vt> operator/(htps::f32 const delta,
-                                 ColorImp<vt> const& color) noexcept
+constexpr Color operator/(htps::f32 const delta, Color const& color) noexcept
 {
-    return ColorImp<vt>{color} /= delta;
+    return Color{color} /= delta;
 }
 
-template <typename vt>
-constexpr ColorImp<vt> operator+(ColorImp<vt> const& lhs,
-                                 ColorImp<vt> const& rhs) noexcept
+constexpr Color operator+(Color const& lhs, Color const& rhs) noexcept
 {
-    return (ColorImp{lhs} += rhs);
+    return (Color{lhs} += rhs);
 }
 
-extern template struct ColorImp<htps::u8>;
-using Color = ColorImp<htps::u8>;
 
 inline scene::Color interpolate(scene::Color const& begin,
                                 scene::Color const& end,
@@ -330,13 +284,13 @@ inline scene::Color interpolate(scene::Color const& begin,
 {
     using namespace htps;
     f32 my_r{anim::interpolate(static_cast<f32>(begin.red()),
-                         static_cast<f32>(end.red()), delta)};
+                               static_cast<f32>(end.red()), delta)};
     f32 my_g{anim::interpolate(static_cast<f32>(begin.green()),
-                         static_cast<f32>(end.green()), delta)};
+                               static_cast<f32>(end.green()), delta)};
     f32 my_b{anim::interpolate(static_cast<f32>(begin.blue()),
-                         static_cast<f32>(end.blue()), delta)};
+                               static_cast<f32>(end.blue()), delta)};
     f32 my_a{anim::interpolate(static_cast<f32>(begin.alpha()),
-                         static_cast<f32>(end.alpha()), delta)};
+                               static_cast<f32>(end.alpha()), delta)};
 
     return scene::Color{static_cast<u8>(my_r), static_cast<u8>(my_g),
                         static_cast<u8>(my_b), static_cast<u8>(my_a)};
--- haf/include/haf/include/scene/color_imp.hpp
+++
@@ -1,9 +0,0 @@
-HTPS_PRAGMA_ONCE
-#ifndef HAF_SCENE_COLOR_IMP_INCLUDE_HPP
-#define HAF_SCENE_COLOR_IMP_INCLUDE_HPP
-
-namespace haf::scene
-{
-}  // namespace haf::scene
-
-#endif
