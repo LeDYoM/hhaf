93c029c7a01afd4f81d123afb44f8e1e0356e9bf
LeDYoM <ledyom@hotmail.com>
2023-06-23 16:35:42 +0200

    wip

--- haf/include/haf/include/math/vvector.hpp
+++ haf/include/haf/include/math/vvector.hpp
@@ -2,12 +2,14 @@ HAF_PRAGMA_ONCE
 #ifndef HAF_CORE_MATH_MATH_VVECTOR_INCLUDE_HPP
 #define HAF_CORE_MATH_MATH_VVECTOR_INCLUDE_HPP
 
+#include <haf/include/haf_export.hpp>
 #include <haf/include/core/types.hpp>
 #include <facil_math/include/vector3d.hpp>
 #include <facil_math/include/vvector.hpp>
 
 namespace haf::math
 {
+HAF_EXPORT_TEMPLATE template class HAF_API fmath::vvector3d<core::f32>;
 using vvector3df = fmath::vvector3d<core::f32>;
 
 }  // namespace haf::math
