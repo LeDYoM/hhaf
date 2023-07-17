015fe32884fb68fcd041055b822953bc1f71c749
LeDYoM <ledyom@hotmail.com>
2023-07-17 10:34:08 +0200

    wip

--- haf/src/render/render_system.cpp
+++ haf/src/render/render_system.cpp
@@ -3,6 +3,11 @@
 #include <hlog/include/hlog.hpp>
 #include <hogl/include/render_system_functions.hpp>
 
+#include "imgui.h"
+#include "backends/imgui_impl_opengl3.h"
+#include "backends/imgui_impl_win32.h"
+#include <Windows.h>
+
 using namespace haf::core;
 using namespace haf::render;
 
@@ -14,10 +19,40 @@ void RenderSystem::initialize(bool const enableDebug)
     LogAsserter::log_assert(m_initialized == true);
     ogl::setDebug(enableDebug);
     ogl::setDefaultRenderState();
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;   // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;    // Enable Gamepad Controls
+    // Setup Platform/Renderer bindings
+    ImGui_ImplOpenGL3_Init();
+    ImGui_ImplWin32_Init(GetActiveWindow());
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
 }
 
 void RenderSystem::update()
-{}
+{
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplWin32_NewFrame();
+
+    ImGui::NewFrame();
+
+    ImGui::Begin("Demo window");
+    if (ImGui::Button("Hello world!"))
+    {
+        ImGui::Text("abc");
+    }
+    ImGui::End();
+
+    // Render dear imgui into screen
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+}
 
 sptr<RenderTarget> const& RenderSystem::currentRenderTarget() const
 {
