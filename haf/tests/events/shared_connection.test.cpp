19a84ddda6f17d26db7654d0d903eba1c4974937
LeDYoM <ledyom@hotmail.com>
2023-10-04 11:05:52 +0200

    wip

--- haf/tests/events/shared_connection.test.cpp
+++ haf/tests/events/shared_connection.test.cpp
@@ -5,6 +5,7 @@
 #include <haf/include/events/emitter.hpp>
 #include <haf/include/events/receiver.hpp>
 #include <haf/include/events/connection.hpp>
+#include <haf/include/core/types.hpp>
 
 using namespace haf;
 using namespace haf::core;
@@ -13,19 +14,32 @@ using namespace haf::evt;
 struct StructWithEmmiter
 {
     emitter<s32> m_emitter;
-    s32 data{0};
+    s32 m_data{0};
 };
 
 struct StructToReceive
 {
-    void receiveData(s32 const data)
-    {
-        m_data = data;
-    }
+    void receive(s32 const data) { m_data = data; }
     s32 m_data{0};
+    ireceiver m_receiver;
+
+    void connectTo(sptr<StructWithEmmiter> s_emitter)
+    {
+        m_receiver.shared_connect(
+            s_emitter, s_emitter->m_emitter,
+            make_function(this, &StructToReceive::receive));
+    }
 };
 
 TEST_CASE("emitter_to_shared", "[emitter][connection]")
 {
-}
+    auto test_emmiter{msptr<StructWithEmmiter>()};
+    StructToReceive receiver{};
 
+    receiver.connectTo(test_emmiter);
+    CHECK(0 == receiver.m_data);
+    CHECK(0 == test_emmiter->m_data);
+
+    test_emmiter->m_emitter(-5);
+    CHECK(-5 == receiver.m_data);
+}
