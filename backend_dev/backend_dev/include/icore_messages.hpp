7a4b1d2f583e3d0067a5ac67e13adde2ac7a585a
LeDYoM <ledyom@hotmail.com>
2024-03-19 10:06:03 +0100

    wip

---
+++ backend_dev/backend_dev/include/icore_messages.hpp
@@ -0,0 +1,22 @@
+#ifndef HAF_BACKEND_ICORE_MESSAGES_INCLUDE_HPP
+#define HAF_BACKEND_ICORE_MESSAGES_INCLUDE_HPP
+
+#include <htypes/include/types.hpp>
+#include <backend_dev/include/ikey.hpp>
+
+namespace haf::backend
+{
+class ICoreMessages
+{
+public:
+
+protected:
+    /**
+     * @brief Destroy the ICoreMessages object
+     */
+    virtual ~ICoreMessages() = default;
+};
+
+}  // namespace haf::backend
+
+#endif
