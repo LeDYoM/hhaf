29e386650ccef46bfb48b264a8aad1b379d6535e
LeDYoM <ledyom@hotmail.com>
2025-05-28 09:46:09 +0200

    wip

---
+++ haf/src/scene_pass/scene_pass_manager.hpp
@@ -0,0 +1,18 @@
+HTPS_PRAGMA_ONCE
+#ifndef HAF_SCENE_SCENE_PASS_MANAGER_INCLUDE_HPP
+#define HAF_SCENE_SCENE_PASS_MANAGER_INCLUDE_HPP
+
+#include <haf/include/core/types.hpp>
+#include "haf_private.hpp"
+#include <haf/include/scene/scene_node.hpp>
+
+namespace haf::scene
+{
+class HAF_PRIVATE ScenePassManager
+{
+public:
+};
+
+}  // namespace haf::scene
+
+#endif
