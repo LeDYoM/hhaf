3e3f11d1772b554207b530b283b44985c1662e26
LeDYoM <ledyom@hotmail.com>
2025-07-16 09:30:04 +0200

    wip

--- haf/include/haf/include/core/log.hpp
+++ haf/include/haf/include/core/log.hpp
@@ -4,9 +4,7 @@
 #include <hlog/include/hlog.hpp>
 
 #define HAF_LOG_DEBUG(...)  haf::DisplayLog::debug(__VA_ARGS__)
+#define HAF_LOG_VERBOSE(...)  haf::DisplayLog::verbose(__VA_ARGS__)
+#define HAF_LOG_INFO(...)  haf::DisplayLog::info(__VA_ARGS__)
 
-namespace haf
-{
-
-}  // namespace haf
 #endif
