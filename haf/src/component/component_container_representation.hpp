8efbbe0897e26eb45844e532fa7579ecb0f37824
LeDYoM <ledyom@hotmail.com>
2025-10-02 19:40:16 +0200

    wip

--- haf/src/component/component_container_representation.hpp
+++ haf/src/component/component_container_representation.hpp
@@ -2,10 +2,15 @@ HTPS_PRAGMA_ONCE
 #ifndef HAF_COMPONENT_COMPONENT_CONTAINER_REPRESENTATION_INCLUDE_HPP
 #define HAF_COMPONENT_COMPONENT_CONTAINER_REPRESENTATION_INCLUDE_HPP
 
+#include "haf_private.hpp"
 
 namespace haf::component
 {
-
+/// @brief
+class HAF_PRIVATE ComponentContainerRepresentation
+{
+    vector<sptr<Component>> m_components;
+}
 }  // namespace haf::component
 
 #endif
