98c4cc1bf0597600104f77f6186af93ea2479d02
Ismael Gonzalez <ledyom@hotmail.com>
2026-02-16 17:03:25 +0100

    wip

--- haf/include/haf/include/core/log.hpp
+++ haf/include/haf/include/core/log.hpp
@@ -1,11 +1,8 @@
-#ifndef HAF_CORE_LOG_INCLUDE_HPP
-#define HAF_CORE_LOG_INCLUDE_HPP
+export module haf:core;
 
-#include <hlog/include/hlog.hpp>
+import hlog;
 
 #define HAF_LOG_DEBUG(...)  haf::DisplayLog::debug(__VA_ARGS__)
 #define HAF_LOG_VERBOSE(...)  haf::DisplayLog::verbose(__VA_ARGS__)
 #define HAF_LOG_INFO(...)  haf::DisplayLog::info(__VA_ARGS__)
 #define HAF_ASSERT(...) haf::LogAsserter::log_assert(__VA_ARGS__)
-
-#endif
