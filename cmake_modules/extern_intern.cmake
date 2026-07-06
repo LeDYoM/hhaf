19923cd05ed4e6aa475805f3b4df630e01365b9f
Ismael Gonzalez <ledyom@hotmail.com>
2026-07-06 16:38:43 +0200

    Updated agloader dependency

--- cmake_modules/extern_intern.cmake
+++ cmake_modules/extern_intern.cmake
@@ -4,7 +4,7 @@ function (load_agloader)
   FetchContent_Declare(
     agloader
     GIT_REPOSITORY https://github.com/LeDYoM/agloader.git
-    GIT_TAG main
+    GIT_TAG rc_0.3.1.7
   )
 
   FetchContent_MakeAvailable(agloader)
